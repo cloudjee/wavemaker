344b68929f32d31314ba52a74d2e05ae31a8667c
Michael Kantor <mkantor@vmware.com>
2010-09-09 21:22:26 +0000

    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@29862 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/app/templates/javaservices/FileUploadDownload.java
@@ -0,0 +1,6 @@
+String s1 = "${java.lang.String:returnName:the string to return}";
+String s2 = "${java.lang.Integer:myInt:the string to return}";
+String s3 = "${java.lang.String:returnName3:the string to return}";
+
+public String getS1() {return s1;}
+
