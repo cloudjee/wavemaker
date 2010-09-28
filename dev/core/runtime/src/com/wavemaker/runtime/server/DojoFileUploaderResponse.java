eab3d3eeb77544893e6c8a26400af2c5645ef359
Michael Kantor <mkantor@vmware.com>
2010-09-28 21:50:55 +0000

    New server classes for handling requests from the dojo File Uploader flash widget and generating a response that the flash widget will condescend to parse.
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30015 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/core/runtime/src/com/wavemaker/runtime/server/DojoFileUploaderResponse.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.wavemaker.runtime.server;
+
+import java.io.InputStream;
+
+/**
+ * A class containing a download response. This should be used as the return
+ * type for any operation which handles download requests.
+ * 
+ * @author Matt Small
+ * @version $Rev: 29059 $ - $Date: 2010-04-29 17:19:33 -0700 (Thu, 29 Apr 2010) $
+ * 
+ */
+public class DojoFileUploaderResponse {
+    
+    public DojoFileUploaderResponse() {
+        // empty constructor
+    }
+    
+    public DojoFileUploaderResponse(String path, String name, String type, String error, String width, String height) {
+        this.path = path;
+        this.name = name;
+        this.type = type;
+        this.error= error;
+        this.width= width;
+        this.height=height;
+    }
+
+    private String path;
+    private String name;
+    private String type;
+    private String error;
+    private String width;
+    private String height;
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
+    }
+    public String getWidth() {
+        return width;
+    }
+    public void setWidth(String width) {
+        this.width = width;
+    }
+    public String getHeight() {
+        return height;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+}
---
+++ dev/core/runtime/src/com/wavemaker/runtime/server/FlashFileUploadController.java
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.wavemaker.runtime.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wavemaker.common.WMException;
+import com.wavemaker.common.WMRuntimeException;
+import com.wavemaker.runtime.server.view.FlashUploadResponseView;
+import com.wavemaker.runtime.server.view.DownloadView;
+import com.wavemaker.runtime.server.view.TypedView;
+import com.wavemaker.runtime.service.ServiceWire;
+import com.wavemaker.runtime.service.TypedServiceReturn;
+
+/**
+ * @author small
+ * @version $Rev: 29059 $ - $Date: 2010-04-29 17:19:33 -0700 (Thu, 29 Apr 2010) $
+ *
+ */
+public class FlashFileUploadController extends ControllerBase {
+
+    /** Logger for this class and subclasses */
+    protected final Logger logger = Logger.getLogger(getClass());
+    
+      /** UPLOADS stores the location of files on disk.  Windows 
+	  users 
+	  * must update this; Mac & Linux should be fine. */ 
+
+    /* (non-Javadoc)
+     * @see com.wavemaker.runtime.server.ControllerBase#executeRequest(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    protected ModelAndView executeRequest(HttpServletRequest request,
+            HttpServletResponse response) throws IOException, WMException {
+        
+        String serviceName = ServerUtils.getServiceName(request);
+        Map<String, Object[]> params = ServerUtils.mergeParams(request);
+        String method = ServerUtils.getMethod(params);
+        if (logger.isInfoEnabled()) {
+            logger.info("invoke service: " + serviceName + ", method: " + method);
+            if (logger.isDebugEnabled()) {
+                logger.debug("method "+method+" parameters: "+params);
+            }
+        }
+        
+        ServiceWire sw = this.getServiceManager().getServiceWire(serviceName);
+        TypedServiceReturn reflInvokeRef = invokeMethod(sw, method, null, params);
+        
+        return getModelAndView(getView(), reflInvokeRef);
+    }
+
+    /* (non-Javadoc)
+     * @see com.wavemaker.runtime.server.ControllerBase#getView()
+     */
+    @Override
+    protected TypedView getView() {
+        return new FlashUploadResponseView();
+    }
+
+    /* (non-Javadoc)
+     * @see com.wavemaker.runtime.server.ControllerBase#handleError(java.lang.String, java.lang.Throwable)
+     */
+    @Override
+    protected ModelAndView handleError(String message, Throwable t) {
+        throw new WMRuntimeException(t);
+    }
+}
