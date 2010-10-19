b92d37cc4c548f44247b7fa4e993599b6dde3713
Michael Kantor <mkantor@vmware.com>
2010-10-19 22:14:57 +0000

    Response object to use to respond to the DojoFileUpload widget when using the html uploader
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30236 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/core/runtime/src/com/wavemaker/runtime/server/FileUploadResponse.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright (C) 2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.wavemaker.runtime.server;
+
+/* Response object to return when sending a file via the "html" version of the DojoFileUpload widget */
+
+public class FileUploadResponse {
+    private String path;
+    private String error;
+    private String width;
+    private String height;
+
+    public String getPath() {return path;}
+    public void setPath(String path) {this.path = path;}
+
+    public String getError() {return error;}
+    public void setError(String error) {this.error = error;}
+
+    public String getWidth() {return width;}
+    public void setWidth(String width) {this.width = width;}
+
+    public String getHeight() {return height;}
+    public void setHeight(String height) {this.height = height;}
+
+};
