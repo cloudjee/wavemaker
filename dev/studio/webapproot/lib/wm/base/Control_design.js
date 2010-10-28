11c031fe813b2a26283d60f684bde3ff41429aaf
Michael Kantor <mkantor@vmware.com>
2010-10-28 18:21:24 +0000

    wm.isDesignable can now determine if app level components are designable
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30370 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/Control_design.js
+++ dev/studio/webapproot/lib/wm/base/Control_design.js
@@ -20,7 +20,8 @@ dojo.provide("wm.base.Control_design");
 wm.isDesignable = function(inControl) {
 	// inControl is designable if it has a non-Widget owner
         // return inControl.owner && !(inControl.owner instanceof wm.Control);
-    return inControl.owner && inControl.owner == studio.page || inControl.owner == studio.application && (inControl instanceof wm.Dialog || inControl.isAncestorInstanceOf(wm.DesignableDialog));
+    // warning: can't use inControl.owner == studio.application as studio.application may not have been set yet if we're still creating app level dialogs and components
+    return inControl.owner && inControl.owner == studio.page || inControl.owner instanceof wm.Application && (inControl instanceof wm.Dialog || inControl.isAncestorInstanceOf(wm.DesignableDialog));
 }
 
 wm.Control.extend({
