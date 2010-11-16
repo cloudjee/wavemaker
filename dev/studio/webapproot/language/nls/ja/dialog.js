ddfb3e3f4fb6cdd24e82ef2b95e434bf5927637b
Derek Henninger <dhenninger@vmware.com>
2010-11-16 01:21:01 +0000

    Added copyright messages to Japanese language files.
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30573 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/language/nls/ja/dialog.js
+++ dev/studio/webapproot/language/nls/ja/dialog.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2010 Infoteria Corporation and WaveMaker Software, Inc.
+ *
+ * This file is part of WaveMaker Studio.
+ *
+ * WaveMaker Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, only.
+ *
+ * WaveMaker Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with WaveMaker Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */
 {
 	"Welcome" : "Welcome",
 	"WelcomeMessage" : "WaveMaker Studioへようこそ",
