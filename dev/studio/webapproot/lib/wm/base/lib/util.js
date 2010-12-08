fcb2c2be3fbc3071cd2b29cd0eb430b08f228ac1
Michael Kantor <mkantor@vmware.com>
2010-12-08 19:05:06 +0000

    Fixed stupid bug in the not-much-used wm.Array.equals method
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30729 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/lib/util.js
+++ dev/studio/webapproot/lib/wm/base/lib/util.js
@@ -38,21 +38,6 @@ wm.decapitalize = function(s) {
 	return s ? s.charAt(0).toLowerCase() + s.slice(1) : "";
 }
 
-wm.requireCss = function(modulepath) {
-    var stylenode = dojo.byId("CSS_" + modulepath);
-    if (stylenode) return;
-
-    var parts = modulepath.split(".");
-    var filename = parts.pop();
-    var path = parts.join(".");
-    path = dojo.moduleUrl(path).path + filename + ".css";
-    stylenode = document.createElement("link");
-    stylenode.id = "CSS_"+modulepath;
-    stylenode.type="text/css";
-    stylenode.href = path;
-    document.getElementsByTagName("head")[0].appendChild(stylenode);
-}
-
 wm.isEqual = function (a1, a2){ 
 	try{
 		if(a1 == a2)
@@ -562,7 +547,7 @@ wm.Array.equals = function(a, b,optionalCallback) {
     if (a == b) return true;
     if (!a || !b) return false;
     if (a.length != b.length) return false;
-    for (var i = 0; i < i.length; i++) {
+    for (var i = 0; i < a.length; i++) {
 	if (optionalCallback) {
 	    if (!optionalCallback(a[i],b[i])) return false;
 	} else {
