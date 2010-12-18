5832c48b0e0039ea3b7db3bdf40b0c2fd7727369
Michael Kantor <mkantor@vmware.com>
2010-12-18 01:48:33 +0000

    Assorted failed attempts to get Bespin to work reliably
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30797 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/widget/Bespin.js
+++ dev/studio/webapproot/lib/wm/base/widget/Bespin.js
@@ -16,6 +16,8 @@ dojo.declare("wm.Bespin", wm.Container, {
     layoutKind: "left-to-right",
     horizontalAlign: "left",
     verticalAlign: "top",
+    minWidth: "300",
+    minHeight: "300",
 
     // Access to bespin objects
     _editor: null,
@@ -31,9 +33,12 @@ dojo.declare("wm.Bespin", wm.Container, {
     // toolbar props
     useSearchToolbar: true,
     _lastFind: "",
+    prettyPrintButton: false,
+    evalButton: false,
+    toolbarId: "",
 
     // autocompletion props
-    useAutoCompletion: true, // only works for javascript
+    useAutoCompletion: false, // only works for javascript
     autoCompletionThisPointer: "main",    
 
     // wm.Control props
@@ -56,18 +61,21 @@ dojo.declare("wm.Bespin", wm.Container, {
     },
     postInit: function() {
 	this.inherited(arguments);
-	this.leftContainer = new wm.Container({name: "leftContainer", width: "100%", height: "100%", layoutKind: "top-to-bottom", horizontalAlign: "left", verticalAlign: "top", parent: this, owner: this});
-        this.toolBar = new wm.Container({name: "toolBar", width: "100%", height: "25px", margin: "0,0,2,0", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left", parent: this.leftContainer, owner: this});	
+	this.leftContainer = new wm.Container({lock: true, name: "leftContainer", width: "100%", height: "100%", layoutKind: "top-to-bottom", horizontalAlign: "left", verticalAlign: "top", parent: this, owner: this});
+
+	if (this.toolbarId) 
+	    this.toolBar = this.owner.getValue(this.toolbarId);
+	else
+	    this.toolBar = new wm.Container({name: "toolBar", width: "100%", height: "25px", layoutKind: "left-to-right", owner: this, parent: this.leftContainer});
         this.bespinControl = new wm.Control({name: "bespinControl", width: "100%", height: "100%", parent: this.leftContainer, owner: this});
         this.bespinControl.connect(this.bespinControl, "renderBounds", this, function() {
-	    if (this._env) 
-	        this._env.dimensionsChanged();
+	    if (this._env) {
+		wm.job(this.getRuntimeId() + ":dimensionsChanged", 10, dojo.hitch(this, function() {
+		    this._editor.dimensionsChanged();
+		}));
+	    }
         });
 
-	if (wm.Bespin.onLoadFinished)
-	    this.initBespinObject();
-	else
-	    this.connect(null, "onBespinLoad", this, "initBespinObject");
         this.buildToolbar();
 	if (this.useAutoCompletion) {
 	    this.autoCompletionList = new wm.List({name: "autoCompletionList",
@@ -77,32 +85,99 @@ dojo.declare("wm.Bespin", wm.Container, {
 						   parent: this,
 						   owner: this});
 	    this.autoCompletionList.connect(this.autoCompletionList, "onselect", this, function() {
-		this._editor.selectedText = this.autoCompletionList.selectedItem.getData().dataValue;
-		this._editor.setCursor(this._editor.selection.end);
-		this.focus();		
+
+		var length = this._findRemainder.length + this._findToken.length;
+		this._editor.selection = {end:   this._editor.selection.end,
+					  start: {row: this._editor.selection.end.row,
+						  col: this._editor.selection.end.col-length}};
+
+		var newtext = this._findToken + 
+		    ((this._findRemainder) ? "." + this._findRemainder : "") + 
+		    this.autoCompletionList.selectedItem.getData().dataValue;
+		this._editor.selectedText = newtext;
+
+		this._editor.selection = {end:   this._editor.selection.end,
+					  start: {row: this._editor.selection.end.row,
+						  col: this._editor.selection.end.col-newtext.length}};
+
+
+		//this._editor.setCursor(this._editor.selection.end);
+		//this.focus();		
 	    });
 	    this.autoCompletionVar = new wm.Variable({name: "autoCompletionVar",
 						      type: "StringData",
 						      isList: true,
 						      owner: this});
 	    this.autoCompletionList.setDataSet(this.autoCompletionVar);
-	    this.thisRegex = new RegExp("^this.");
+	    this.autoCompletionThisRegex = new RegExp("^this.");
 	}
 
 	this.connect(this.domNode, "onkeydown", this, "handleKeyPress");
-	
+
+    },
+    renderBounds: function() {
+	this.inherited(arguments);
+
+	// return if we either aren't ready to generate the editor or we already have the editor
+	if (this._cupdating || this.owner._loadingPage || this.isAncestorHidden() || !this.bespinControl || this._editor) 
+	    return;
+	if (wm.Bespin.onLoadFinished)
+	    this.initBespinObject();
+	else
+	    this.connect(null, "onBespinLoad", this, "initBespinObject");
     },
     buildToolbar: function() {
 	this.gotoLineButton = new wm.ToolButton({name: "gotoLineButton", 
-						 caption: "L",
+						 caption: "C-L",
+						 hint: "Goto Line",
 						 backgroundColor: "transparent", 
-						 width: "20px", 
+						 width: "30px", 
 						 height: "100%", 
 						 parent: this.toolBar, 
 						 owner: this});
 	this.gotoLineButton.connect(this.gotoLineButton, "onclick", this, function() {
 	    this.promptGotoLine();
 	});
+
+	this.saveButton = new wm.ToolButton({name: "saveButton", 
+						 caption: "C-S",
+						 hint: "Save",
+						 backgroundColor: "transparent", 
+						 width: "30px", 
+						 height: "100%", 
+						 parent: this.toolBar, 
+						 owner: this});
+	this.saveButton.connect(this.saveButton, "onclick", this, function() {
+	    this.onSave();
+	});
+
+	if (this.prettyPrintButton) {
+	    this.pretifyButton = new wm.ToolButton({name: "pretifyButton", 
+						    caption: "C-i",
+						    hint: "Indent/format javascript",
+						    backgroundColor: "transparent", 
+						    width: "30px", 
+						    height: "100%", 
+						    parent: this.toolBar, 
+						    owner: this});
+	    this.pretifyButton.connect(this.pretifyButton, "onclick", this, function() {
+		this.prettyPrint();
+	    });
+	}
+
+	if (this.evalButton) {
+	    this.evalButton = new wm.ToolButton({name: "evalButton", 
+						 caption: "C-E",
+						 hint: "Evaluate Code",
+						 backgroundColor: "transparent", 
+						 width: "30px", 
+						 height: "100%", 
+						 parent: this.toolBar, 
+						 owner: this});
+	    this.evalButton.connect(this.evalButton, "onclick", this, function() {
+		this.jsvalidate();
+	    });
+	}
 
         if (this.useSearchToolbar) {
             this.searchSpacer = new wm.Spacer({name: "searchSpacer", width: "100%", height: "1px", parent: this.toolBar, owner: this});
@@ -124,11 +199,22 @@ dojo.declare("wm.Bespin", wm.Container, {
 							 owner: this});
 	    this.replacePopupButton.domNode.tabIndex = -1;
 	    this.replacePopupButton.connect(this.replacePopupButton, "onclick", this, function() {
-		this.getFindDialog().setShowing(!this.getFindDialog().showing);
-		if (this.searchText.getDataValue())
-		    this.fullSearchText.setDataValue(this.searchText.getDataValue());
+		this.showFindDialog();
+
 	    });
         }
+    },
+    showFindDialog: function() {
+	this.getFindDialog().setShowing(!this.getFindDialog().showing);
+	if (this.searchEditor.getDataValue())
+	    this.fullSearchEditor.setDataValue(this.searchEditor.getDataValue());
+	    this.fullSearchEditor.focus();
+    },
+    prettyPrint: function() {
+	    try {
+		dojo.require("lib.github.beautify");
+	    } catch(e){}
+	    this.setDataValue(js_beautify(this.getDataValue()));
     },
     getFindDialog: function() {
 	if (this.findDialog) return this.findDialog;
@@ -214,41 +300,62 @@ dojo.declare("wm.Bespin", wm.Container, {
     },
 					       
     initBespinObject: function() {
+	if (this._initBespinCalled) return;
+	this._initBespinCalled = true;
+	while(this.bespinControl.domNode.childNodes.length)
+	    dojo.destroy(this.bespinControl.domNode.childNodes[0]);
+    
 	bespin.useBespin(this.bespinControl.domNode,
-			 {stealFocus: true,
-			  syntax:     this.syntax,
-			  settings:   {
-			      tabstop: this.tabSize,
-			      fontsize: this.fontSize,
-			      fontface: this.fontFace
+			 {
+			     syntax:     this.syntax			     
+			 }).then(dojo.hitch(this, function(env) {
+			      this._editor = env.editor;
+			     this._editor.value = this.dataValue;
+
+
+			     /* Important to understand that these settings are not widget specific,
+			      * they will affect ALL bespin editors
+			      */
+			     this._env = env;
+			     this._env.settings.set("tabstop", this.tabSize);
+			     this._env.settings.set("fontsize", this.fontSize);
+			     this._env.settings.set("fontface", this.fontFace);
 			      //theme
 			      //customKeymapping
-			  }
-			 }).then(dojo.hitch(this, function(env) {
-			      console.log("Have ENV!");console.log(env);
-			      this._editor = env.editor;
-			      this._editor.value = this.dataValue;
-			      this._env = env;
 
 			      this._editor.syntax = this.syntax;
 			      this._editor.textChanged.add(dojo.hitch(this, "_change"));
 			      this._editor.selectionChanged.add(dojo.hitch(this, "_selectionChange"));
-			  }));
-	console.log("USE BESPIN DONE");
+			     console.log("CLIENT HEIGHT: " + this._editor.container.clientHeight + "; " + this.toString());
+/*
+			     if (!this._editor.container.clientHeight) {
+				     delete this._initBespinCalled;
+				     console.log("TRY AGAIN");
+				 window.setTimeout(dojo.hitch(this, "initBespinObject"), 200);
+			     }
+			     */
+			 }));
     },
     handleSearchKey: function(text, e) {
 	if (e.keyCode == dojo.keys.TAB) return;
 	if (e.ctrlKey && e.keyCode == 70) {
-	    this.getFindDialog().show();
-	    this.fullSearchText.focus();
+	    this.showFindDialog();
 	    return;
 	}
-	this._editor.setCursor(this._editor.selection.start);
-	this.doFind(text, false);
+	if (e.keyCode == 13 || e.keyCode == 8 || e.keyCode >= 46 && e.keyCode <= 90 || e.keyCode >= 96 && e.keyCode <= 111 || e.keyCode >= 186 && e.keyCode <=222) {
+	    this._editor.setCursor(this._editor.selection.start);
+	    this.doFind(text, e.keyCode == 8 || e.keyCode == 46);
+	}
     },
     handleKeyPress: function(e) {
 	if (e.ctrlKey) {
 	    switch(e.keyCode) {
+	    case 69:
+		if (this.evalButton) {
+		    this.jsvalidate();
+		    dojo.stopEvent(e);
+		}
+		break;
 	    case 70:
 		this.searchEditor.focus();
 		dojo.stopEvent(e);
@@ -257,33 +364,52 @@ dojo.declare("wm.Bespin", wm.Container, {
 		this.promptGotoLine();
 		dojo.stopEvent(e);
 		break;
+	    case 73:
+		if (this.prettyPrintButton) {
+		    this.prettyPrint();
+		    dojo.stopEvent(e);
+		}
+		break;
 	    }
 	} else {
-	    wm.onidle(this, function() {
-		var text = this._editor.value;
-		var cursor = this._editor.selection.end;
-		var line = text.split(/\n/)[cursor.row];
-		var lastcol = cursor.col;
-		for (var i = lastcol; i >= 0 && line[i] != ' '; i--) ;
-		var token = line.substring(i+1,lastcol);
-		console.log("TOKEN:"+token);
-		token = token.replace(this.thisRegex, this.autoCompletionThisPointer +".");
-		try {
-		    var obj = dojo.getObject(token);
-		    var keylist = [];
-		    if (obj) {
-			for (var i in obj) {
-			    if (i[0] != "_") {
-				keylist.push({dataValue: i});
+	    if (this.useAutoCompletion) {
+		wm.onidle(this, function() {
+		    var text = this._editor.value;
+		    var cursor = this._editor.selection.end;
+		    var line = text.split(/\n/)[cursor.row];
+		    var lastcol = cursor.col;
+		    for (var i = lastcol; i >= 0 && line[i] != ' '; i--) ;
+		    var token = line.substring(i+1,lastcol);
+		    token = token.replace(this.autoCompletionThisRegex, this.autoCompletionThisPointer +".");
+		    try {
+			console.log("TOKEN TEST:"+token);
+			var obj = dojo.getObject(token);
+			var remainder = "";
+			while (!obj && token.indexOf(".") != -1) {
+			    remainder = token.substring(token.lastIndexOf(".")+1);
+			    token = token.substring(0,token.lastIndexOf("."));
+			    console.log("TOKEN TEST:"+token);
+			    obj = dojo.getObject(token);
+			}
+			this._findRemainder = remainder;
+			this._findToken = token;
+			var keylist = [];
+			if (obj) {
+			    console.log("OBJ:"+obj.toString());
+			    for (var i in obj) {
+				if (i == "root") console.log(i.indexOf(remainder) + " | " + remainder);
+				if (i[0] != "_" && i.indexOf(remainder) == 0) {
+				    keylist.push({dataValue: i});
+				}
 			    }
 			}
+			this.autoCompletionVar.setData(keylist);
+		    } catch(e) {
+			this.autoCompletionVar.setData([]);
 		    }
-		    this.autoCompletionVar.setData(keylist);
-		} catch(e) {
-		    this.autoCompletionVar.setData([]);
-		}
-		this.autoCompletionList.setDataSet(this.autoCompletionVar);
-	    });
+		    this.autoCompletionList.setDataSet(this.autoCompletionVar);
+		});
+	    }
 	}
     },
     promptGotoLine: function() {
@@ -300,12 +426,16 @@ dojo.declare("wm.Bespin", wm.Container, {
 	    this._editor.syntax = this.syntax;
 	}
     },
+
+    setText: function(inValue){ this.setDataValue(inValue);}, // for EditArea compatibility
     setDataValue: function(inValue) {
 	this.dataValue = inValue;
 	if (this._editor) {
 	    this._editor.value = inValue;
 	}
     },
+
+    getText: function() {return this.getDataValue();}, // for EditArea compatibility
     getDataValue: function() {
 	if (this._editor) 
 	    this.dataValue = this._editor.value;
@@ -391,7 +521,7 @@ dojo.declare("wm.Bespin", wm.Container, {
 	this.onChange(this._editor.value);
     },
     onChange: function(newValue) {
-	console.log("VALUE:"+newValue);
+	//console.log("VALUE:"+newValue);
     },
     _selectedText: "",
     _selectionChange: function() {
@@ -404,6 +534,63 @@ dojo.declare("wm.Bespin", wm.Container, {
     onSelectionChange: function(newSelection) {
 	console.log("SEL:"+newSelection);
     },
+    onSave: function() {
+    },
+    jsvalidate: function() {
+	if (!this.jsvalidateService)
+	    this.jsvalidateService = new wm.JsonRpcService({owner: this, service: "studioService", sync: false});
+	this.jsvalidateService.requestAsync("closurecompile", [this.getDataValue()],
+					    dojo.hitch(this, function(response) {
+						console.log(dojo.fromJson(response));
+						if (response)
+						    responseObj = dojo.fromJson(response);
+						
+						if (!response || !responseObj.errors || !responseObj.errors.length)
+						    app.toastSuccess("No errors found");
+						else {
+						    if (!this.compileErrorDialog) {
+							this.compileErrorDialog = new wm.Dialog({
+							    owner: this,
+							    name: "compileErrorDialog",
+							    useContainerWidget: true,
+							    width: "220px",
+							    height: "350px",
+							    title: "Compiler Results",
+							    corner: "tr",
+							    modal: false});
+							this.compileErrorList = new wm.List({
+							    owner: this,
+							    parent: this.compileErrorDialog.containerWidget,
+							    dataFields: "dataValue, name",
+							    columnWidths: "30px,190",
+							    headerVisible: false,
+							    width: "100%",
+							    height: "100%"});
+							this.compileErrorList.connect(this.compileErrorList, "onselect", this, function() {
+							    this.setLineNumber(this.compileErrorList.selectedItem.getData().dataValue);
+							    var start = this._editor.selection.start;
+							    var end = this._editor.selection.end;
+							    end.col = 2000;
+							    this._editor.selection = {start: start,end:end};
+							});
+							this.compileErrorVar = new wm.Variable({
+							    owner: this,
+							    name: "compileErrorVar",
+							    type: "EntryData"});
+						    }
+						    var data = [];
+						    var errors = responseObj.errors;
+						    for (var i = 0; i < errors.length; i++)
+							data.push({name: errors[i].error,
+								   dataValue: errors[i].lineno});
+						    this.compileErrorVar.setData(data);
+						    this.compileErrorList.setDataSet(this.compileErrorVar);
+						    this.compileErrorDialog.show();
+						}
+					    }),
+					    function(error) {alert(error);});
+
+    },
     _end:0
 });
 
@@ -411,3 +598,8 @@ wm.Bespin.onLoadFinished = false;
 window.onBespinLoad = function () { 
     wm.Bespin.onLoadFinished = true;
 }; 
+
+
+wm.Object.extendSchema(wm.Bespin, {
+    dataValue: {ignore: 1, bindable: 1, group: "editData", order: 3, simpleBindProp: true, type: "String"}
+});
