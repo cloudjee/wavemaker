6f719122c72b7cd734c058801df85f93e6b56cde
Michael Kantor <mkantor@vmware.com>
2010-12-18 04:10:30 +0000

    Move studio.js to Studio.js to be consistent with new I18nPlugin.js
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30861 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/app/resource.js
+++ dev/studio/webapproot/app/resource.js
@@ -18,7 +18,7 @@
 
 dojo.requireLocalization("language", "package");
 var bundlePackage = dojo.i18n.getLocalization("language", "package");
-dojo.requireLocalization("language", "studio");
-var bundleStudio = dojo.i18n.getLocalization("language", "studio");
+dojo.requireLocalization("language", "Studio");
+var bundleStudio = dojo.i18n.getLocalization("language", "Studio");
 dojo.requireLocalization("language", "dialog");
 var bundleDialog = dojo.i18n.getLocalization("language", "dialog");
