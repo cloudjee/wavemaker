4f4b708dafba847d37ba323c302aaf5af87489a4
Michael Kantor <mkantor@vmware.com>
2010-12-21 00:56:47 +0000

    Added new dialog for asking user what to do when a LivePanel is dropped onto project
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30884 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/pages/NewLivePanelDialog/NewLivePanelDialog.js
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2010 WaveMaker Software, Inc.
+ *
+ * This file is part of WaveMaker Studio.
+ *
+ * WaveMaker Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, only.
+ *
+ * WaveMaker Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with WaveMaker Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+dojo.declare("NewLivePanelDialog", wm.Page, {
+    selectedTemplate: null,
+    options: null,
+    start: function() {
+
+	var options = this.options = [{name: "Traditional",
+				       img: "images/GridLiveForm.png"},
+				      {name: "Dialog",
+				       img: "images/GridLiveFormDialog.png"}];
+	
+        var i = 0; 
+
+	for (var i = 0; i < options.length; i++) {
+            var option = options[i];
+            if (i % 3 == 0) {
+                var panel = new wm.Panel({width: "100%", height: "128px", layoutKind: "left-to-right", parent: this.templatesInsertPanel, owner: this, name: "templateRow" + i});
+            }
+            var imgpanel = new wm.Panel({_classes: {domNode: ["SelectableTemplate"]}, 
+					 layoutKind: "top-to-bottom", 
+					 parent: panel, 
+					 owner: this, 
+					 name: "templatepanel_" + i, 
+					 margin: "4", 
+					 border: "1", 
+					 borderColor: "#888888", 
+					 width: "112px", 
+					 height: "120px"});
+	    if (i == 0) 
+		var firstimgpanel = imgpanel;
+            var img = new wm.Picture({width: "100%", 
+				      height: "100px", 
+				      parent: imgpanel, 
+				      owner: this, 
+				      name: "template"+ i});
+            img.domNode.style.backgroundImage = "url(" + option.img + ")";
+            var label = new wm.Label({"width": "100%",
+				      height: "20px",
+				      parent: imgpanel,
+				      owner: this,
+				      name: "templatelabel" + i,
+				      caption: option.name});
+        }
+        dojo.query(".SelectableTemplate", this.templatesInsertPanel.domNode).connect("onclick", this, "templateClicked");
+        dojo.query(".SelectableTemplate", this.templatesInsertPanel.domNode).connect("dblclick", this, "templateDblClicked");
+	this.templateClicked2(firstimgpanel);
+	this.templatesInsertPanel.reflow();
+    },
+/* Copied from NewProjectDialog and not yet updated for use here
+    reset: function() {
+        var projectNames = {};
+	var projects = studio.project.getProjectList();
+	var l={};
+	dojo.forEach(projects, dojo.hitch(this, function(p) {
+	    projectNames[p] = true;
+        }));
+	var newname = wm.findUniqueName("Project", [projectNames]);
+	this.projectName.setDataValue(newname);
+    },
+    */
+    clearSelection: function() {
+        if (this.selectedTemplate) {
+            this.selectedTemplate.setBorderColor("#888888");
+            this.selectedTemplate.setBorder("1");	    
+            dojo.removeClass(this.selectedTemplate.domNode, "Selected");
+            this.selectedTemplate = null;
+        }
+    },
+    templateDblClicked: function(inMouseEvent) {
+        var target = wm.getWidgetByDomNode(inMouseEvent.target);
+        while(target && !dojo.hasClass(target.domNode, "SelectableTemplate"))
+            target = target.parent;
+        if (!target) return;
+        if (target != this.selectedTemplate) {
+	    this.templateClicked2(target);
+        }
+	var selectedName = target.c$[1].caption;
+        this.onOkClick(selectedName);
+    },
+    templateClicked: function(inMouseEvent) {
+        var target = wm.getWidgetByDomNode(inMouseEvent.target);
+        while(target && !dojo.hasClass(target.domNode, "SelectableTemplate"))
+            target = target.parent;
+        if (!target) return;
+        if (target == this.selectedTemplate) return;
+	this.templateClicked2(target);
+    },
+    templateClicked2: function(inTarget) {
+        this.clearSelection();
+        this.selectedTemplate = inTarget;
+        this.selectedTemplate.setBorderColor("#333333");
+        this.selectedTemplate.setBorder("2");
+        dojo.addClass(inTarget.domNode, "Selected");
+    },
+    onCancelClick: function() {
+        this.owner.owner.dismiss();
+    },
+    onOkClick: function(selectedName) {
+        this.owner.owner.dismiss();
+    },
+    _onOkClick: function() {
+	this.onOkClick(this.selectedTemplate.c$[1].caption);
+    },
+  _end: 0
+});
---
+++ dev/studio/webapproot/pages/NewLivePanelDialog/NewLivePanelDialog.widgets.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010 WaveMaker Software, Inc.
+ *
+ * This file is part of WaveMaker Studio.
+ *
+ * WaveMaker Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, only.
+ *
+ * WaveMaker Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with WaveMaker Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+NewLivePanelDialog.widgets = {
+    layoutBox1: ["wm.Layout", {layoutKind: "top-to-bottom", width: "100%", height: "100%"}, {}, {
+        mainPanel: ["wm.Panel", {_classes: {domNode: ["dialogcontainer"]},layoutKind: "top-to-bottom", width: "100%", height: "100%", horizontalAlign: "left", verticalAlign: "top", padding: "15", border: "10", borderColor: "#313743", backgroundColor: "#848c99"},{}, {
+
+            templatesPanel: ["wm.Panel", {width: "100%", height: "100%", layoutKind: "top-to-bottom", horizontalAlign: "left", verticalAlign: "top", autoScroll: true},{}, {
+                templatesPanelLabel: ["wm.Label", {width: "100%", height: "24px", caption: "Select how you want your data to look"}],
+                templatesInsertPanel: ["wm.Panel", {width: "100%", height: "100%", layoutKind: "top-to-bottom", horizontalAlign: "left", verticalAlign: "top", border: "1", borderColor: "#333333"}]
+            }]
+        }],
+            buttonPanel: ["wm.Panel", {_classes: {domNode: ["wmDialogFooter"]}, width: "100%", height: "35px", layoutKind: "left-to-right", horizontalAlign: "right", verticalAlign: "top"},{}, {
+                CancelButton: ["wm.Button", {caption: "Cancel", width: "100px", height: "100%"}, {onclick: "onCancelClick"}],
+                OKButton: ["wm.Button", {caption: "OK", width: "100px", height: "100%"}, {onclick: "_onOkClick"}]
+        }]
+}]
+}
