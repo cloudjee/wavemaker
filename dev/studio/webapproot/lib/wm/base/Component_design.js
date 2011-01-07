b690637fa3c69eceaa062037a858863c8800814b
Michael Kantor <mkantor@vmware.com>
2011-01-07 05:08:26 +0000

    Now makes sure the correct prototype is loaded before trying to write a component (issue causd by themes)
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30949 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/Component_design.js
+++ dev/studio/webapproot/lib/wm/base/Component_design.js
@@ -117,7 +117,9 @@ wm.Component.extend({
 		return !ps || ((ps.writeonly || !(ps.ignore || ps.ignoretmp || ps.readonly)) && !ps.isEvent &&  !ps.componentonly);
 	},
 	writeProps: function() {
-	    this.owner.loadThemePrototypeForClass(this.constructor, this); // make sure the proper prototype is loaded so we correctly write the properties that are different from default
+	    // make sure the proper prototype is loaded so we correctly write the properties that are different from default
+	    if (this.owner.loadThemePrototypeForClass) 
+		this.owner.loadThemePrototypeForClass(this.constructor, this);
 
 		// iterates over all props and checks it's writeable via isWriteableProp
 		// NOTE: previously used listWriteableProps, which was eliminated as unnecessary.
