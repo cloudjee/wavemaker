1aaeea662879a8c9d9b5fb13d3aec10c319d0913
Michael Kantor <mkantor@vmware.com>
2011-01-07 23:47:10 +0000

    Updates everything about how we navigate to services, adds major tab groups and subtabs for each of them with individual service editors; polls each service editor's isDirty() method so that the main tab can be tagged as dirty/clean and handles the closing of these tabs
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30958 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/app/mainNavigation.js
+++ dev/studio/webapproot/app/mainNavigation.js
@@ -20,8 +20,8 @@ dojo.provide("wm.studio.app.mainNavigation");
 Studio.extend({
 	navGotoDesignerClick: function() {
 		this.disableMenuBar(false);
-		this.disableCanvasOnlyMenuItems(false);
-		this.toggleCanvasSourceBtns(true, false);
+	        //this.disableCanvasOnlyMenuItems(false);
+	        //this.toggleCanvasSourceBtns(true, false);
 		this.tabs.setLayer("workspace");
             if (this.page) {
                 if (this._themeDesignerChange) {
@@ -46,8 +46,8 @@ Studio.extend({
 	},
 	navGotoSourceClick: function() {
 		this.disableMenuBar(false);
-		this.disableCanvasOnlyMenuItems(true);
-		this.toggleCanvasSourceBtns(false, true);
+		//this.disableCanvasOnlyMenuItems(true);
+	        //this.toggleCanvasSourceBtns(false, true);
 		this.tabs.setLayer("sourceTab");
 		//this.mainAndResourcePalettes.setLayer("mainPalettes");
 	},
@@ -61,13 +61,18 @@ Studio.extend({
 	},
 	*/
 	navGoToLoginPage: function() {
+	    this.loginDialog.show();
+/*
 		this.disableCanvasOnlyMenuItems(true);
 		this.disableMenuBar(true);
 		if (this.loginPage.page != "Login")
 		  this.loginPage.setPageName("Login");
 		this.tabs.setLayer("loginLayer");
+		*/
 	},
 	navGoToDeploymentPage: function() {
+	    this.deploymentDialog.show();
+/*
 		this.disableMenuBar(false);
 		this.disableCanvasOnlyMenuItems(true);
 		this.toggleCanvasSourceBtns(false, false);
@@ -76,10 +81,13 @@ Studio.extend({
 		this.tabs.setLayer("deploymentLayer");
 		if (this.deploymentPage.page && this.deploymentPage.page instanceof DeploymentPage)
 		  this.deploymentPage.page.setup();
+		  */
 	  },
 	isLoginShowing: function() {
-	    return this.tabs.getLayer().name == "loginTab";
+	    /*return this.tabs.getLayer().name == "loginTab";*/
+	    return this.loginDialog.showing;
 	},
+/*
 	toggleCanvasSourceBtns: function(inCanvasOn, inSourceOn) {
 		dojo.removeClass(this.designerCanvasBtn.domNode, (inCanvasOn ? "Studio-sourceCanvasBtn" : "Studio-designerCanvasBtn"));
 		dojo.addClass(this.designerCanvasBtn.domNode, (inCanvasOn ? "Studio-designerCanvasBtn" : "Studio-sourceCanvasBtn"));
@@ -89,37 +97,123 @@ Studio.extend({
 	    //dojo.addClass(this.designerResourcesBtn.domNode, (inResourcesOn ? "Studio-sourceResourceBtn" : "Studio-designerResourceBtn"));
 
 	},
+	*/
 	disableCanvasSourceBtns: function(inDisable) {
 		this.leftToolbarButtons.setDisabled(inDisable);
 		this.leftToolbarButtons.reflow();
 	},
-	navGotoEditor: function(inEditorName) {
-		this.disableCanvasOnlyMenuItems(true);
-		this.toggleCanvasSourceBtns(false, false);
-		return this.getEditor(inEditorName, true);
+    navGotoEditor: function(inEditorName, parentLayer, layerName, layerCaption) {
+		//this.disableCanvasOnlyMenuItems(true);
+	        //this.toggleCanvasSourceBtns(false, false);
+	return this.getEditor(inEditorName, parentLayer, true, layerName, layerCaption);
 	},
-	getEditor: function(inEditorName, inNav) {
-		var n = wm.decapitalize(inEditorName);
-		var i = this.tabs.indexOfLayerName(n);
+    getEditor: function(inEditorName, parentLayer, inNav, layerName, layerCaption) {
+	var tabs;
+	if (parentLayer) {
+	    parentLayer.show();
+	    parentLayer.activate();
+	    tabs = parentLayer.c$[0];
+	} else {
+	    tabs = this.tabs;
+	}
+
+
+	    //var n = wm.decapitalize(inEditorName);
+	    var i = tabs.indexOfLayerName(layerName);
 		if (i < 0) {
-			return this.addEditor(inEditorName, inNav);
+		    return this.addEditor(inEditorName, parentLayer, inNav, layerName, layerCaption);
 		} else {
-			inNav && this.tabs.setLayerIndex(i);
-			return this.tabs.getLayer(i).getOrderedWidgets()[0];
+		    tabs.layers[i].setShowing(true);
+		    inNav && tabs.setLayerIndex(i);
+		    var widgets = tabs.getLayer(i).getOrderedWidgets();
+		    for (var j = 0; j < widgets.length; j++)
+			if (widgets[j] instanceof wm.PageContainer)
+			    return widgets[j];
 		}
 	},
-	addEditor: function(inEditorName, inNav) {
-		var n = wm.decapitalize(inEditorName);
-		var t = inNav ? this.tabs.addLayer(n) : this.tabs.createLayer(n);
-		var editor = studio.createComponent(n + "Container", "wm.PageContainer", {
+    addEditor: function(inEditorName, parentLayer, inNav, layerName, layerCaption) {
+	    var tabs = parentLayer ? parentLayer.c$[0] : this.tabs; // tablayers should be the first widget within the tab
+
+	    /* Destroy empty layer created automatically for every TabLayers */
+	    if (tabs.layers.length == 1 && tabs.layers[0].c$.length == 0)
+		tabs.layers[0].destroy();
+
+
+	    //var n = inEditorName.replace(/\s/g,"_");
+	    var t = inNav ? tabs.addLayer(layerName) : tabs.createLayer(layerName);
+	    if (layerName)
+		t.setName(layerName);
+	    if (layerCaption)
+		t.setCaption(layerCaption);	    
+	    t.setDestroyable(true);
+	    t.parent.renderBounds(); // in case multiple rows of tabs are needed after changing the caption
+		var editor = studio.createComponent(layerName + "Container", "wm.PageContainer", {
 			parent: t,
 			pageName: inEditorName,
 			width: "100%",
 			height: "100%"
 		});
 		editor.reflowParent();
+	        editor._isNewlyCreated = true;
+
 		return editor;
 	},
+    updateServicesDirtyTabIndicators: function() {
+	var tabs = [this.JavaEditorSubTab, this.databaseSubTab, this.webServiceSubTab, this.securitySubTab];
+	for (var i = 0; i < tabs.length; i++) {
+	    var tab = tabs[i];
+	    var isDirty = false;
+	    for (var j = 0; j < tab.layers.length; j++) {
+		var layer = tab.layers[j];
+		if (layer.c$.length == 0) continue;
+		var page = layer.c$[0].page;
+		if (page && page.getDirty && page.getDirty()) {
+		    isDirty = true;
+		    break;
+		}
+	    }
+	    var caption = tab.parent.caption.replace(/^\<.*?\>\s*/, "");
+	    if (isDirty) caption = "<img class='StudioDirtyIcon'  src='images/blank.gif' /> " + caption;
+	    if (caption != tab.parent.caption)
+		tab.parent.setCaption(caption);
+	}
+    },
+        closeServiceTab: function(inLayer) {
+	    var page = inLayer.c$[0].page;
+	    var f = dojo.hitch(this, function() {
+		var index = inLayer.getIndex();
+		var parent = inLayer.parent;
+		inLayer.destroy();
+		this.updateServicesDirtyTabIndicators();
+	    });
+	    if (page && page.getDirty())
+		app.confirm("You have changes that are not saved; Leave anyway?", false, f);
+	    else
+		f();
+	},
+    closeServiceParentTab: function(inLayer) {
+	var layers = inLayer.c$[0].layers;
+	var unsavedPages = [];
+	for (var i = 0; i < layers.length; i++) {
+	    var page = layers[i].c$[0].page;
+	    if (page.getDirty())
+		unsavedPages.push(page);
+	}
+	if (unsavedPages.length == 0) {
+	    for (var i = 0; i < layers.length; i++) layers[i].destroy();
+	    inLayer.hide();
+	    this.updateServicesDirtyTabIndicators(); // remove unsaved indicator from the tab now that it has no layers
+	} else {
+	    studio.listUnsavedDialog.page.setUnsaved(unsavedPages);
+	    studio.listUnsavedDialog.page.onDone = function() {
+		while (layers.length) layers[0].destroy();
+		inLayer.hide();
+		this.updateServicesDirtyTabIndicators(); // remove unsaved indicator from the tab now that it has no layers
+	    };
+	    studio.listUnsavedDialog.show();
+	}
+
+    },
 	navGotoPaletteModelClick: function() {
 		var n = this.left.getLayerCaption();
 		switch (n) {
@@ -138,6 +232,9 @@ Studio.extend({
 	},
 	navGotoModelTreeClick: function() {
 		this.left.setLayer("leftObjects");
+	},
+	navGotoComponentsTreeClick: function() {
+		this.left.setLayer("componentModel");
 	},
 	navGotoProjectTreeClick: function() {
 		this.left.setLayer("projects");
