ce9428ec187e0fdda370f24e5017fa0d00576ff9
Michael Kantor <mkantor@vmware.com>
2011-01-11 00:38:15 +0000

    Checkin of the Licensing page; originally intended to be a dialog, its now a part of the start dialog
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@30992 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/pages/LicenseDialog/LicenseDialog.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ * This file is part of WaveMaker Studio.
+ *
+ * WaveMaker Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, only.
+ *
+ * WaveMaker Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with WaveMaker Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+dojo.provide("wm.studio.pages.LicenseDialog.LicenseDialog");
+
+dojo.declare("LicenseDialog", wm.Page, {
+	start: function() {
+
+	},
+        onSuccess: function(inSender) {
+	    studio.startPageDialog.page.layer1.activate();
+	    app.toastSuccess("License File Accepted!");
+	},
+        onError: function(inSender, inError) {
+	    this.resultLabel.setCaption(inError);
+	},
+	_end: 0
+});
---
+++ dev/studio/webapproot/pages/LicenseDialog/LicenseDialog.widgets.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2009-2010 WaveMaker Software, Inc.
+ *
+ * This file is part of WaveMaker Studio.
+ *
+ * WaveMaker Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, only.
+ *
+ * WaveMaker Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with WaveMaker Studio.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+LicenseDialog.widgets = {
+	layoutBox1: ["wm.Layout", {_classes: {domNode: ["wm-darksnazzy"]}, height: "100%", width: "100%", layoutKind: "top-to-bottom"}, {}, {
+	    titleBar: ["wm.Panel", {height: "29px", layoutKind: "left-to-right"}, {}, {
+		dialogLabel: ["wm.Label", {_classes: {domNode: ["wm_TextDecoration_Bold", "wm_TextAlign_Center", "wm_Padding_4px", "wm_FontColor_White"]}, caption: "Studio Licensing", border: "0", width: "100%"}, {}, {
+		    format: ["wm.DataFormatter", {}, {}]
+		}]
+	    }],
+
+	    panelouter: ["wm.Panel", {height: "100%", width: "100%", horizontalAlign: "center", verticalAlign: "top", margin: "10"}, {}, {
+		label2: ["wm.Label", {_classes: {domNode: ["wm_TextDecoration_Bold"]}, caption: "You are running an unlicensed version of studio; please upload your license.  Note: Connection to the internet may be required for this to work:", border: "0", margin: "0,5", padding: "0,4", height: "48px", width: "100%", singleLine: false}],
+		licenseUploader: ["wm.DojoFileUpload",   {_classes: {domNode: ["studioButton"]},
+							  width: "100px",
+							      height: "32px",
+							      margin: "2",
+							      useList: false,
+							      buttonCaption: "Select License File",
+							      service: "licensingService",
+							      operation: "uploadLicense"},
+				      {onSuccess: "onSuccess", onError: "onError"}],
+		    resultPanel: ["wm.Panel", {width: "100%", height: "100%", layoutKind: "left-to-right"}, {}, {
+			resultLabel: ["wm.Label", {width: "100%", height: "100%", caption: "", singleLine: false}]
+		    }]
+
+		}]
+	}]
+}
