8e36b2d3bab9eaf4a6822e498c51e6facaaaba77
Michael Kantor <mkantor@vmware.com>
2011-01-19 02:02:56 +0000

    Long delayed, but finally checked in
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31077 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/pages/MenuDesigner/MenuDesigner.js
@@ -0,0 +1,205 @@
+dojo.declare("MenuDesigner", wm.Page, {
+    //defaultItem: null,
+    //defaultItemEnabled: false,
+  start: function() {
+    
+  },
+    setMenu: function(inMenu/*, inDefaultItem, inDefaultItemEnabled*/) {
+	this.editMenu = inMenu;
+/*
+	this.defaultItemEnabled = inDefaultItemEnabled;
+	this.DefaultItemButton.setShowing(inDefaultItemEnabled);
+	if (inDefaultItem) {
+	    this._defaultLabel = inDefaultItem.content;
+	    this._defaultClass = inDefaultItem.iconClass;
+	}
+	*/
+	var s = inMenu.fullStructure;
+	if (!s && inMenu.fullStructureStr)
+	    s = dojo.fromJson(inMenu.fullStructureStr);
+	this.tree.clear();
+	if (s)
+	    this.setMenuBuildTree(s, this.tree.root);
+	this.updateMenu();
+
+	this.menuItemImageList.setOptions(studio.getImageLists().join(","));
+	delete this._defaultLabel;
+	delete this._defaultClass;
+    },
+    setMenuBuildTree: function(structure, parentNode) {
+	for (var i = 0; i < structure.length; i++) {
+	    var content = structure[i].label;
+	    if (structure[i].iconClass)
+		content = '<img src="lib/dojo/dojo/resources/blank.gif" alt="" class="dijitIcon dijitMenuItemIcon ' + structure[i].iconClass + '" dojoattachpoint="iconNode">&nbsp;' + content;
+
+	    var n = new wm.TreeNode(parentNode, {closed: false, content: content, data: {content: structure[i].label,
+											 iconClass: structure[i].iconClass,
+											 imageList: structure[i].imageList}});
+	    if (this._defaultLabel == structure[i].label && this._defaultClass == structure[i].iconClass)
+		n.domNode.style.fontWeight = "bold";
+	    if (structure[i].children) // test for children needed on upgraded projects
+		this.setMenuBuildTree(structure[i].children, n);
+	}
+    },
+  AddButtonClick: function(inSender, inEvent, inTarget) {
+      try {
+          var parent = this.tree.selected || this.tree.root;
+	  var childCount = parent.kids.length;
+          new wm.TreeNode(parent, {closed: false, content: "Menu Item " + childCount, data: {content: "Menu Item " + childCount}});
+          this.updateMenu();
+          
+      } catch(e) {
+          console.error('ERROR IN AddButtonClick: ' + e); 
+      } 
+  },
+  DeleteButtonClick: function(inSender, inEvent, inTarget) {
+      try {
+          var node = this.tree.selected;
+          if (!node || node == this.tree.root) return;
+          node.destroy();
+          this.updateMenu();          
+	  this.tree.selected = null;
+      } catch(e) {
+          console.error('ERROR IN DeleteButtonClick: ' + e); 
+      } 
+  },
+  EditButtonClick: function(inSender, inEvent, inTarget) {
+      try {
+          var node = this.tree.selected;
+          if (!node || node == this.tree.root) return;
+          var data = node.data;
+          this.menuItemName.setDataValue(data.content);
+          this.menuItemImageList.setDataValue(data ? data.imageList : null);
+          this.menuItemIconClass.setDataValue(data ? data.iconClass : null);
+	  this.menuItemImageListBtn.setIconUrl("images/blank.gif");
+	  dojo.query("img", this.menuItemImageListBtn.domNode)[0].className = data.iconClass;
+
+          this.editMenuItemDialog.show();
+          this.menuItemName.focus();
+      } catch(e) {
+          console.error('ERROR IN EditButtonClick: ' + e); 
+      } 
+  },
+/*
+    DefaultButtonClick: function(inSender) {
+        var node = this.tree.selected;
+	if (!node) return;
+	if (this.defaultItem) {
+	    this.defaultItem.domNode.style.fontWeight = "";
+	}
+	this.defaultItem = node;
+	this.defaultItem.domNode.style.fontWeight = "bold";
+    },
+    */
+  menuItemCancelButtonClick: function(inSender, inEvent, inTarget) {
+          this.editMenuItemDialog.hide();
+      this.disconnectImageListPopup();
+  },
+  menuItemOKButtonClick: function(inSender, inEvent, inTarget) {
+      try {
+          this.tree.selected.data = {content: this.menuItemName.getDataValue(),
+                                     imageList: this.menuItemImageList.getDataValue(),
+                                     iconClass: this.menuItemIconClass.getDataValue()};
+	  var content = this.tree.selected.data.content;
+	  if (this.tree.selected.data.iconClass)
+              content = '<img src="lib/dojo/dojo/resources/blank.gif" alt="" class="dijitIcon dijitMenuItemIcon ' + this.tree.selected.data.iconClass + '" dojoattachpoint="iconNode">&nbsp;' + content;
+	  this.tree.selected.setContent(content);
+          this.editMenuItemDialog.hide();
+          this.updateMenu();
+
+	  // Usually the next act is to create a new menu item; should be a sibling of not a child of the item just created (most times)
+	  this.tree.select(this.tree.selected.parent); 
+      } catch(e) {
+          console.error('ERROR IN menuItemOKButtonClick: ' + e); 
+      } 
+      this.disconnectImageListPopup();
+  },
+  onTextEnterKeyPress: function(inSender) {
+      try {
+          this.menuItemOKButtonClick();
+          
+      } catch(e) {
+          console.error('ERROR IN onTextEnterKeyPress: ' + e); 
+      } 
+  },
+  treeDblclick: function(inSender, inNode) {
+      try {
+          this.EditButtonClick();
+          
+      } catch(e) {
+          console.error('ERROR IN treeDblclick: ' + e); 
+      } 
+  },
+    mainOKClick: function() {
+	this.editMenu.setFullStructureStr(dojo.toJson(this.write()), true);
+/*
+	if (this.defaultItemEnabled && this.defaultItem && this.defaultItem.data) 
+	    this.editMenu.setDefaultItem(this.defaultItem.data);
+	    */
+	this.owner.owner.dismiss();
+    },
+    mainCancelClick: function() {
+	this.owner.owner.dismiss();
+    },
+
+    popupImageSelector: function() {
+	var imageList = this.menuItemImageList.getDataValue();
+	if (!imageList) {
+	    imageList = studio.getImageLists()[0];
+            this.menuItemImageList.setDataValue(imageList);
+	}
+	if (imageList)
+	    imageList = studio.page.getValueById(imageList);
+	if (imageList) {
+	    var popupDialog = imageList.getPopupDialog();
+	    this._iconlistDialog = popupDialog;
+	    popupDialog.fixPositionNode = this.menuItemImageListBtn.domNode;
+	    
+	    this._designImageListSelectCon = dojo.connect(imageList._designList, "onselect", this, function() {		    
+		var cssClass = imageList.getImageClass(imageList._designList.getSelectedIndex());
+		this.menuItemIconClass.setDataValue(cssClass);
+		dojo.query("img", this.menuItemImageListBtn.domNode)[0].className = cssClass;
+	    });
+
+	    popupDialog.show();
+	}
+	},
+	disconnectImageListPopup: function() {
+
+	    if (this._designImageListSelectCon) {
+		dojo.disconnect(this._designImageListSelectCon);
+		delete this._designImageListSelectCon;
+	    }
+
+	    this._iconlistDialog.hide();
+	    delete this._iconlistDialog;
+    },
+
+  updateMenu: function() {
+/*
+      this.sample.setFullStructure(this.write());
+      this.sample.renderDojoObj();
+      */
+  },
+  write: function() {
+    var result = [];
+      for (var i =0; i < this.tree.root.kids.length; i++) {
+        result.push(this.writeNode(this.tree.root.kids[i]));
+   }
+
+    return result;
+  },
+  writeNode: function(inNode) {
+   
+      var obj = {label: inNode.data.content,
+	       iconClass: inNode.data.iconClass,
+	       imageList: inNode.data.imageList,
+               children: []};
+   for (var i =0; i < inNode.kids.length; i++) {
+      obj.children.push(this.writeNode(inNode.kids[i]));
+   }
+    return obj;
+  },
+
+  _end: 0
+});
