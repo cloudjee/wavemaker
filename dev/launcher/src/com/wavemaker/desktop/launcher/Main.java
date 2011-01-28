ecd88644f1e5ce223f749e578d3dd0fbeec7cfc0
Craig Conover <cconover@vmware.com>
2011-01-28 19:04:48 +0000

    Fixes http://jira.wavemaker.com/browse/WM-2523?page=com.atlassian.jira.plugin.system.issuetabpanels:all-tabpanel DevBuild Launcher: Debug output from wavemaker.sh - removed debug statement
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31176 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/launcher/src/com/wavemaker/desktop/launcher/Main.java
+++ dev/launcher/src/com/wavemaker/desktop/launcher/Main.java
@@ -197,8 +197,6 @@ public class Main
     public static void main(String[] args) throws InterruptedException,
             URISyntaxException, IOException
     {
-        System.out.println("********************* in Launcher main *************************");
-
         if (GraphicsEnvironment.isHeadless())
         {
             // Run silently
