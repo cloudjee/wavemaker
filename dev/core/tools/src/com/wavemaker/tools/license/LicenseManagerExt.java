4d835e452b5f4fb2fa501273fba8798d0b54e727
Seung Lee <lees@vmware.com>
2011-02-03 20:02:58 +0000

    Improved hardware verification logic to cope with mac address changes better.
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31225 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/core/tools/src/com/wavemaker/tools/license/LicenseManagerExt.java
+++ dev/core/tools/src/com/wavemaker/tools/license/LicenseManagerExt.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.wavemaker.tools.license;
 
 import de.schlichtherle.license.*;
@@ -20,10 +37,7 @@ public class LicenseManagerExt extends LicenseManager {
         if (lc == null) return null;
 
         String macAddr = lc.getMacAddr();
-        String myMacAddr = LicenseUtil.getMacAddr();
-        //System.out.println("-------------- Mac Addr in License = " + macAddr);
-        //System.out.println("-------------- Your Mac Addr = " + myMacAddr);
-        if (!macAddr.equals(myMacAddr)) {
+        if (!LicenseUtil.hardwareMatched(macAddr)) {
             throw new Exception("Mis-matched hardware information in the license");
         }
 
@@ -68,13 +82,6 @@ public class LicenseManagerExt extends LicenseManager {
     public synchronized LicenseNotary getLicenseNotary() {
         return super.getLicenseNotary();
     }
-
-    /*public synchronized LicenseContentExt install(
-            byte[] key,
-            LicenseNotary notary) throws Exception {
-        //return super.install(key, notary);
-        return install(key, notary);
-    }*/
 
     public synchronized LicenseContentExt installExt(File keyFile)
     throws Exception {
--- dev/core/tools/src/com/wavemaker/tools/license/LicenseUtil.java
+++ dev/core/tools/src/com/wavemaker/tools/license/LicenseUtil.java
@@ -1,14 +1,30 @@
+/*
+ *  Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.wavemaker.tools.license;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.text.ParseException;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.BufferedInputStream;
+import java.io.*;
 
 public class LicenseUtil {
 //[nonemac-begin]
@@ -92,4 +108,39 @@ public class LicenseUtil {
         return m.matches();
     }
 //[mac-end]*/
+
+    public static boolean hardwareMatched(String macAddr) {
+
+        String cmd = "";
+        String line = "";
+        boolean matched = false;
+
+        String os = System.getProperty("os.name");
+
+        try
+        {
+            if (os.startsWith("Windows")) {
+                cmd = "ipconfig.exe /all";
+            }
+            else if (os.startsWith("Linux")) {
+                cmd = "ifconfig -a";
+            }
+            else { //Mac OS
+                cmd = "ifconfig";
+            }
+
+            Process conf = Runtime.getRuntime().exec(cmd);
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(conf.getInputStream()));
+            while ((line = buffer.readLine()) != null) {
+                if (line.contains(macAddr)) {
+                    matched = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return matched;
+    }
 }
