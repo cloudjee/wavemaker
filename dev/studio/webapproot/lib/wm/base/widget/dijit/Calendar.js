e87bec4094eb0372b5d66ab6bb496dbea72b5d73
Michael Kantor <mkantor@vmware.com>
2011-02-12 00:02:00 +0000

    Updated to have the useLocalTime property and use the wm.serverTimeOffset for calculating dates in server time
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31351 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/widget/dijit/Calendar.js
+++ dev/studio/webapproot/lib/wm/base/widget/dijit/Calendar.js
@@ -37,6 +37,7 @@ dojo.extend(dijit._Calendar, {
 
 
 dojo.declare("wm.dijit.Calendar", wm.Dijit, {
+        useLocalTime: false,
 	displayDate: "",
 	dijitClass: dijit._Calendar,
 	width: "360px",
@@ -128,7 +129,10 @@ dojo.declare("wm.dijit.Calendar", wm.Dijit, {
 		s.width = s.height = "100%";
 	},
 	setDate: function(inValue) {
-	    this.dijit.set("value", wm.convertValueToDate(inValue));
+	    var d = wm.convertValueToDate(inValue);
+	    if (!this.useLocalTime)
+		d.setHours(d.getHours() + wm.serverTimeOffset + wm.Date.timezoneOffset);
+	    this.dijit.set("value",d);
 	},
 	getDisplayDate: function() {
 		return dojo.date.locale.format(this.dijit.value, { selector: "date"});
@@ -136,26 +140,18 @@ dojo.declare("wm.dijit.Calendar", wm.Dijit, {
 	setDisplayDate: function(inValue) {
 		this.setDate(inValue);
 	},
-	getDateObject: function() {
-	    // dijit._Calendar doesn't have a getValue()
-	    var v = this.dijit.value;
-	    if (v instanceof Date) {
-		v.setUTCMinutes(0);
-		v.setUTCHours(0);
-		v.setUTCSeconds(0);
-		return v;
-	    } else {
-		return null;
+	getDateValue: function() {
+		// dijit._Calendar doesn't have a getValue()
+		var d = this.dijit.value;
+	    if (d instanceof Date) {
+		if (!this.useLocalTime)
+		    d.setHours(-wm.serverTimeOffset - wm.Date.timezoneOffset,0,0);
+		else
+		    d.setHours(1,0,0);
+		return d.getTime();
 	    }
+	    return null;
 	},
-    getDateValue: function() {
-	var d = this.getDateObject();
-	if (d) return dojo.date.locale.format(d, {datePattern: "yyyy-MM-dd", selector: "date"});
-    },
-    getDateLong: function() {
-	var d = this.getDateObject();
-	if (d) return d.getTime();
-    },
 	setDateValue: function(inValue) {
 		this.setDate(inValue);
 	},
@@ -179,8 +175,7 @@ dojo.declare("wm.dijit.Calendar", wm.Dijit, {
                 this.dialog.$.description.setCaption(data.description);
             } else if (this.useDialog && this.dialog.showing)
                 this.dialog.dismiss();
-	    this.valueChanged("dateLong", inDate instanceof Date ? inDate.getTime() : null);
-	    this.valueChanged("dateValue", this.getDateValue());
+	    this.setValue("dateValue", inDate instanceof Date ? inDate.getTime() : null);
 	}
 });
 
@@ -189,7 +184,6 @@ wm.Object.extendSchema(wm.dijit.Calendar, {
         specialDates: { readonly: true, group: "data", order: 0, type: "wm.Variable", isList: true, bindTarget: true},
     useDialog: {group: "data", order: 1, type: "boolean"},
     dateValue: { ignore: 1, bindable: 1, type: "Date" },
-    dateLong:  { ignore: 1, bindable: 1, type: "Date" },
     setDate: {group: "method"},
     getDateValue: {group: "method"},
     setDisplayDate: {group: "method"},
