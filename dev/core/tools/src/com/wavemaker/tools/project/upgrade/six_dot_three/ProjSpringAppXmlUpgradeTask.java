3c6efdc2b41df52897d76d2b8147b6eb9259c8f6
Seung Lee <lees@vmware.com>
2011-02-18 00:48:36 +0000

    Upgrade scripts for 6.3.1 beta
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31431 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/core/tools/src/com/wavemaker/tools/project/upgrade/six_dot_three/ProjSpringAppXmlUpgradeTask.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2008-2010 WaveMaker Software, Inc.
+ *
+ *  This file is part of the WaveMaker Server Runtime.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.wavemaker.tools.project.upgrade.six_dot_three;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.wavemaker.tools.project.Project;
+import com.wavemaker.tools.project.upgrade.UpgradeInfo;
+import com.wavemaker.tools.project.upgrade.UpgradeTask;
+import com.wavemaker.runtime.server.ServerConstants;
+
+import org.apache.commons.io.FileUtils;
+/**
+ * Changes for setting server time offset.
+ * 
+ * @author S Lee
+ */
+public class ProjSpringAppXmlUpgradeTask implements UpgradeTask {
+
+    private String fromStr = "/resources/gzipped/**=fileController";
+
+    private String toStr = fromStr + "\r\n\r\n\t\t\t\t/config.js=fileController";
+
+    private boolean error = false;
+
+    /* (non-Javadoc)
+     * @see com.wavemaker.tools.project.upgrade.UpgradeTask#doUpgrade(com.wavemaker.tools.project.Project, com.wavemaker.tools.project.upgrade.UpgradeInfo)
+     */
+    public void doUpgrade(Project project, UpgradeInfo upgradeInfo) {
+
+        File file = new File(project.getWebInf() + "/project-springapp.xml");
+
+        try {
+            String content = FileUtils.readFileToString(file, ServerConstants.DEFAULT_ENCODING);
+            content = content.replace(fromStr, toStr);
+            FileUtils.writeStringToFile(file, content, ServerConstants.DEFAULT_ENCODING);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            error = true;
+        }
+
+        if (error) {
+            upgradeInfo.addMessage("*** Terminated with error while upgrading project-springapp.xml. " +
+                    "Please check the console message.***");
+        } else {
+            upgradeInfo.addMessage("Upgrading project-springapp.xml completed successfully.");
+        }
+    }
+}
