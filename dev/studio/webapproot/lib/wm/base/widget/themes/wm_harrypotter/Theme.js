bb87fb4d3487964065975a33eaf203dca60c51b1
Michael Kantor <mkantor@vmware.com>
2011-02-22 19:52:54 +0000

    Adds harry potter theme from theme contest
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31448 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/lib/wm/base/widget/themes/wm_harrypotter/Theme.js
@@ -0,0 +1,243 @@
+{
+	"wm.ToggleButton": {
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Button": {
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Layout": {
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Bevel": {
+		"bevelSize": "10", 
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Splitter": {
+		"bevelSize": "10", 
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.AccordionDecorator": {
+		"captionBorder": "1", 
+		"captionBorderColor": "#333333"
+	}, 
+	"wm.AccordionLayers": {
+		"border": "0", 
+		"borderColor": "#333333", 
+		"captionBorder": "2", 
+		"layerBorder": "1", 
+		"captionHeight": "30"
+	}, 
+	"wm.FancyPanel": {
+		"margin": "2", 
+		"border": "0", 
+		"borderColor": "#999999", 
+		"innerBorder": "1", 
+		"labelHeight": "24", 
+		"innerBorderColor": "#333333"
+	}, 
+	"wm.TabLayers": {
+		"layersType": "Tabs", 
+		"margin": "0,2,0,2", 
+		"clientBorder": "1", 
+		"border": "0", 
+		"isMajorContent": 1, 
+		"clientBorderColor": "#999999", 
+		"headerHeight": "28px", 
+		"borderColor": "#999999"
+	}, 
+	"wm.WizardLayers": {
+		"margin": "0,2,0,2", 
+		"border": "0", 
+		"clientBorder": "1", 
+		"isMajorContent": 1, 
+		"clientBorderColor": "#333333"
+	}, 
+	"wm.Layer": {
+		"margin": "2,0,2,0"
+	}, 
+	"wm.Dialog": {
+		"border": "1", 
+		"borderColor": "#333333", 
+		"titlebarBorder": "0,0,1,0", 
+		"titlebarBorderColor": "#333333", 
+		"containerClass": "Document", 
+		"titlebarHeight": "22"
+	}, 
+	"wm.GenericDialog": {
+		"border": "1", 
+		"borderColor": "#333333", 
+		"titlebarBorder": "0,0,1,0", 
+		"titlebarBorderColor": "#333333", 
+		"footerBorder": "1,0,0,0", 
+		"footerBorderColor": "#333333", 
+		"containerClass": "Document"
+	}, 
+	"wm.RichTextDialog": {
+		"border": "1", 
+		"borderColor": "#333333", 
+		"titlebarBorder": "0,0,1,0", 
+		"titlebarBorderColor": "#333333", 
+		"footerBorder": "1,0,0,0", 
+		"footerBorderColor": "#333333", 
+		"containerClass": "Document"
+	}, 
+	"wm.PageDialog": {
+		"border": "1", 
+		"borderColor": "#333333", 
+		"titlebarBorder": "0,0,1,0", 
+		"titlebarBorderColor": "#333333", 
+		"footerBorder": "1,0,0,0", 
+		"footerBorderColor": "#333333", 
+		"noBevel": true, 
+		"containerClass": "Document"
+	}, 
+	"wm.DesignableDialog": {
+		"border": "1", 
+		"borderColor": "#333333", 
+		"titlebarBorder": "0,0,1,0", 
+		"titlebarBorderColor": "#333333", 
+		"footerBorder": "1,0,0,0", 
+		"footerBorderColor": "#333333", 
+		"containerClass": "Document"
+	}, 
+	"wm.DojoMenu": {
+		"padding": "0", 
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.List": {
+		"margin": "0,2,0,2", 
+		"border": "2", 
+		"borderColor": "#333333"
+	}, 
+	"wm.dijit.ProgressBar": {
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.RichText": {
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.RoundedButton": {
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.DataGrid": {
+		"border": "2", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Label": {
+	}, 
+	"wm.Picture": {
+	}, 
+	"wm.Spacer": {
+	}, 
+	"wm.Layers": {
+		"border": "0"
+	}, 
+	"wm.PageContainer": {
+	}, 
+	"wm.Panel": {
+		"borderColor": "#333333", 
+		"border": 0
+	}, 
+	"wm.CheckBoxEditor": {
+	}, 
+	"wm.CurrencyEditor": {
+	}, 
+	"wm.Text": {
+		"border": "0"
+	}, 
+	"wm.SelectMenu": {
+		"border": "0"
+	}, 
+	"wm.dijit.Calendar": {
+		"border": "0", 
+		"borderColor": "#333333"
+	}, 
+	"wm.DojoGrid": {
+		"border": "1", 
+		"borderColor": "#999999"
+	}, 
+	"wm.Control": {
+		"borderColor": "#333333"
+	}, 
+	"wm.BusyButton": {
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.Checkbox": {
+		"border": "0"
+	}, 
+	"wm.ColorPicker": {
+		"border": "0"
+	}, 
+	"wm.Currency": {
+		"border": "0"
+	}, 
+	"wm.Date": {
+		"border": "0"
+	}, 
+	"wm.Number": {
+		"border": "0"
+	}, 
+	"wm.RadioButton": {
+		"border": "0"
+	}, 
+	"wm.Slider": {
+		"border": "0"
+	}, 
+	"wm.LargeTextArea": {
+		"border": "0"
+	}, 
+	"wm.Time": {
+		"border": "0"
+	}, 
+	"wm.WidgetsJsDialog": {
+		"containerClass": "Document", 
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.FileUploadDialog": {
+		"containerClass": "Document", 
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.ColorPickerDialog": {
+		"border": "1", 
+		"borderColor": "#333333"
+	}, 
+	"wm.MainContentPanel": {
+		"border": "0", 
+		"borderColor": "#eb1111"
+	}, 
+	"wm.HeaderContentPanel": {
+		"border": "0", 
+		"borderColor": "#c8c581"
+	}, 
+	"wm.EmphasizedContentPanel": {
+		"border": "2", 
+		"borderColor": "#152e31"
+	}, 
+	"wm.WidgetList": {
+	}, 
+	"wm.ListViewer": {
+	}, 
+	"wm.PopupMenuButton": {
+	}, 
+	"wm.PopupMenu": {
+	}, 
+	"wm.DateTime": {
+	}, 
+	"wm.DojoFileUpload": {
+	}, 
+	"wm.DojoFlashFileUpload": {
+	}, 
+	"wm.JsonStatus": {
+	}
+}
