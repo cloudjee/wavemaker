54ce23db3106db97e372d6636663e0bd57ff0ccd
Michael Kantor <mkantor@vmware.com>
2011-03-23 18:09:13 +0000

    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31668 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/language/nls/schema.js
+++ dev/studio/webapproot/language/nls/schema.js
@@ -1,4 +1,5 @@
 {
+/*
 "wm.Button": {caption: "a big caption",
 	      iconUrl: "URL FOR Icon",
 	      onclick: "onPush"},
@@ -8,6 +9,6 @@
 "wm.Application": {main: "Main Page"},
 "GROUP_OkDokie": "My Special Group",
 "GROUP_display": "Show me what you've got"
-
+*/
 
 }
