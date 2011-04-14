13c65abad48126a221ab0f87235621d2c365682f
Michael Kantor <mkantor@vmware.com>
2011-04-14 19:52:34 +0000

    Themed to look consistent with studio
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31752 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/pages/HandleRequiredJars/HandleRequiredJars.widgets.js
+++ dev/studio/webapproot/pages/HandleRequiredJars/HandleRequiredJars.widgets.js
@@ -18,13 +18,13 @@
 
 HandleRequiredJars.widgets = {
     layoutBox1: ["wm.Layout", {layoutKind: "top-to-bottom", width: "100%", height: "100%", _classes: ["wm-darksnazzy"]}, {}, {
-	dialog: ["wm.Panel", {layoutKind: "top-to-bottom",  width: "100%", height: "100%", border: "10", borderColor: "#424959"}, {}, {
+	dialog: ["wm.Panel", {layoutKind: "top-to-bottom",  width: "100%", height: "100%", border: "10,10,0,10", borderColor: "#424959"}, {}, {
 	    label0: ["wm.Label", {singleLine: false, width: "100%", autoSizeHeight: true, height: "30px", caption: "You will need to install one or more jar files"}],
-	    layers: ["wm.WizardLayers", {width: "100%", height: "100%", clientBorder: "2", clientBorderColor: "#BBB"}, {onCancelClick: "close", onDoneClick: "close"}, {
-		layer1: ["wm.Layer", {caption: "Step 1", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+	    layers: ["wm.WizardLayers", {width: "100%", height: "100%", clientBorder: "0"}, {onCancelClick: "close", onDoneClick: "close"}, {
+		layer1: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, caption: "Step 1", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    html1: ["wm.Html", {width: "100%", height: "100%"}]
 		}],
-		layer2: ["wm.Layer", {caption: "Step 2", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer2: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, caption: "Step 2", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    labelc1: ["wm.Label", {singleLine: false, width: "100%", autoSizeHeight: true, height: "100%", caption: "Now upload the jar into studio"}],
 		    fileUploader: ["wm.DojoFileUpload", {  width: "80px",
 							   height: "40px",
@@ -37,10 +37,10 @@ HandleRequiredJars.widgets = {
 		}],
 
 
-		layer3: ["wm.Layer", {showing: false, caption: "Step 3", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer3: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, showing: false, caption: "Step 3", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    html2: ["wm.Html", {width: "100%", height: "100%"}]
 		}],
-		layer4: ["wm.Layer", {showing: false, caption: "Step 4", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer4: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, showing: false, caption: "Step 4", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    labelc4: ["wm.Label", {singleLine: false, width: "100%", autoSizeHeight: true, height: "100%", caption: "Now upload the jar into studio"}],
 		    fileUploader2: ["wm.DojoFileUpload", {  width: "80px",
 							   height: "40px",
@@ -53,10 +53,10 @@ HandleRequiredJars.widgets = {
 		}],
 
 
-		layer5: ["wm.Layer", {showing: false, caption: "Step 5", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer5: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, showing: false, caption: "Step 5", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    html3: ["wm.Html", {width: "100%", height: "100%"}]
 		}],
-		layer6: ["wm.Layer", {showing: false, caption: "Step 6", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer6: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, showing: false, caption: "Step 6", padding: "5", layoutKind: "left-to-right", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    labelc5: ["wm.Label", {singleLine: false, width: "100%", autoSizeHeight: true, height: "100%", caption: "Now upload the jar into studio"}],
 		    fileUploader3: ["wm.DojoFileUpload", {  width: "80px",
 							   height: "40px",
@@ -69,7 +69,7 @@ HandleRequiredJars.widgets = {
 		}],
 
 
-		layer7: ["wm.Layer", {caption: "End", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
+		layer7: ["wm.Layer", {_classes: {domNode: ["wmGroupBox"]}, caption: "End", padding: "5", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 		    labeld1: ["wm.Label", {singleLine: false, width: "100%", height: "30px", caption: "Now restart studio"}],
 		    labeld2: ["wm.Html", {width: "100%",height: "100%", html: "<ol><li>Restart the studio application (server) that you launched from your computer's file system</li><li>After you've restarted your application, reload studio in your browser</li></ol>"}]
 		}]
