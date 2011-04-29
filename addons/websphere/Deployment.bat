77c6988d5d53ea0f3a21153c8373ae4b98bf55db
Seung Lee <lees@vmware.com>
2011-04-29 22:02:47 +0000

    Complete WebSphere deployment
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@31905 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- addons/websphere/Deployment.bat
+++ addons/websphere/Deployment.bat
@@ -11,18 +11,21 @@ echo passwd		= %7
 echo jsr88 home = %8
 echo ----
 
-@rem set DEBUG_OPT=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+set DEBUG_OPT=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
 
 set path=C:/IBMJava/java/bin;%path%
-set WSPATH=C:/src/dev/studio/webapproot/websphere
+set WSPATH=%8
+set WSPATH=%WSPATH:"=%
+echo WSPATH = %WSPATH%
+
 
 set C_PATH=.;%WSPATH%/classes;%WSPATH%/properties;%WSPATH%/lib/com.ibm.ws.admin.client_7.0.0.jar;%WSPATH%/lib/com.ibm.ws.security.crypto.jar;
 
 set TRACE=-DtraceSettingsFile=TraceSettings.properties -Djava.util.logging.manager=com.ibm.ws.bootstrap.WsLogManager -Djava.util.logging.configureByServer=true
 
-set SOAPURL=-Dcom.ibm.SOAP.ConfigURL=file:%WSPATH%/properties/soap.client.props
-set SSLURL=-Dcom.ibm.SSL.ConfigURL=file:%WSPATH%/properties/ssl.client.props
+set SOAPURL=-Dcom.ibm.SOAP.ConfigURL=file:"%WSPATH%/properties/soap.client.props"
+set SSLURL=-Dcom.ibm.SSL.ConfigURL=file:"%WSPATH%/properties/ssl.client.props"
 
 java -version
 
-java -classpath "%C_PATH%" %SSLURL% %TRACE% %SOAPURL% com.ec2was.clients.DeployEarFile %1 %2 %3 %4 %5 %6 %7 %8
+java -classpath "%C_PATH%" %SSLURL% %DEBUG_OPT% %TRACE% %SOAPURL% com.ec2was.clients.DeployEarFile %1 %2 %3 %4 %5 %6 %7 %8
