0c03a9d3b368efda132f226160b9537e1ba31046
Seung Lee <lees@vmware.com>
2011-07-13 17:40:52 +0000

    At least, changes should not break the existing functionality.
    Checked in for source code management.
    
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@32593 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/services/warpFlowService/src/com/wavemaker/studio/warp/FlowService.java
+++ dev/studio/services/warpFlowService/src/com/wavemaker/studio/warp/FlowService.java
@@ -13,17 +13,8 @@
  */
  
 package com.wavemaker.studio.warp;
-
-import com.wavemaker.runtime.ws.HTTPBindingSupport;
 import com.wavemaker.runtime.service.annotations.HideFromClient;
-//import com.wavemaker.json.JSONArray;
-//import com.wavemaker.json.JSONObject;
-
-import javax.xml.namespace.QName;
-import java.util.Iterator;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.wavemaker.tools.ws.infoteria.FlowSupport;
 
 /**
  * Login service class for Asteria Flow Designer Server
@@ -31,147 +22,29 @@ import org.json.JSONObject;
 public class FlowService {
 
     private LoginService warpLoginService;
+    private FlowSupport flowSupport;
 
     public String listProjects(String host, String port, String userName, String password) throws Exception {
         String sessionId = warpLoginService.logIn(host, port, userName, password);
 
-        return listProjects(host, port, sessionId);
-    }
-
-    /*public JSONArray listProjects(String host, String port) throws Exception {
-        String sessionId = warpLoginService.acquireSessionId();
-
-        return listProjects(host, port, sessionId);
-    }*/
-
-    public String listProjects(String host, String port, String sessionId) throws Exception {
-        String result;
-
-        if (sessionId == null) {
-            throw new Exception("Null session id is passed.");
-        }
-
-        String endPointAddress = "http://" + host + ":" + port + "/flow-rest/project/list.json?sessionid="
-                + sessionId;
-        QName thisQName = new QName(endPointAddress, "asteriaServer");
-
-        String method = "GET";
-
-        result = HTTPBindingSupport.getResponseString(thisQName, thisQName, endPointAddress,
-                HTTPBindingSupport.HTTPRequestMethod.valueOf(method), null, null);
-
-        org.json.JSONObject returnObj = new org.json.JSONObject(result);
-
-        org.json.JSONObject resultObj = (org.json.JSONObject)returnObj.get("result");
-
-        org.json.JSONObject listObj = (org.json.JSONObject)resultObj.get("list");
-
-        org.json.JSONArray projList;
-        if (listObj.isNull("project")) {
-            return "[]";
-        } else {
-            projList = (org.json.JSONArray)listObj.get("project");
-        }
-
-        //return convertOrgArrayToWm(projList);
-        return projList.toString();
+        return flowSupport.listProjects(host, port, sessionId);
     }
 
     public String listFlows(String host, String port, String projectName, String userName, String password)
             throws Exception {
         String sessionId = warpLoginService.logIn(host, port, userName, password);
 
-        return listFlows(host, port, projectName, sessionId);
-    }
-
-    /*public JSONArray listFlows(String host, String port, String projectName) throws Exception {
-        String sessionId = warpLoginService.acquireSessionId();
-
-        return listFlows(host, port, projectName, sessionId);
-    }*/
-
-    public String listFlows(String host, String port, String projectName, String sessionId) throws Exception {
-        String result;
-
-        if (sessionId == null) {
-            throw new Exception("Null session id is passed.");
-        }
-
-        String endPointAddress = "http://" + host + ":" + port + "/flow-rest/flow/list.json?name="
-                + projectName + "&sessionid="+ sessionId;
-        QName thisQName = new QName(endPointAddress, "asteriaServer");
-
-        String method = "GET";
-
-        result = HTTPBindingSupport.getResponseString(thisQName, thisQName, endPointAddress,
-                HTTPBindingSupport.HTTPRequestMethod.valueOf(method), null, null);
-
-        org.json.JSONObject returnObj = new org.json.JSONObject(result);
-
-        //If an error is encountered and the error code is "NotFound", it may be a case that the project has not been
-        //compiled yet.  Simply skip the project in that case.
-        if (!returnObj.isNull("error")) {
-            org.json.JSONObject errorObj = (org.json.JSONObject)returnObj.get("error");
-            String code = (String)errorObj.get("code");
-            if (code.equals("NotFound")) {
-                throw new WarpException(code, code);
-            }
-        }
-
-        org.json.JSONObject resultObj = (org.json.JSONObject)returnObj.get("result");
-
-        org.json.JSONObject listObj = (org.json.JSONObject)resultObj.get("list");
-
-        org.json.JSONArray flowList;
-        if (listObj.isNull("flow")) {
-            return "[]";
-        } else {
-            flowList = (org.json.JSONArray)listObj.get("flow");
-        }
-
-        //return convertOrgArrayToWm(flowList);
-        return flowList.toString();
+        return flowSupport.listFlows(host, port, projectName, sessionId);
     }
 
     public String listAllFlows(String host, String port, String userName, String password) throws Exception {
         String sessionId = warpLoginService.logIn(host, port, userName, password);
 
-        return listAllFlows(host, port, sessionId);
+        return flowSupport.listAllFlows(host, port, sessionId);
     }
 
-    /*public JSONArray listAllFlows(String host, String port) throws Exception {
-        String sessionId = warpLoginService.acquireSessionId();
-
-        return listAllFlows(host, port, sessionId);
-    }*/
-
-    public String listAllFlows(String host, String port, String sessionId) throws Exception {
-        JSONArray rtn = null;
-        JSONArray projList = new JSONArray(listProjects(host, port, sessionId));
-
-        if (projList != null) {
-            rtn = new JSONArray();
-            for (int i=0; i<projList.length(); i++) {
-                JSONObject proj = (JSONObject)projList.get(i);
-                JSONArray flowList;
-                try {
-                    String sFlowList = listFlows(host, port, (String)proj.get("name"), sessionId);
-                    flowList = new JSONArray(sFlowList);
-                } catch (WarpException ex) {
-                    if (ex.getReason().equals("NotFound")) {
-                        continue;
-                    } else {
-                        throw new Exception(ex.getMessage());
-                    }
-                }
-                org.json.JSONObject projFlows = new org.json.JSONObject();
-                projFlows.put("project", proj);
-                projFlows.put("flows", flowList);
-                rtn.put(projFlows);
-            }
-        }
-
-        return rtn.toString();
+    public void importFlows(String host, String port, String userName, String password, String projectName, String sessionId) throws Exception {
+        flowSupport.importFlows(host, port, userName, password, projectName, sessionId);
     }
 
     @HideFromClient
@@ -179,39 +52,8 @@ public class FlowService {
         this.warpLoginService = loginService;
     }
 
-    /*private JSONArray convertOrgArrayToWm(Object list) throws Exception {
-        org.json.JSONArray arr = (org.json.JSONArray)list;
-        JSONArray array = new JSONArray();
-        for (int i=0; i<arr.length(); i++) {
-            Object o = arr.get(i);
-            if (o.getClass().equals(org.json.JSONArray.class)) {
-                o = convertOrgArrayToWm(o);
-            } else if (o.getClass().equals(org.json.JSONObject.class)) {
-                o = convertOrgObjectToWm(o);
-            }
-
-            array.add(o);
-        }
-
-        return array;
+    @HideFromClient
+    public void setFlowSupport(FlowSupport flowSupport) {
+        this.flowSupport = flowSupport;
     }
-
-    private JSONObject convertOrgObjectToWm(Object obj1) throws Exception {
-        org.json.JSONObject obj = (org.json.JSONObject)obj1;
-        Iterator it = obj.keys();
-        JSONObject rtn = new JSONObject();
-        while (it.hasNext()) {
-            String key = (String)it.next();
-            Object o = obj.get(key);
-            if (o.getClass().equals(org.json.JSONArray.class)) {
-                o = convertOrgArrayToWm(o);
-            } else if (o.getClass().equals(org.json.JSONObject.class)) {
-                o = convertOrgObjectToWm(o);
-            }
-
-            rtn.put(key, o);
-        }
-
-        return rtn;
-    }*/
 }
