e7bb3e16ff37dfc5406e05c362fc2ce84a7d50fd
Michael Kantor <mkantor@vmware.com>
2011-07-16 02:35:51 +0000

    Updated security page to use new editors and added helpText property to Database settings
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@32637 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/language/nls/Security.js
+++ dev/studio/webapproot/language/nls/Security.js
@@ -14,10 +14,6 @@
 {
 MENU_SELECT_ONE: "- Select One -",
 MENU_NO_VALUE: "(no value)",
-HELP_DIALOG_HEADER_USERNAME: "Username Field",
-HELP_DIALOG_CONTENT_USERNAME: "Select the user name that the user will type in to log in.  Typically an email address or identifier that is based on the user's personal name.  You can find the value of the logged in user's username in your application by <ol><li>creating a new ServiceVariable</li><li>set its 'service' to 'securityService'</li><li>select the operation 'getUserName'</li></ol>",
-HELP_DIALOG_HEADER_USERID: "User ID Field",
-HELP_DIALOG_CONTENT_USERID: "Select the user ID that uniquely identifies the user in the database.  This is typically a number that the database has assigned to the user's entry in the database.  While you can use an email address, this tends to result in a database that bogs down badly as the size of your database goes up as this value is used by other database tables to identify the user account the data is associated with. You can  find out the ID when your project is running by <ol><li>creating a new ServiceVariable</li><li>set its 'service' to 'securityService'</li><li>select the operation 'getUserId'</li></ol>",
 ERROR_DEMO_NO_USER: "At least one user needs to be added to the list!",
 ERROR_DATABASE_INPUT_REQUIRED: "All required fields must be filled out!",
 ERROR_LDAP_INPUT_REQUIRED: "All required fields must be filled out!",
