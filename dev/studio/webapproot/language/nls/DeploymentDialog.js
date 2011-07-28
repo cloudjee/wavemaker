f68e565f292025cdbe1a17ad0f075fdcc6b23699
Michael Kantor <mkantor@vmware.com>
2011-07-28 00:57:57 +0000

    Dictionary for the new DeploymentDialog
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@32819 bab4cc61-abf4-0310-9601-8b162d8b0fb1

---
+++ dev/studio/webapproot/language/nls/DeploymentDialog.js
@@ -0,0 +1,11 @@
+{
+HSQLDB_DATABASE_NAME_CAPTION: "File name",
+DATABASE_BOX_TITLE: "Database: ${databaseName}",
+CONFIRM_DELETE_HEADER: "Are you sure you want to delete this deployment?",
+CONFIRM_DELETE_NAME: "Name:",
+CONFIRM_DELETE_TARGET: "Target:",
+CONFIRM_DELETE_TYPE: "Type:",
+CONFIRM_DELETE_HOST: "Host:",
+CONFIRM_DELETE_DATABASE: "Database:",
+CONFIRM_UNSAVED_CHANGES: "You have unsaved changes that will be lost; continue?"
+}
