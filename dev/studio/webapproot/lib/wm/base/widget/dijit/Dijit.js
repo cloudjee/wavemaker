6aa3debdcc36c8482bf6315c3f1e004a2a58db43
Michael Kantor <mkantor@vmware.com>
2011-07-31 21:21:30 +0000

    Adds a dojo.require to insure that any dijit class entered will be loaded and available
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@32865 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/webapproot/lib/wm/base/widget/dijit/Dijit.js
+++ dev/studio/webapproot/lib/wm/base/widget/dijit/Dijit.js
@@ -73,6 +73,7 @@ dojo.declare("wm.Dijit", wm.Control, {
 	},
 	initDijit: function(inDomNode) {
 	    if (this.dijitClass) {
+		dojo.require(this.dijitClass);
 		var n = document.createElement('div');
 		inDomNode.appendChild(n);
 		var p = dojo.mixin({srcNodeRef: n}, this.getProperties());
