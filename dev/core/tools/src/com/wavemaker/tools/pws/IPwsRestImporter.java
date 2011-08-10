72516c7023dd957fc4dd51bb5e86b8be98d21a39
Seung Lee <lees@vmware.com>
2011-08-10 21:06:05 +0000

    added javadoc
    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@32962 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/core/tools/src/com/wavemaker/tools/pws/IPwsRestImporter.java
+++ dev/core/tools/src/com/wavemaker/tools/pws/IPwsRestImporter.java
@@ -19,17 +19,64 @@ import com.wavemaker.runtime.pws.PwsLoginInfo;
 import java.util.List;
 
 /**
+ * This interface defines methods to render the list of available web services for a partner and a method to import the selected
+ * service and operations. Partners must provide APIs that generates the list of available services. Partners must also provide relevant
+ * specifications of the service APIs (different from service listing APIs mentioned above). In case of REST style web service,
+ * XSD or WADL must be provided. In case of SOAP style web service, WSDL must be provided.
+ *
  * @author slee
  *
  */
 public interface IPwsRestImporter {
 
+    /**
+     * Generates the list of services for a partner.
+     *
+     * @param loginInfo the object of <code>PwsLoginInfo</code> containing information such as host, posrt, user name and
+     * password
+     * @return a JSON string that contains the list of services
+     * @throws Exception if the request fails
+     */
     String listServices(PwsLoginInfo loginInfo) throws Exception;
 
+    /**
+     * Generates the list of operations for a service.
+     *
+     * @param loginInfo the object of <code>PwsLoginInfo</code> containing information such as host, port number, user name and
+     * password
+     * @param service the service
+     * @return a Json string that contains the list of operations
+     * @throws Exception if the request fails
+     */
     String listOperations(PwsLoginInfo loginInfo, String service) throws Exception;
 
+    /**
+     * Generates the list of all services and operations for a partner.
+     *
+     * @param loginInfo the object of <code>PwsLoginInfo</code> containing information such as host, posrt, user name and
+     * password
+     * @return a Json string that contains the list of all services and operations for a partner
+     * @throws Exception if the request fails
+     */
     String listAllOperations(PwsLoginInfo loginInfo) throws Exception;
 
-    void importOperations(PwsLoginInfo loginInfo, String service, List<String> selectedOperations) throws Exception;
+    
+    /**
+     * <p>Imports partner web services for the selected service and operations. Typically the implemented logic in this method
+     * first gets the service specification (<tt>XSD</tt>, <tt>WSDL</tt> or <tt>WADL</tt>) and then call <code>buildRestService</code>
+     * (if <tt>XSD</tt> ) in <code>WebServiceToolsManager</code> or call <code>importWSDL</code> (if <tt>WSDL</tt> or <tt>WADL</tt>)
+     * in the same class.</p>
+     *
+     * <p>Artifacts generated include files such as Spring bean configuration files, servicedefinition files, java scripts
+     * to store element types, service invocation Java classes, and Java classes for JAXB.</p>
+     *
+     * @param loginInfo the object of <code>PwsLoginInfo</code> containing information such as host, posrt, user name and
+     * password
+     * @param service the service name
+     * @param operations selected operations
+     * @return a Json string that contains the list of all services and operations for a partner
+     * @throws Exception if the request fails
+     */
+    void importOperations(PwsLoginInfo loginInfo, String service, List<String> operations) throws Exception;
 
 }
--- dev/core/tools/src/com/wavemaker/tools/pws/IPwsRestWsdlGenerator.java
+++ dev/core/tools/src/com/wavemaker/tools/pws/IPwsRestWsdlGenerator.java
@@ -29,38 +29,120 @@ import javax.xml.transform.TransformerException;
 import javax.xml.namespace.QName;
 
 /**
+ * The default implementation of this interface is <code>RESTWsdlGenerator</code>. Developers may extend the default
+ * implementation to customize the content of the generated WSDL.
  * @author slee
  *
  */
 public interface IPwsRestWsdlGenerator {
 
+    /**
+     * Sets the http request method (<tt>GET</tt>, <tt>POST</tt>, <tt>PUT</tt>, ...)
+     *
+     * @param httpMethod the http request method
+     */
     void setHttpMethod(String httpMethod);
 
+    /**
+     * Sets the content type of the request body (<tt>application/xml</tt>, <tt>application/x-www-form-urlencoded</tt>, ...)
+     *
+     * @param contentType the content type
+     */
     void setContentType(String contentType);
 
+    /**
+     * Sets the input parameter list
+     *
+     * @param inputParts_list the list of input parameters
+     */
     void setInputParts_list(List<List<RESTInputParam>> inputParts_list);
 
+    /**
+     * Sets the flag indicating that the output is string type
+     *
+     * @param isStringOutput <i>yes</i> - output is a string,  <i>no</i> - output is not a string
+     */
     void setStringOutput(boolean isStringOutput);
 
+    /**
+     * Sets the name of the output type.  Usually, specific response message type name.
+     *
+     * @param outputType the name of the output type
+     */
     void setOutputType(String outputType);
 
+    /**
+     * Sets the list of XML schema strings. Uaually, the list contais only one element unless there are multiple XSD parts
+     * (separated by "--- schema end ---") are involved.
+     *
+     * @param schemas the list of schema strings
+     */
     void setSchemaStrings(List<String> schemas);
 
-    void setSchemaElements(List<Element> schemas);
+    /**
+     * Sets the list of schema elements
+     *
+     * @param elems the list of schema strings
+     */
+    void setSchemaElements(List<Element> elems);
 
+    /**
+     * This is the main mathod in the interface and it generates a WSDL basaed on the populated properties and XSD document.
+     *
+     * @param wsdlFile the file where the generated WSDL is stored
+     * @throws WSDLException if a WSDL related error ossure
+     * @throws IOException if a file IO error occurs
+     * @throws SAXException if SAX parser fails
+     * @throws ParserConfigurationException if a parser configuration error occurs
+     * @throws TransformerException if a XML transformer fails when attempting to write WSDl to the file
+     */
     void write(File wsdlFile) throws WSDLException, IOException,
             SAXException, ParserConfigurationException, TransformerException;
 
+    /**
+     * Sets the service name
+     *
+     * @param serviceName the service name
+     */
     void setServiceName(String serviceName);
 
+    /**
+     * Sets the name space string if used
+     *
+     * @param namespace the name space
+     */
     void setNamespace(String namespace);
 
+    /**
+     * Sets the list of operation names
+     *
+     * @param operationName_list the list of operation names
+     */
     void setOperationNameList(List<String> operationName_list);
 
+    /**
+     * Sets the service url including <i>name=value</i> parameters
+     *
+     * @param parameterizedUrl the parameterized url
+     */
     void setParameterizedUrl(String parameterizedUrl);
 
+    /**
+     * Sets the partner name
+     *
+     * @param partnerName the partner name
+     */
     void setPartnerName(String partnerName);
 
+    /**
+     * Build JDK's <code>QName</code> object. Usually, developers override this method in <code>RESTWsdlGenerator</code> to manipulate
+     * the local part of <code>QName</code> when building <code>QName</code> for request messages or response messages.
+     *
+     * @param namespace the name space. pass <tt>null</tt> if no name space is used
+     * @param operationName the operation name
+     * @param suffix the suffix
+     * @return a <code>Qname</code> object
+     */
     QName buildQName(String namespace, String operationName, String suffix);
 
 }
