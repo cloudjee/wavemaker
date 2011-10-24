b2dfa6251616940061c378726b88d748f60479e5
Michael Kantor <mkantor@vmware.com>
2011-10-24 21:24:12 +0000

    git-svn-id: https://src.springsource.com/svn/wm_svn/eng3/trunk@33802 bab4cc61-abf4-0310-9601-8b162d8b0fb1

--- dev/studio/src/main/webapp/language/nls/es/Start.js
+++ dev/studio/src/main/webapp/language/nls/es/Start.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011 VMware, Inc. All rights reserved.
+ *  Copyright (C) 2011 VMWare, Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -12,6 +12,28 @@
  *  limitations under the License.
  */
 {
+
+layer1: {caption: "Bienvenido"},
+welcomeTitleLabel: {caption: "Bienvenido a WaveMaker Studio"},
+screencast: {caption: "<div class=\"start_icon_label\">VideoTutoriales</div><div style=\"font-weight:normal;font-size:85%\">Videos de WaveMaker en accion</div>"},
+demo: {caption: "<div class=\"start_icon_label\">Explore</div><div style=\"font-weight:normal;font-size:85%\">Ejemplos y Demostraciones</div>"},
+community: {caption: "<div class=\"start_icon_label\">Comunidad</div><div style=\"font-weight:normal;font-size:85%\">Find answers quickly</div>"},
+newProject: {caption: "Crear Nuevo Proyecto..."},
+tutorial: {caption: "Tutoriales"},
+documentation: {caption: "Documentacion"},
+register: {caption: "Registrarse"},
+layer2: {caption: "Proyectos"},
+projectSearch: {placeHolder: "Buscar por nombre del proyecto"},
+openProjectBtn: {caption: "Abrir proyecto"},
+deleteProjectBtn: {caption: "Eliminar proyecto"},
+newProjectBtn: {caption: "Nuevo proyecto"},
+URL_SCREENCAST: "http://www.wavemaker.com/product/screencasts.html",
+URL_DEMO: "http://www.wavemaker.com/product/demos.html",
+URL_COMMUNITY: "http://dev.wavemaker.com/",
+URL_USERGUIDE:"http://dev.wavemaker.com/wiki/bin/wmdoc/",
+URL_TUTORIAL:"http://dev.wavemaker.com/wiki/bin/wmdoc/Tutorials",
+URL_DOCS:"http://dev.wavemaker.com/wiki/bin/wmdoc/",
+
 URL_REGISTER:"http://dev.wavemaker.com/forums/?q=user/register",
 WAIT_OPENNING_PROJECT: "Abrir proyecto; ${projectName}",
 CONFIRM_DELETE: "Seguro que quiere borrar el proyecto ${projectName}"
