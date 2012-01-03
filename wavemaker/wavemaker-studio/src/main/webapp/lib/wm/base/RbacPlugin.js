02707f97b01d082d1d6d17a299890c3476bf7cda
michaelkantor <mkantor@vmware.com>
2012-01-03 09:16:53 -0800

    Adds dojo.requires so that build will work

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/RbacPlugin.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/RbacPlugin.js
@@ -14,6 +14,7 @@
  
 dojo.provide("wm.base.RbacPlugin");
 dojo.require("wm.base.Control");
+dojo.require("wm.base.components.ServiceVariable");
 dojo.require("wm.base.widget.Layers");
 dojo.require("wm.base.Plugin");
 
