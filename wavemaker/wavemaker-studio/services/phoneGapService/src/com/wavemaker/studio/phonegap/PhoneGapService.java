02e0f62e9b3a7ff38a50a650856242197dfac1df
Phil Webb <pwebb@vmware.com>
2012-03-12 14:55:47 -0700

    Added example method to show how the new Resource API can be used

--- wavemaker/wavemaker-studio/services/phoneGapService/src/com/wavemaker/studio/phonegap/PhoneGapService.java
+++ wavemaker/wavemaker-studio/services/phoneGapService/src/com/wavemaker/studio/phonegap/PhoneGapService.java
@@ -4,7 +4,6 @@ package com.wavemaker.studio.phonegap;
 import java.io.IOException;
 
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.io.Resource;
 
 import com.wavemaker.common.util.IOUtils;
 import com.wavemaker.common.util.SystemUtils;
@@ -12,6 +11,10 @@ import com.wavemaker.runtime.service.annotations.ExposeToClient;
 import com.wavemaker.runtime.service.annotations.HideFromClient;
 import com.wavemaker.studio.StudioService;
 import com.wavemaker.studio.StudioServiceWriteWebFileEvent;
+import com.wavemaker.tools.io.File;
+import com.wavemaker.tools.io.Folder;
+import com.wavemaker.tools.io.ResourceFilter;
+import com.wavemaker.tools.io.Resources;
 import com.wavemaker.tools.project.ProjectManager;
 import com.wavemaker.tools.project.StudioFileSystem;
 
@@ -24,7 +27,6 @@ import com.wavemaker.tools.project.StudioFileSystem;
 public class PhoneGapService implements ApplicationListener<StudioServiceWriteWebFileEvent> {
 
     // FIXME this class needs a test case
-    // FIXME rework to use new Resource abstraction
 
     private ProjectManager projectManager;
 
@@ -43,14 +45,54 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
         }
     }
 
+    // FIXME rework to use new Resource abstraction, example here:
+    private void exampleUsingNewFileSystem() {
+
+        // Getting the project root folder
+        Folder projectFolder = this.projectManager.getCurrentProject().getRootFolder();
+
+        // Getting a subfolder
+        Folder phoneFolder = projectFolder.getFolder("/phonegap");
+
+        // Test for file/folder existance
+        boolean phoneGapFolderExists = phoneFolder.exists();
+
+        // Creating a sub-folder
+        phoneFolder.getFolder("assets/www").touch();
+
+        // Reading contents
+        String content = phoneFolder.getFile("example/folder/file.txt").getContent().asString();
+
+        // Writing contents
+        phoneFolder.getFile("example/folder/file.txt").getContent().write("New content");
+
+        // Copy folder
+        phoneFolder.getFile("lib").copyTo(phoneFolder);
+
+        // List Files matching some pattern
+        Resources<File> files = phoneFolder.getFolder("assets/www").list(new ResourceFilter<File>() {
+
+            @Override
+            public boolean include(File resource) {
+                return resource.getName().startsWith("phonegap-") && resource.getName().endsWith(".js");
+            }
+        });
+
+        // Delete files
+        files.delete();
+
+        // Delete folder
+        phoneFolder.delete();
+    }
+
     private void writePhoneGapFile(String path, String data) throws IOException {
         // XCODE
         String projectName = this.projectManager.getCurrentProject().getProjectName();
         String pathPrefix = "phonegap/" + projectName + "/www/";
-        Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
+        org.springframework.core.io.Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
 
         if (phonegap.exists()) {
-            Resource lib = phonegap.createRelative("lib");
+            org.springframework.core.io.Resource lib = phonegap.createRelative("lib");
             this.projectManager.getCurrentProject().writeFile(pathPrefix + (path.startsWith("/") ? path.substring(1) : path), data, false);
         }
 
@@ -59,7 +101,7 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
         phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
 
         if (phonegap.exists()) {
-            Resource lib = phonegap.createRelative("lib");
+            org.springframework.core.io.Resource lib = phonegap.createRelative("lib");
             this.projectManager.getCurrentProject().writeFile(pathPrefix + (path.startsWith("/") ? path.substring(1) : path), data, false);
         }
     }
@@ -70,7 +112,7 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
 
         // XCODE
         String pathPrefix = "phonegap/" + projectName + "/www/";
-        Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
+        org.springframework.core.io.Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
         setupPhonegapFiles(portNumb, projectName, phonegap);
 
         // Eclipse
@@ -79,9 +121,9 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
         setupPhonegapFiles(portNumb, projectName, phonegap);
     }
 
-    private void setupPhonegapFiles(int portNumb, String projectName, Resource phonegap) throws IOException {
+    private void setupPhonegapFiles(int portNumb, String projectName, org.springframework.core.io.Resource phonegap) throws IOException {
         if (phonegap.exists()) {
-            Resource lib = phonegap.createRelative("lib");
+            org.springframework.core.io.Resource lib = phonegap.createRelative("lib");
             if (!lib.exists()) {
                 // Copy studio's lib folder into phonegap's folder
                 IOUtils.copy(new java.io.File(this.fileSystem.getStudioWebAppRoot().getFile(), "lib"), lib.getFile());
@@ -93,7 +135,7 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
                 // Copy the common folder into phonegap's folder
                 IOUtils.copy(this.fileSystem.getCommonDir().getFile(), phonegap.createRelative("common").getFile());
 
-                Resource MainViewerLib = this.projectManager.getCurrentProject().getProjectRoot().createRelative(
+                org.springframework.core.io.Resource MainViewerLib = this.projectManager.getCurrentProject().getProjectRoot().createRelative(
                     "phonegap/" + projectName + "/" + projectName + "/Classes/MainViewController.m");
                 if (MainViewerLib.exists()) {
                     System.out.println("MAINVIEWER: " + MainViewerLib.getFile().getAbsolutePath());
@@ -116,7 +158,7 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
         // XCODE
         String projectName = this.projectManager.getCurrentProject().getProjectName();
         String pathPrefix = "phonegap/" + projectName + "/www/";
-        Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
+        org.springframework.core.io.Resource phonegap = this.projectManager.getCurrentProject().getProjectRoot().createRelative(pathPrefix);
         updatePhonegapFiles(portNumb, isDebug, projectName, phonegap);
 
         // ECLIPSE
@@ -125,10 +167,11 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
         updatePhonegapFiles(portNumb, isDebug, projectName, phonegap);
     }
 
-    private void updatePhonegapFiles(int portNumb, boolean isDebug, String projectName, Resource phonegap) throws IOException {
+    private void updatePhonegapFiles(int portNumb, boolean isDebug, String projectName, org.springframework.core.io.Resource phonegap)
+        throws IOException {
         if (phonegap.exists()) {
-            Resource lib = phonegap.createRelative("lib");
-            Resource indexhtml = phonegap.createRelative("index.html");
+            org.springframework.core.io.Resource lib = phonegap.createRelative("lib");
+            org.springframework.core.io.Resource indexhtml = phonegap.createRelative("index.html");
             String indexhtml_text = IOUtils.read(indexhtml.getFile());
 
             // Update paths in index.html
@@ -149,7 +192,7 @@ public class PhoneGapService implements ApplicationListener<StudioServiceWriteWe
             IOUtils.write(indexhtml.getFile(), indexhtml_text);
 
             // Concatenate boot.js and config.js together and save as config.js
-            Resource configjs = phonegap.createRelative("config.js");
+            org.springframework.core.io.Resource configjs = phonegap.createRelative("config.js");
             String configjs_text = this.studioService.readWebFile("config.js"); // get the project config.js rather than
                                                                                 // the phonegap
             // version which has already been modified; TODO: This is
