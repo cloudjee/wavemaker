976affdb215cd417a91522c0308c833c05519615
Phil Webb <pwebb@vmware.com>
2012-03-12 11:11:49 -0700

    Removed old filesystem project

--- wavemaker/wavemaker-tools-filesystem/src/main/java/com/wavemaker/tools/filesystem/adapter/cloudfoundry/FolderApplicationArchiveAdapter.java
+++
@@ -1,8 +0,0 @@
-
-package com.wavemaker.tools.filesystem.adapter.cloudfoundry;
-
-import org.cloudfoundry.client.lib.archive.ApplicationArchive;
-
-public abstract class FolderApplicationArchiveAdapter implements ApplicationArchive {
-
-}
