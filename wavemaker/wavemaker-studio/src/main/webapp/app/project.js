7f84dad55940f20be5ac3e1382c09346374ca98b
Michael Kantor <mkantor@vmware.com>
2012-03-30 20:55:08 -0700

    Adds alert after download phonegap build; changes prompt for IP address for phonegap build

--- wavemaker/wavemaker-studio/src/main/webapp/app/project.js
+++ wavemaker/wavemaker-studio/src/main/webapp/app/project.js
@@ -728,7 +728,7 @@ dojo.declare("wm.studio.Project", null, {
     getPhonegapBuild: function() {
 	var d = studio.phoneGapService.requestAsync("getDefaultHost", []);
 	d.addCallback(dojo.hitch(this, function(inHost) {
-	    app.prompt("Enter server name that you want the phonegap application to connect to.  eg. '" + studio.project.projectName + ".cloudfoundry.com' or 'www.mycompany.com/" + studio.project.projectName + "'; include port number if needed", inHost, dojo.hitch(this, function(inValue) {
+	    app.prompt("Enter the network name or IP address of the server that this application will connect to. Add a port number to the address if needed (e.g. host.mydomain.com:880)", inHost, dojo.hitch(this, function(inValue) {
 		var serverName, portNumb;
 		inValue = inValue.replace(/^http.?\:\/\//,"");
 		var results = inValue.split(/\:/);
@@ -738,6 +738,7 @@ dojo.declare("wm.studio.Project", null, {
 		var d = studio.phoneGapService.requestAsync("generateBuild", [serverName,portNumb,studio.application.theme]);
 		d.addCallbacks(
 		    dojo.hitch(this, function() {		
+			app.alert("After the zip file has downloaded, login at https://build.phonegap.com and upload the zip file");
 			studio.downloadInIFrame("services/phoneGapService.download?method=downloadBuild");
 			studio.endWait();
 		    }),
