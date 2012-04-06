9b9c378715179d5c082337afde505001a4416dc8
Michael Kantor <mkantor@vmware.com>
2012-04-05 17:50:15 -0700

    Copyright update

--- wavemaker/wavemaker-studio/services/dataService/src/com/wavemaker/studio/data/DataServiceProjectEventListener.java
+++ wavemaker/wavemaker-studio/services/dataService/src/com/wavemaker/studio/data/DataServiceProjectEventListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2011 VMware, Inc. All rights reserved.
+ * Copyright (C) 2007-2012 VMware, Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
