b3cee2ea98c03b9e3dcadf2a5d195bc85e4ae42b
Michael Kantor <mkantor@vmware.com>
2012-04-06 15:53:23 -0700

    No longer assumes it has a dataSet, but instead uses the formField to find its type

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Editors/OneToMany_design.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Editors/OneToMany_design.js
@@ -46,16 +46,28 @@ wm.OneToMany.extend({
 
 	/* Step 2: Match that fieldName to the related object's relationship definitions because we need the name of the related object's relationship
 	 * to delete that relationship.*/
-	def = studio.dataService.requestSync("getRelated", [service, this.dataSet.type.replace(/^.*\./,"")]);
-	def.addCallback(dojo.hitch(this, function(inData) {
-	    for (var i = 0; i < inData.length; i++) {
-		var relationship = inData[i];
-		var columnNames = relationship.columnNames.join(",");
-		if (columnNames == fieldName && tableName == relationship.tableName  && relationship.cardinality.match(/\-one/)) {
-		    this.relationshipName = relationship.name; // setting this is what all of this set_formField method is all about
-		}
+	if (parentForm && parentForm.type) {
+	    var typeDef = wm.typeManager.getType(parentForm.type);
+	    if (typeDef) {
+		var subtypeDef = typeDef.fields[inField];
 	    }
-	}));
+	    if (subtypeDef) {
+		var subtype = subtypeDef.type;
+	    }
+	    if (subtype) {
+		def = studio.dataService.requestSync("getRelated", [service, subtype.replace(/^.*\./,"")]);
+		def.addCallback(dojo.hitch(this, function(inData) {
+		    for (var i = 0; i < inData.length; i++) {
+			var relationship = inData[i];
+			var columnNames = relationship.columnNames.join(",");
+			if (columnNames == fieldName && tableName == relationship.tableName  && relationship.cardinality.match(/\-one/)) {
+			    this.relationshipName = relationship.name; // setting this is what all of this set_formField method is all about
+			}
+		    }
+		}));
+	    }
+	}
+
     }
 });
 
