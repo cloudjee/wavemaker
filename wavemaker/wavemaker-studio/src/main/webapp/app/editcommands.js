83c4060e605ce42e3ac33ad43a3d38e90ec50bcb
Michael Kantor <mkantor@vmware.com>
2012-04-11 14:48:58 -0700

    Fixes bugs in autoCompletion; updates handling of method from 6.4 to 6.5

--- wavemaker/wavemaker-studio/src/main/webapp/app/editcommands.js
+++ wavemaker/wavemaker-studio/src/main/webapp/app/editcommands.js
@@ -90,6 +90,7 @@ Studio.extend({
 	    return this.editArea.promptGotoLine();
 	    */
 	case ".":
+	case " ":
 	    return this.listCompletions();
 	}
     },
@@ -433,8 +434,8 @@ Studio.extend({
 	    for (var i in props) {
 		var p = props[i];
 		var params = "";
-		if (p.group != "operation" && (p.group == "method" || !p.ignore && !p.tmpignore || p.doc)) {
-		    if (p.group == "method") {
+		if (p.group != "operation" && (p.method || !p.ignore && !p.tmpignore || p.doc)) {
+		    if (p.method) {
 			if (!this._autoCompletionRemainder || i.indexOf(this._autoCompletionRemainder) == 0) {
 			    var methodstring = String(object[i]).toString();
 			    var methodstringmatch = methodstring.match(/function\s*\(([^)]*)/);
