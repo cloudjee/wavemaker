a72da9b83c186dd408b565d39f575d223caf2d25
Michael Kantor <mkantor@vmware.com>
2012-04-10 20:16:54 -0700

    Fixes the hiding and reordering of Layers

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Layers/BreadcrumbDecorator.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Layers/BreadcrumbDecorator.js
@@ -22,7 +22,7 @@ dojo.declare("wm.BreadcrumbDecorator", wm.TabsDecorator, {
      * Whenever a layer is set to inactive, we need to hide it if the current layer is left of this layer.
      */
     setLayerActive: function(inLayer, inActive) {
-	var wasShowing = inLayer.showing;
+	var wasShowing = !inLayer._isShowing;
 	this.inherited(arguments);
 	if (inLayer._isDesignLoaded || this.decoree._cupdating ) return;
 
@@ -30,30 +30,30 @@ dojo.declare("wm.BreadcrumbDecorator", wm.TabsDecorator, {
 
 	/* If we're activating a hidden layer, show that layer and move it after the last visisible layer so
 	 * that it appears to be the last breadcrumb/step. */
-	if (inActive && !wasShowing) {
-	    var layerIndex = inLayer.getIndex();
-	    for (var i = layers.length-1; i > layerIndex; i--) {
-		if (layers[i].showing)
-		    break;
-	    }
-	    if (i > layerIndex) {
-		this.decoree.moveLayerIndex(inLayer,i+1);
-	    }
-	    inLayer.show();
-	    if (inLayer.showing) {
-		inLayer.domNode.style.display = "";
-		inLayer.reflow();
-	    }
-	}
-	/* If activating a layer that is already showing, hide any layers that are after this layer */
-	else if (inActive) {
-	    for (var i = inLayer.getIndex()+1; i < layers.length; i++) {
-		if (layers[i].showing) layers[i].setShowing(false);
-	    }
-	}
-
-	/* If hide all layers after the layer that is activated */
 	if (inActive) {
+	    if (!wasShowing) {
+		var layerIndex = inLayer.getIndex();
+		for (var i = layers.length-1; i > layerIndex; i--) {
+		    if (layers[i].showing)
+			break;
+		}
+		if (i > layerIndex && !this.decoree._isDesignLoaded) {
+		    this.decoree.moveLayerIndex(inLayer,i+1);
+		}
+		inLayer.show();
+		if (inLayer.showing) {
+		    inLayer.domNode.style.display = "";
+		    inLayer.reflow();
+		}
+	    }
+	    /* If activating a layer that is already showing, hide any layers that are after this layer */
+	    else {
+		for (var i = inLayer.getIndex()+1; i < layers.length; i++) {
+		    if (layers[i].showing) layers[i].setShowing(false);
+		}
+	    }
+
+	    /* If hide all layers after the layer that is activated */
 	    this.decoree.layerIndex = inLayer.getIndex();
 	    var count = this.decoree.layers.length;
 	    for (var i = inLayer.getIndex() + 1; i < count; i++) {
