857b646410aaaf711814bd35353b266b22a437d6
Edward Callahan <ecallahan@vmware.com>
2012-04-19 12:45:19 -0700

    WM-3960 WM-3966

--- wavemaker-desktop/wavemaker-launcher/src/main/java/com/wavemaker/desktop/launcher/ui/CancelEvent.java
+++ wavemaker-desktop/wavemaker-launcher/src/main/java/com/wavemaker/desktop/launcher/ui/CancelEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 VMWare, Inc. All rights reserved.
+ * Copyright (C) 2012 VMWare, Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
 
 package com.wavemaker.desktop.launcher.ui;
 
