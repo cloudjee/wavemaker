9406ddaa8a977402fae7dc7236f98e88f91a19e9
Michael Kantor <mkantor@vmware.com>
2012-04-20 11:38:18 -0700

    Checks for existence of this.$.binding before using the binding

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Editors/AbstractEditor_design.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/Editors/AbstractEditor_design.js
@@ -114,7 +114,7 @@ wm.AbstractEditor.extend({
 	    props.displayValue.readonly = this.formField;
 	    props.defaultInsert.ignoretmp = !wm.LiveFormBase || !this.isAncestorInstanceOf(wm.LiveFormBase);
 	    props.ignoreParentReadonly.ignoretmp = !hasForm;
-	    props.dataValueBindingEvaluated.ignoretmp = !wm.DataForm || !this.isAncestorInstanceOf(wm.DataForm) || !this.$.binding.wires.dataValue;
+	    props.dataValueBindingEvaluated.ignoretmp = !wm.DataForm || !this.isAncestorInstanceOf(wm.DataForm) || this.$.binding && !this.$.binding.wires.dataValue;
 	    props.minEditorWidth.ignoretmp = this.captionPosition == "top" || this.captionPosition == "bottom" || this.captionSize.match(/px/);
 		return props;
 	},
