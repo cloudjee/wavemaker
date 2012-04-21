23900f65932f0f6f8f007a50882bcf6c59b3fe2d
michaelkantor <mkantor@vmware.com>
2012-04-21 16:12:57 -0700

    Removes support for layerBorder; use clientBorder instead

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/AccordionLayers.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/widget/AccordionLayers.js
@@ -18,13 +18,9 @@ dojo.declare("wm.AccordionLayers", wm.Layers, {
     multiActive: false,
     themeStyleType: "ContentPanel",
     layersType: 'Accordion',
-    layerBorder: 1,
     captionHeight: 26, // used by decorator
     dndTargetName: "",
-    postInit: function() {
-        this.inherited(arguments);
-        this.setLayerBorder(this.layerBorder);
-    },
+
     setCaptionHeight: function(inHeight) {
 	this.captionHeight = inHeight;
         for (var i = 0; i < this.layers.length; i++) {
@@ -37,18 +33,5 @@ dojo.declare("wm.AccordionLayers", wm.Layers, {
         for (var i = 0; i < this.layers.length; i++) {
 	    this.layers[i].setBorderColor(this.borderColor);
 	}
-    },
-    setLayerBorder: function(inBorder) {
-        this.layerBorder = inBorder;
-        for (var i = 0; i < this.layers.length; i++) {
-            this.layers[i].setBorder(this.layerBorder);
-	    this.layers[i].setBorderColor(this.borderColor);
-	}
-    },
-    addLayer: function(inCaption, doNotSelect) {
-        var result = this.inherited(arguments);
-        result.setBorder(this.layerBorder);
-        result.setBorderColor(this.clientBorderColor);
-	return result;
     }
 });
