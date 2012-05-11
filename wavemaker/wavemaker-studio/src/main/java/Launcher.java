6351c0fb00fa41dd066d126c99f1d30663994319
Phillip Webb <pwebb@vmware.com>
2012-05-10 17:22:59 -0700

    Tomcat work

--- wavemaker/wavemaker-studio/src/main/java/Launcher.java
+++ wavemaker/wavemaker-studio/src/main/java/Launcher.java
@@ -1,18 +1,37 @@
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.awt.Desktop;
+import java.net.URI;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.startup.Tomcat;
 
 public class Launcher {
 
     public static void main(String[] args) throws Exception {
+        String tc = "/Users/pwebb/tools/apache-tomcat-7.0.26/";
+        Tomcat tomcat = new Tomcat();
+        StandardHost host = (StandardHost) tomcat.getHost();
+        host.setUnpackWARs(true);
+        tomcat.setPort(8080);
+        tomcat.getHost().setAppBase("/Users/pwebb/projects/wavemaker/code/wavemaker/wavemaker-studio/target");
+        String baseDir = Launcher.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println(baseDir);
+        Context webapp = tomcat.addWebapp("/wavemaker", baseDir);
+        ((StandardContext) webapp).setUnpackWAR(true);
 
-        Class<?> loadClass = ClassLoader.getSystemClassLoader().loadClass("Launcher2");
-        System.out.println(loadClass);
+        webapp.setPrivileged(true);
+        tomcat.addUser("manager", "manager");
+        tomcat.addRole("manager", "manager-script");
+        tomcat.addRole("manager", "manager-gui");
 
-        ClassLoader classLoader = new URLClassLoader(new URL[] {});
-        Thread.currentThread().setContextClassLoader(classLoader);
-        Class<?> launcher2 = classLoader.loadClass("Launcher2");
-        Method method = launcher2.getMethod("main", String[].class);
-        method.invoke(null, new Object[] { args });
+        Context managerWebApp = tomcat.addWebapp("/manager", tc + "webapps/manager");
+        managerWebApp.addSecurityRole("manager");
+        managerWebApp.setPrivileged(true);
+
+        tomcat.start();
+
+        Desktop.getDesktop().browse(URI.create("http://localhost:8080/wavemaker/?debug"));
+        tomcat.getServer().await();
     }
 }
