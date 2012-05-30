5e079d14916e4a432fa67098944e9595d717fae2
Edward Callahan <ecallahan@vmware.com>
2012-05-29 18:13:26 -0700

    updates to spin up app

---
+++ wavemaker-desktop/applications/projects/WM_Spinup_App2/services/userlogDB/src/com/userlogdb/UserlogDBConstants.java
@@ -0,0 +1,15 @@
+
+package com.userlogdb;
+
+
+
+/**
+ *  Query names for service "userlogDB"
+ *  05/29/2012 17:24:59
+ * 
+ */
+public class UserlogDBConstants {
+
+    public final static String getUserloginByIdQueryName = "getUserloginById";
+
+}
