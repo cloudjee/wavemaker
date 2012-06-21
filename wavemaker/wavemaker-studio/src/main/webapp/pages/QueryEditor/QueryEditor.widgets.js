b885bbd30154b3f0a8feab7e5cc410d3e2d7809b
michaelkantor <mkantor@vmware.com>
2012-06-21 08:53:03 -0700

    Adds StudioEditor classes

--- wavemaker/wavemaker-studio/src/main/webapp/pages/QueryEditor/QueryEditor.widgets.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/QueryEditor/QueryEditor.widgets.js
@@ -29,13 +29,13 @@ QueryEditor.widgets = {
 			    panel1: ["wm.Panel", {border: "0", height: "100%", width: "100%", minWidth: "400", verticalAlign: "top", horizontalAlign: "left"}, {}, {
 				    panelTopLayout: ["wm.Panel", {height: "100px", width: "100%", layoutKind: "left-to-right"}, {}, {
 					    queryPropsPanel: ["wm.Panel", {border: "1,0,0,0", borderColor: "#000000", height: "100%", width: "400px", padding: "4", verticalAlign: "top", horizontalAlign: "left"}, {}, {
-						    queryDataModelInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel"]}, layoutKind: "left-to-right", caption: "Data Model", display: "Select", width: "100%", height: "24px", disabled: true,emptyValue: "emptyString"}, {onchange: "queryDataModelInputChange"}, {
+						queryDataModelInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel", "StudioEditor"]}, layoutKind: "left-to-right", caption: "Data Model", display: "Select", width: "100%", height: "24px", disabled: true,emptyValue: "emptyString"}, {onchange: "queryDataModelInputChange"}, {
 							    editor: ["wm._SelectEditor", {}, {}]
 							}],
-						queryNameInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel"]},layoutKind: "left-to-right", caption: "Name", width: "100%", height: "24px",emptyValue: "emptyString"}, {onchange: "queryNameChanged"}, {
+						queryNameInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel", "StudioEditor"]},layoutKind: "left-to-right", caption: "Name", width: "100%", height: "24px",emptyValue: "emptyString"}, {onchange: "queryNameChanged"}, {
 							    editor: ["wm._TextEditor", {changeOnKey: true}, {}]
 							}],
-						    queryCommentInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel"]},layoutKind: "left-to-right", caption: "Comment", width: "100%", height: "24px",emptyValue: "emptyString"}, {onchange: "queryCommentChanged"}, {
+						    queryCommentInput: ["wm.Editor", {_classes: {domNode: ["StudioLabel", "StudioEditor"]},layoutKind: "left-to-right", caption: "Comment", width: "100%", height: "24px",emptyValue: "emptyString"}, {onchange: "queryCommentChanged"}, {
 							    editor: ["wm._TextEditor", {changeOnKey: true}, {}]
 							}]
 						}],
