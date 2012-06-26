47fcd58325536f70d26d48146de37395f8afcb81
Phillip Webb <pwebb@vmware.com>
2012-06-26 16:05:34 -0700

    Deleted experimental deployment code and updated tomcat manager to use Spring rest

--- wavemaker/wavemaker-tools/src/main/java/com/wavemaker/tools/deployment/tomcat/TomcatDeploymentTarget.java
+++ wavemaker/wavemaker-tools/src/main/java/com/wavemaker/tools/deployment/tomcat/TomcatDeploymentTarget.java
@@ -14,25 +14,13 @@
 
 package com.wavemaker.tools.deployment.tomcat;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.core.io.Resource;
-
-import com.wavemaker.common.WMRuntimeException;
 import com.wavemaker.common.util.SystemUtils;
-import com.wavemaker.common.util.Tuple;
-import com.wavemaker.tools.deployment.AppInfo;
 import com.wavemaker.tools.deployment.DeploymentInfo;
 import com.wavemaker.tools.deployment.DeploymentStatusException;
 import com.wavemaker.tools.deployment.DeploymentTarget;
+import com.wavemaker.tools.io.File;
 import com.wavemaker.tools.project.DeploymentManager;
 import com.wavemaker.tools.project.Project;
-import com.wavemaker.tools.util.TomcatServer;
 
 /**
  * @author Simon Toens
@@ -41,85 +29,25 @@ import com.wavemaker.tools.util.TomcatServer;
  */
 public class TomcatDeploymentTarget implements DeploymentTarget {
 
-    public static final String HOST_PROPERTY_NAME = "host";
-
-    public static final String PORT_PROPERTY_NAME = "port";
-
-    public static final String MANAGER_USER_PROPERTY_NAME = "username";
-
-    public static final String MANAGER_PASSWORD_PROPERTY_NAME = "password";
-
-    public static final Map<String, String> CONFIGURABLE_PROPERTIES;
-
-    static {
-        Map<String, String> m = new LinkedHashMap<String, String>(4);
-        m.put(HOST_PROPERTY_NAME, "localhost");
-        m.put(PORT_PROPERTY_NAME, "8080");
-        m.put(MANAGER_USER_PROPERTY_NAME, "manager");
-        m.put(MANAGER_PASSWORD_PROPERTY_NAME, "manager");
-        CONFIGURABLE_PROPERTIES = Collections.unmodifiableMap(m);
-    }
-
     @Override
     public String deploy(Project project, DeploymentInfo deploymentInfo, java.io.File tempWebAppRoot) throws DeploymentStatusException {
-        try {
-            Resource warFile = project.getProjectRoot().createRelative(DeploymentManager.DIST_DIR_DEFAULT + project.getProjectName() + ".war");
-            TomcatServer tomcat = initTomcat(deploymentInfo);
-            return verifyOK(tomcat.deploy(warFile.getFile(), deploymentInfo.getApplicationName()));
-        } catch (IOException e) {
-            throw new WMRuntimeException(e);
-        }
+        File warFile = project.getRootFolder().getFile(DeploymentManager.DIST_DIR_DEFAULT + project.getProjectName() + ".war");
+        TomcatManager tomcat = initTomcat(deploymentInfo);
+        tomcat.deploy(deploymentInfo.getApplicationName(), warFile);
+        return "SUCCESS";
     }
 
     @Override
     public void undeploy(DeploymentInfo deploymentInfo, boolean deleteServices) throws DeploymentStatusException {
-        TomcatServer tomcat = initTomcat(deploymentInfo);
-        verifyOK(tomcat.undeploy(deploymentInfo.getApplicationName()));
+        TomcatManager tomcat = initTomcat(deploymentInfo);
+        tomcat.undeploy(deploymentInfo.getApplicationName());
     }
 
-    private String verifyOK(String output) throws DeploymentStatusException {
-        if (output != null) {
-            if (output.trim().startsWith("OK") || output.trim().equals("SUCCESS")) {
-                return output;
-            }
-        }
-        throw new DeploymentStatusException(output);
-    }
-
-    @Deprecated
-    public List<AppInfo> listDeploymentNames(DeploymentInfo deploymentInfo) {
-        TomcatServer tomcat = initTomcat(deploymentInfo);
-        List<Tuple.Two<String, String>> apps = tomcat.listDeployments();
-        List<AppInfo> rtn = new ArrayList<AppInfo>(apps.size());
-        for (Tuple.Two<String, String> t : apps) {
-            StringBuilder url = new StringBuilder();
-            url.append("http://").append(deploymentInfo.getHost()).append(":").append(deploymentInfo.getPort()).append(t.v1);
-            StringBuilder href = new StringBuilder();
-            href.append("<a href=\"").append(url).append("\" target=\"_blank\">").append(url).append("</a>");
-            rtn.add(new AppInfo(t.v1, href.toString(), t.v2));
-        }
-
-        Collections.sort(rtn);
-
-        return rtn;
-    }
-
-    @Deprecated
-    public Map<String, String> getConfigurableProperties() {
-        return CONFIGURABLE_PROPERTIES;
-    }
-
-    private TomcatServer initTomcat(DeploymentInfo deploymentInfo) {
+    private TomcatManager initTomcat(DeploymentInfo deploymentInfo) {
         if (SystemUtils.isEncrypted(deploymentInfo.getPassword())) {
             deploymentInfo.setPassword(SystemUtils.decrypt(deploymentInfo.getPassword()));
         }
-
-        TomcatServer rtn = new TomcatServer();
-        rtn.setHost(deploymentInfo.getHost());
-        rtn.setPort(deploymentInfo.getPort());
-        rtn.setUsername(deploymentInfo.getUsername());
-        rtn.setPassword(deploymentInfo.getPassword());
-        return rtn;
+        return new TomcatManager(deploymentInfo.getHost(), deploymentInfo.getPort(), deploymentInfo.getUsername(), deploymentInfo.getPassword());
     }
 
     @Override
