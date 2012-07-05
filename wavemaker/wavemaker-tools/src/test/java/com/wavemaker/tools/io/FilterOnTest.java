ab6a7c20309dc178d67c56f01aa90cabd9247f08
Phillip Webb <pwebb@vmware.com>
2012-07-05 12:33:51 -0700

    Support for relative to pattern matching

--- wavemaker/wavemaker-tools/src/test/java/com/wavemaker/tools/io/FilterOnTest.java
+++ wavemaker/wavemaker-tools/src/test/java/com/wavemaker/tools/io/FilterOnTest.java
@@ -4,12 +4,17 @@ package com.wavemaker.tools.io;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.wavemaker.tools.io.FilterOn.PathStyle;
 
 /**
  * Tests for {@link FilterOn}.
@@ -55,7 +60,27 @@ public class FilterOnTest {
     }
 
     @Test
+    public void shouldFilterOnPatternWithFullPathStyle() throws Exception {
+        ResourceFilter filter = FilterOn.antPattern(PathStyle.FULL, "/dojo/**/tests/**");
+        assertThat(filter.match(this.context, folderWithPath("/dojo/folder/tests/file.js")), is(true));
+        assertThat(filter.match(this.context, folderWithPath("/dojo/some/folder/tests/file.js")), is(true));
+        assertThat(filter.match(this.context, folderWithPath("/dojo/some/folder/tests/another/file.js")), is(true));
+    }
+
+    @Test
     public void shouldFilterOnPattern() throws Exception {
+        Folder source = folderWithPath("/x/");
+        given(this.context.getSource()).willReturn(source);
+        ResourceFilter filter = FilterOn.antPattern("dojo/**/tests/**");
+        assertThat(filter.match(this.context, folderWithPath("/x/dojo/folder/tests/file.js")), is(true));
+        assertThat(filter.match(this.context, folderWithPath("/x/dojo/some/folder/tests/file.js")), is(true));
+        assertThat(filter.match(this.context, folderWithPath("/x/dojo/some/folder/tests/another/file.js")), is(true));
+    }
+
+    @Test
+    public void shouldFilterOnPatternWithExtraSlash() throws Exception {
+        Folder source = folderWithPath("/");
+        given(this.context.getSource()).willReturn(source);
         ResourceFilter filter = FilterOn.antPattern("/dojo/**/tests/**");
         assertThat(filter.match(this.context, folderWithPath("/dojo/folder/tests/file.js")), is(true));
         assertThat(filter.match(this.context, folderWithPath("/dojo/some/folder/tests/file.js")), is(true));
@@ -75,10 +100,18 @@ public class FilterOnTest {
         return resourceWithName(Folder.class, name, path);
     }
 
-    private <T extends Resource> T resourceWithName(Class<T> resourceType, String name, String path) {
+    private <T extends Resource> T resourceWithName(Class<T> resourceType, String name, final String path) {
         T resource = mock(resourceType);
         given(resource.getName()).willReturn(name);
         given(resource.toString()).willReturn(path);
+        given(resource.toStringRelativeTo(any(Folder.class))).willAnswer(new Answer<String>() {
+
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                final ResourcePath resourcePath = new ResourcePath().get(path);
+                return resourcePath.toStringRelativeTo(invocation.getArguments()[0].toString());
+            }
+        });
         return resource;
     }
 }
