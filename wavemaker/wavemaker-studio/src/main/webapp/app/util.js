fcc39aed165b18acdf8d40b6cd5fd59a8854a502
Michael Kantor <mkantor@vmware.com>
2012-07-18 14:04:44 -0700

    Merge

--- wavemaker/wavemaker-studio/src/main/webapp/app/util.js
+++ wavemaker/wavemaker-studio/src/main/webapp/app/util.js
@@ -23,64 +23,65 @@ dojo.provide("wm.studio.app.util");
 wm.listOfWidgetType = function(inType, inIgnoreBuiltin, ignoreDialogs) {
     var list = [];
     if (ignoreDialogs) {
-	var owners = [studio.page];
+    var owners = [studio.page];
     } else {
-	var owners = wm.listComponents([studio.application, studio.page], wm.Dialog, false);
-	owners.push(studio.page);
+    var owners = wm.listComponents([studio.application, studio.page], wm.Dialog, false);
+    owners.push(studio.page);
     }
     for (var i = 0; i < owners.length; i++) {
-	var root = owners[i] instanceof wm.Page ? owners[i].root : owners[i];
-	if (root) {
-		wm.forEachWidget(root, function(w) {
-		    if (wm.isInstanceType(w, inType) && !(wm.isInstanceType(w.owner, wm.Composite))) {
-			list.push(w);
-		    }
-		}, inIgnoreBuiltin);
-		list.sort(function(a, b) {
-			return a.name < b.name ? -1 : a.name == b.name ? 0 : 1;
-		});
-	}
+    var root = owners[i] instanceof wm.Page ? owners[i].root : owners[i];
+    if (root) {
+        wm.forEachWidget(root, function(w) {
+            if (wm.isInstanceType(w, inType) && !(wm.isInstanceType(w.owner, wm.Composite))) {
+            list.push(w);
+            }
+        }, inIgnoreBuiltin);
+        list.sort(function(a, b) {
+            return a.name < b.name ? -1 : a.name == b.name ? 0 : 1;
+        });
+    }
     }
 
-	return list;
+    return list;
 }
 
 // produce a name ordered list of widgets of a given set of classes
 wm.listOfWidgetTypes = function(inTypes) {
 /*
-	var list = [];
+    var list = [];
     dojo.forEach(inTypes, function(t) {
-		list = list.concat(wm.listOfWidgetType(t));
-	});
-	*/
+        list = list.concat(wm.listOfWidgetType(t));
+    });
+    */
     var list = wm.listOfWidgetType(inTypes); // change made possible by adding support in isInstanceType for a class or array of classes
-	return list;
+    return list;
 };
 
 // list components (not widgets) within owners by types
 wm.listMatchingComponents = function(inOwners, inMatch, inRecurse,inNoPageContainerRecurse) {
-	var l=[];
-	if (inMatch)
-		dojo.forEach(inOwners, function(o) {
-			if (!o)
-				return;
-			wm.forEachProperty(o.components, function(c) {
-				if (inMatch(c))
-					l.push(c);
-			        if (inRecurse && (!inNoPageContainerRecurse || c instanceof wm.PageContainer == false))
-				    l = l.concat(wm.listMatchingComponents([c], inMatch, inRecurse, inNoPageContainerRecurse));
-			});
-		});
-	return l;
+    var l=[];
+    if (inMatch)
+        dojo.forEach(inOwners, function(o) {
+            if (!o)
+                return;
+            wm.forEachProperty(o.components, function(c) {
+                if (inMatch(c))
+                    l.push(c);
+                    if (inRecurse && (inRecurse == "nonvisual" && c instanceof wm.Control === false ||  inRecurse != "nonvisual" || c instanceof wm.PageContainer) && (!inNoPageContainerRecurse || c instanceof wm.PageContainer == false)) {
+                        l = l.concat(wm.listMatchingComponents([c], inMatch, inRecurse, inNoPageContainerRecurse));
+                    }
+            });
+        });
+    return l;
 }
 
 wm.listMatchingComponentIds = function(inOwners, inMatch, inRecurse, inNoPageContainerRecurse) {
     var d = [], l = wm.listMatchingComponents.call(dojo.global, inOwners, inMatch, inRecurse,inNoPageContainerRecurse);
-	dojo.forEach(l, function(c) {
-		d.push(c.getId());
-	});
-	d.sort();
-	return d;
+    dojo.forEach(l, function(c) {
+        d.push(c.getId());
+    });
+    d.sort();
+    return d;
 }
 
 // Actually returns the page in the page container, not the pagecontainer itself
@@ -88,90 +89,90 @@ wm.listAllPageContainers = function(inOwners) {
     var pagecontainers = wm.listComponents(inOwners, wm.PageContainer, false);
     var pages = [];
     for (var i = 0; i < pagecontainers.length; i++)  {
-	if (pagecontainers[i].page)
-	    pages.push(pagecontainers[i].page);
+    if (pagecontainers[i].page)
+        pages.push(pagecontainers[i].page);
     }
 
     var pagedialogs = wm.listComponents(inOwners, wm.PageDialog, false);
     for (var i = 0; i < pagedialogs.length; i++) {
-	if (pagedialogs[i].page)
-	    pages.push(pagedialogs[i].page);
+    if (pagedialogs[i].page)
+        pages.push(pagedialogs[i].page);
     }
 
     if (pages.length) {
-	var subpages = wm.listAllPageContainers(pages);
-	return pages.concat(subpages);
+    var subpages = wm.listAllPageContainers(pages);
+    return pages.concat(subpages);
     } else {
-	return [];
+    return [];
     }
 }
 
 // produce list of components (not widgets) of a given class name within a set of owners
 // if inStrict is true, only classes with declaredClass == inClass.declaredClass are returned
 wm.listComponents = function(inOwners, inClass, inStrict) {
-	isRequestedType = function(inComp) {
-		if (inComp) {
-			if (inStrict)
-				return (inClass.prototype.declaredClass == inComp.declaredClass);
-			else
-			    return wm.isInstanceType(inComp, inClass);
-		}
-	};
-	return wm.listMatchingComponents(inOwners, isRequestedType);
+    isRequestedType = function(inComp) {
+        if (inComp) {
+            if (inStrict)
+                return (inClass.prototype.declaredClass == inComp.declaredClass);
+            else
+                return wm.isInstanceType(inComp, inClass);
+        }
+    };
+    return wm.listMatchingComponents(inOwners, isRequestedType);
 }
 
 // produce list of component *ids* (not widgets) of a given class within a set of owners
 wm.listComponentIds = function(inOwners, inClass, inStrict) {
-	var d = [], l = wm.listComponents.call(dojo.global, inOwners, inClass, inStrict);
-	dojo.forEach(l, function(c) {
-		d.push(c.getId());
-	});
-	d.sort();
-	return d;
+    var d = [], l = wm.listComponents.call(dojo.global, inOwners, inClass, inStrict);
+    dojo.forEach(l, function(c) {
+        d.push(c.getId());
+    });
+    d.sort();
+    return d;
 }
 
 // open a url and present user with a dialog if popups are blocked.
 wm.openUrl = function(inUrl, inTitle, inWindowName, inWindowOptions) {
-	var w = window.open(inUrl, inWindowName, inWindowOptions);
+    var w = window.open(inUrl, inWindowName, inWindowOptions);
         if (dojo.isChrome) {
-	    wm.job(inWindowName, 1, function() {
-		if (w.closed) wm.openUrlDialog(inUrl,inTitle,inWindowName+1);
-		if (w.document && w.document.body && w.outerWidth == 0)
-		    wm.openUrlDialog(inUrl,inTitle,inWindowName+1);
-	    });
-	} else if (!w) {
-	    wm.openUrlDialog(inUrl,inTitle,inWindowName);
-	}
+        wm.job(inWindowName, 1, function() {
+        if (w.closed) wm.openUrlDialog(inUrl,inTitle,inWindowName+1);
+        if (w.document && w.document.body && w.outerWidth == 0)
+            wm.openUrlDialog(inUrl,inTitle,inWindowName+1);
+        });
+    } else if (!w) {
+        wm.openUrlDialog(inUrl,inTitle,inWindowName);
+    }
     return w;
 }
 wm.openUrlDialog = function(inUrl, inTitle, inWindowName) {
     var d = wm.openUrl.dialog;
     if (!d) {
-	d = wm.openUrl.dialog = new wm.Dialog({_classes: {domNode: ["studiodialog"]},
-					       title: studio.getDictionaryItem("POPUP_BLOCKER_TITLE"),
-					       owner: studio, 
-					       width: 320, 
-					       height: 110,
-					       modal: false,
-					       useContainerWidget: true});
-	var container = d.containerWidget;
-	new wm.Label({parent: container,
-		      width: "100%",
-		      height: "100%",
-		      owner: d,
-		      caption: studio.getDictionaryItem("POPUP_BLOCKER_MESSAGE"), 
-		      singleLine: false,
-		      align: "center"});
-	new wm.Label({
-	    parent: container,
-	    owner: d,
-	    caption: studio.getDictionaryItem("POPUP_BLOCKER_LAUNCH_CAPTION"),
-	    width: "100%",
-	    align: "center"
-	});
+    d = wm.openUrl.dialog = new wm.Dialog({_classes: {domNode: ["studiodialog"]},
+                           title: studio.getDictionaryItem("POPUP_BLOCKER_TITLE"),
+                           owner: studio, 
+                           width: 320, 
+                           height: 110,
+                           modal: false,
+                           useContainerWidget: true});
+    var container = d.containerWidget;
+    new wm.Label({parent: container,
+              width: "100%",
+              height: "100%",
+              owner: d,
+              caption: studio.getDictionaryItem("POPUP_BLOCKER_MESSAGE"), 
+              singleLine: false,
+              align: "center"});
+    new wm.Label({
+        parent: container,
+        owner: d,
+        caption: studio.getDictionaryItem("POPUP_BLOCKER_LAUNCH_CAPTION"),
+        width: "100%",
+        align: "center"
+    });
     }
     var link = d.containerWidget.c$[1];
-	link.setLink(inUrl);
+    link.setLink(inUrl);
     dojo.query("a", link.domNode)[0].target = (inWindowName || "_blank");
     d.show();
 }
@@ -180,8 +181,8 @@ wm.makeLoginHtml =  function(fileTemplate, inProjectName) {
     var macros = ['PROJECT'], data=[inProjectName], t = fileTemplate;
     // change template to index
     for (var i=0, m, d; (m=macros[i]); i++){
-	d=data[i] || "";
-	t = t.replace(new RegExp(['{%', m, '}'].join(''), 'g'), d);
+    d=data[i] || "";
+    t = t.replace(new RegExp(['{%', m, '}'].join(''), 'g'), d);
         t = t.replace(/\wavemakerNode\"\}/, "wavemakerNode\", theme:\"" + studio.application.theme + "\", name:\"" + studio.project.projectName + "\"}");
     }
     return t;
@@ -230,3 +231,4 @@ wm.getEditorClassName = function(type){
 	type = 'Text';
     return 'wm.'+type;
 }
+    
