21d7b78582d961e28c9c8c17d6330efb7532871d
Michael Kantor <mkantor@vmware.com>
2012-08-07 11:39:41 -0700

    WM-4070: Adds slightly more informative messages during database import

--- wavemaker/wavemaker-studio/src/main/webapp/language/nls/ImportCloudFoundryDatabase.js
+++ wavemaker/wavemaker-studio/src/main/webapp/language/nls/ImportCloudFoundryDatabase.js
@@ -1,5 +1,10 @@
 {
-WAIT_IMPORTING: "Importing Database...",
+WAIT_IMPORTING: "Importing Database... (may take a few minutes)",
+WAIT_IMPORTING_BINDING: "Binding Database... (may take a few minutes)",
+WAIT_IMPORTING_BINDING_RESTART1: "Waiting for CloudFoundry Binding to complete... (may take a few minutes)",
+WAIT_IMPORTING_BINDING_RESTART2: "Bound database app spinning up... (may take a few minutes)",
+WAIT_IMPORTING_GENERATING: "Generating Models... (may take a few minutes)",
+
 ALERT_CONNECTION_SUCCESS: "Connection successful",
 ALERT_CONNECTION_FAILED: "Connection failed: ${error}"
 }
--- wavemaker/wavemaker-studio/src/main/webapp/pages/ImportCloudFoundryDatabase/ImportCloudFoundryDatabase.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/ImportCloudFoundryDatabase/ImportCloudFoundryDatabase.js
@@ -19,128 +19,98 @@ dojo.declare("ImportCloudFoundryDatabase", wm.Page, {
     i18n: true,
     
     start: function() {
-	this.update();
+        this.update();
     },
     onShow: function() {
-	this.update();
+        this.update();
     },
     update: function(inImportDataModel) {
-	this.cloudFoundryService.requestAsync("listDatabaseServices", ["",""],
-					      dojo.hitch(this, function(inResult) {
-						  this.populateCloudFoundryAppList(inResult);
-					      }),
-					      dojo.hitch(this, function(inError) {
-						  app.alert(inError);
-					      }));	
+        this.cloudFoundryService.requestAsync("listDatabaseServices", ["", ""], dojo.hitch(this, function(inResult) {
+            this.populateCloudFoundryAppList(inResult);
+        }), dojo.hitch(this, function(inError) {
+            app.alert(inError);
+        }));
     },
-	cancelBtnClick: function(inSender) {
-	    this.owner.owner.hide();
-	},
+    cancelBtnClick: function(inSender) {
+        this.owner.owner.hide();
+    },
     populateCloudFoundryAppList: function(inResult) {
-	this.serviceListVar.setData(inResult);
+        this.serviceListVar.setData(inResult);
     },
-        selectedServiceChange: function(inSender) {
-	    var serviceName = this._serviceName = this.serviceList.selectedItem.getValue("name");
-	    var type =  this.serviceList.selectedItem.getValue("vendor");
-	    this.packageInput.setDataValue(DEFAULT_PACKAGE_ROOT + serviceName);
-	    setupWidgetsForDatabaseType(type, 
-					"", 
-					this.dumpEditor,
-					this.dumpEditor,
-					this.dumpEditor,
-					this.dumpEditor,
-					this.tablePatternInput,
-					this.schemaPatternInput,
-					this.dumpEditor,
-					this.dumpEditor,
-					this.dumpEditor,
-					this.dumpEditor);
-	},
-	importBtnClick: function(inSender) {
-	    this.dataModelName = null;
-	    var serviceName = this.serviceList.selectedItem.getValue("name");
-	    var type =  this.serviceList.selectedItem.getValue("vendor");
+    selectedServiceChange: function(inSender) {
+        var serviceName = this._serviceName = this.serviceList.selectedItem.getValue("name");
+        var type = this.serviceList.selectedItem.getValue("vendor");
+        this.packageInput.setDataValue(DEFAULT_PACKAGE_ROOT + serviceName);
+        setupWidgetsForDatabaseType(type, "", this.dumpEditor, this.dumpEditor, this.dumpEditor, this.dumpEditor, this.tablePatternInput, this.schemaPatternInput, this.dumpEditor, this.dumpEditor, this.dumpEditor, this.dumpEditor);
+    },
+    importBtnClick: function(inSender) {
+        this.dataModelName = null;
+        var serviceName = this.serviceList.selectedItem.getValue("name");
+        var type =  this.serviceList.selectedItem.getValue("vendor");
 
-	    studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING")+" Testing Service");
-	    this.cloudFoundryService.requestAsync("isServiceBound", ["", "", serviceName, ""], 
-					    dojo.hitch(this, function(isBound) {
-						if (isBound) {
-						    this.doImport(serviceName, type);
-						} else {
-						    this.doBind(serviceName, type);
-						}
-					    }),
-					    function(inError) {
-						app.alert(inError.toString());
-						studio.endWait();
-					    }
-					   );
-	},
+        studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING"));
+        this.cloudFoundryService.requestAsync("isServiceBound", ["", "", serviceName, ""], 
+                        dojo.hitch(this, function(isBound) {
+                        if (isBound) {
+                            this.doImport(serviceName, type);
+                        } else {
+                            this.doBind(serviceName, type);
+                        }
+                        }),
+                        function(inError) {
+                        app.alert(inError.toString());
+                        studio.endWait();
+                        }
+                       );
+    },
     doBind: function(serviceName, type) {
-	studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING") + " Binding Service");
-	// this will restart the studio server
-	this.cloudFoundryService.requestAsync("bindService", ["", "", serviceName, ""],
-					      dojo.hitch(this, function() {
-						    this.waitForStudioToRestart(serviceName, type);
-					      }),
-						function(inError) {
-						    app.alert(inError);
-						    studio.endWait();
-						}
-					       );
+        studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING_BINDING"));
+        // this will restart the studio server
+        this.cloudFoundryService.requestAsync("bindService", ["", "", serviceName, ""], dojo.hitch(this, function() {
+            this.waitForStudioToRestart(serviceName, type);
+        }), function(inError) {
+            app.alert(inError);
+            studio.endWait();
+        });
     },
     waitForStudioToRestart: function(serviceName, type) {
-	window.setTimeout(dojo.hitch(this, function() {
-	    this.waitForStudioToRestart2(serviceName, type);
-	}), 5000);
+        studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING_BINDING_RESTART1"));
+        window.setTimeout(dojo.hitch(this, function() {
+            this.waitForStudioToRestart2(serviceName, type);
+        }), 5000);
     },
     waitForStudioToRestart2: function(serviceName, type) {
-	studio.studioService.requestAsync("getOpenProject", [], 
-					  dojo.hitch(this, function(inResult) {
-					      // if a project is still open, the server hasn't yet restarted
-					      this.waitForStudioToRestart(serviceName, type);
-					  }),
-					  dojo.hitch(this, function(inError) {
-					      // server has restarted, and is now responding
-					      if (inError.message.match(/No open project/i)) {
-						  this.waitForStudioToRestart3(serviceName, type);
-					      } else {
-						  // threw an error, the server has definitely restarted, but isn't yet online
-						  this.waitForStudioToRestart(serviceName, type);
-					      }
-					  })
-					 );
+        studio.studioService.requestAsync("getOpenProject", [], dojo.hitch(this, function(inResult) {
+            // if a project is still open, the server hasn't yet restarted
+            this.waitForStudioToRestart(serviceName, type);
+        }), dojo.hitch(this, function(inError) {
+            // server has restarted, and is now responding
+            if (inError.message.match(/No open project/i)) {
+                this.waitForStudioToRestart3(serviceName, type);
+            } else {
+                // threw an error, the server has definitely restarted, but isn't yet online
+                studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING_BINDING_RESTART2"));
+                this.waitForStudioToRestart(serviceName, type);
+            }
+        }));
     },
     waitForStudioToRestart3: function(serviceName, type) {
-	studio.studioService.requestAsync("openProject", [studio.project.projectName], dojo.hitch(this, function() {
-	    this.doImport(serviceName, type);
-	}));
+        studio.studioService.requestAsync("openProject", [studio.project.projectName], dojo.hitch(this, function() {
+            this.doImport(serviceName, type);
+        }));
     },
     doImport: function(serviceName, type) {
-	studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING"));
-	studio.dataService.requestAsync("cfImportDatabase",
-					[serviceName,
-					 this.packageInput.getDataValue(),
-					 this.tablePatternInput.getDataValue(),
-					 this.schemaPatternInput.getDataValue(),
-					 "",
-					 "",
-					 this.revengNamingStrategyInput.getDataValue(),
-					 false,
-					 ""],
-					dojo.hitch(this, "_importResult"), 
-					    function(inError) {
-						app.alert(inError.toString());
-						studio.endWait();
-					    }
-				       );
+        studio.beginWait(this.getDictionaryItem("WAIT_IMPORTING_GENERATING"));
+        studio.dataService.requestAsync("cfImportDatabase", [serviceName, this.packageInput.getDataValue(), this.tablePatternInput.getDataValue(), this.schemaPatternInput.getDataValue(), "", "", this.revengNamingStrategyInput.getDataValue(), false, ""], dojo.hitch(this, "_importResult"), function(inError) {
+            app.alert(inError.toString());
+            studio.endWait();
+        });
     },
     _importResult: function() {
-	studio.endWait();
-	this.dataModelName = this._serviceName;
-	studio.updateServices();
-	this.owner.owner.dismiss("Import");
+        studio.endWait();
+        this.dataModelName = this._serviceName;
+        studio.updateServices();
+        this.owner.owner.dismiss("Import");
     },
-
   _end: 0
 });
