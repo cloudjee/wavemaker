3cf663a871e0da4f4c097d014a5393f1d72a289a
Edward Callahan <ecallahan@vmware.com>
2012-08-16 21:35:18 -0700

    SpinUp - editor focus

--- wavemaker-desktop/applications/projects/WM_Spinup_App2/webapproot/pages/Main/Main.js
+++ wavemaker-desktop/applications/projects/WM_Spinup_App2/webapproot/pages/Main/Main.js
@@ -1,6 +1,7 @@
 dojo.declare("Main", wm.Page, {
 	"preferredDevice": "desktop",
     start: function() {            
+        this.UserName.focus();
     },
 
     DownloadButton1Click: function(inSender) {
@@ -30,6 +31,7 @@ dojo.declare("Main", wm.Page, {
       this.error_warning_spacer_1.setShowing(true);
       this.error_warning_spacer_2.setShowing(true);
       this.loginLayer.activate();
+      this.Password.focus();
     },
   progressBarTimerTimerFire: function(inSender) {
     var max = 1000 * 120;
