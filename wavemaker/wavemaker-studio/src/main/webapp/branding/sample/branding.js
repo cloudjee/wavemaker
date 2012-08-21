e970b2dadd804269c7d54ae5d33ad69af0e598f2
Michael Kantor <mkantor@vmware.com>
2012-08-21 11:51:44 -0700

    WM-4372

---
+++ wavemaker/wavemaker-studio/src/main/webapp/branding/sample/branding.js
@@ -0,0 +1,17 @@
+{
+   Start: {
+       welcomeTitleLabel: {
+	   caption: {
+	       "en": "Welcome to Sample Co Studio EN", 
+	       "en-us": "Welcome to Sample Co Studio EN-US",
+	       "ja": "Welcome to Sample Co Studio Japanese Version",
+	       "default": "Welcome to Sample Co Studio DEFAULT Version"
+	   }
+       },
+	copyright: {
+	    html: {
+	    "default": "Copyright &copy; 2011-2012 Sample Co, Studio Version: "
+	    }
+	}
+   }
+}
