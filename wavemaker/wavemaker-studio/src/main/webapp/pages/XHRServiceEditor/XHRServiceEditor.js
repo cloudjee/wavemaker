f484c9df74b72febebefb99c7b234780aea513d4
Michael Kantor <mkantor@vmware.com>
2012-08-22 15:06:57 -0700

    Code Reformat

--- wavemaker/wavemaker-studio/src/main/webapp/pages/XHRServiceEditor/XHRServiceEditor.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/XHRServiceEditor/XHRServiceEditor.js
@@ -11,151 +11,168 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 
 dojo.declare("XHRServiceEditor", wm.Page, {
-    i18n:true,
+    i18n: true,
     start: function() {
-	this.reset();
+        this.reset();
     },
     reset: function() {
-	var options = ["String", "Number", "Date", "Boolean"];
-	var types = wm.typeManager.getPublicTypes();
-	wm.forEachProperty(types, function(typeDef, typeName) {
-	    options.push(typeName);
-	});
-	this.inputsGrid.setColumnComboBoxOptions("type", options);
-	this.serviceResponseType.setDataValue("New Type");
+        var options = ["String", "Number", "Date", "Boolean"];
+        var types = wm.typeManager.getPublicTypes();
+        wm.forEachProperty(types, function(typeDef, typeName) {
+            options.push(typeName);
+        });
+        this.inputsGrid.setColumnComboBoxOptions("type", options);
+        this.serviceResponseType.setDataValue("New Type");
     },
     setService: function(inService) {
-	this.editService = inService;
-	this.serviceName.setDataValue(inService.name);
-	this.serviceUrl.setDataValue(inService.url);
-	this.serviceContentType.setDataValue(inService.contentType);
-	this.serviceRequestType.setDataValue(inService.requestType);
-	this.useProxyCheckbox.setChecked(inService.useProxy);
-	this.serviceResponseType.setDataValue(inService.returnType);
+        this.editService = inService;
+        this.serviceName.setDataValue(inService.name);
+        this.serviceUrl.setDataValue(inService.url);
+        this.serviceContentType.setDataValue(inService.contentType);
+        this.serviceRequestType.setDataValue(inService.requestType);
+        this.useProxyCheckbox.setChecked(inService.useProxy);
+        this.serviceResponseType.setDataValue(inService.returnType);
 
-	var inputs = inService.parameters;
-	var inputsArray = [];
-	wm.forEachProperty(inputs, function(item, name) {
-	    inputsArray.push({name: name, type: item.type, transmitType: item.transmitType});
-	});
-	this.inputsVar.setData(inputsArray);
+        var inputs = inService.parameters;
+        var inputsArray = [];
+        wm.forEachProperty(inputs, function(item, name) {
+            inputsArray.push({
+                name: name,
+                type: item.type,
+                transmitType: item.transmitType
+            });
+        });
+        this.inputsVar.setData(inputsArray);
 
-	var headers = inService.headers
-	var headersArray = [];
-	wm.forEachProperty(headers, function(value, name) {
-	    headersArray.push({name: name, dataValue: value});
-	});
-	this.fixedHeadersVar.setData(headersArray);
-	this.buttonBar.show();
+        var headers = inService.headers
+        var headersArray = [];
+        wm.forEachProperty(headers, function(value, name) {
+            headersArray.push({
+                name: name,
+                dataValue: value
+            });
+        });
+        this.fixedHeadersVar.setData(headersArray);
+        this.buttonBar.show();
     },
-   okButtonClick: function(inSender) {
-       var oldName = this.editService ? this.editService.name : "";
+    okButtonClick: function(inSender) {
+        var oldName = this.editService ? this.editService.name : "";
 
-      var serviceName = this.serviceName.getDataValue();
-      if (!serviceName) {
-          app.toastError("Please enter a service name");
-          return;
-      }   
-      
-       serviceName  = (this.editService && this.editService.name == serviceName) ? serviceName : studio.application.getUniqueName(serviceName);
-      
-      var url = this.serviceUrl.getDataValue();
-      if (!url) {
-          app.toastError("Please enter a url");
-          return;
-      }         
-      
-      var returnType;
-       var jsonText;
-      if (this.serviceResponseType.getDataValue() != "New Type") {
-          returnType = this.serviceResponseType.getDataValue();
-      } else {
-          jsonText = this.returnedJSONEditor.getDataValue();
-          if (!jsonText) {
-              app.toastError("Please enter a sample JSON result or set the returned type to String");
-              return;
-          }
-      }
-      
-       var c;
-       if (this.editService) {
-	   c = this.editService;
-	   if (c.name != serviceName) {
-	       c.set_name(serviceName,true);
-	   }
-       } else {
-	   c = new wm.XhrDefinition({owner: studio.application, name: serviceName});
-       }
-      c.url = url;
-      c.contentType = this.serviceContentType.getDataValue();
-      c.useProxy = this.useProxyCheckbox.getChecked();
-      c.type = this.serviceRequestType.getDataValue();
+        var serviceName = this.serviceName.getDataValue();
+        if (!serviceName) {
+            app.toastError("Please enter a service name");
+            return;
+        }
 
-       var headers = {};
-       var headersArray =  this.fixedHeadersVar.getData();
-       for (var i = 0; headersArray && i < headersArray.length; i++) {
-	   var item = headersArray[i];
-	   headers[item.name] = item.dataValue;
-       }
+        serviceName = (this.editService && this.editService.name == serviceName) ? serviceName : studio.application.getUniqueName(serviceName);
 
-       c.headers = headers;
+        var url = this.serviceUrl.getDataValue();
+        if (!url) {
+            app.toastError("Please enter a url");
+            return;
+        }
 
-       var inputs = {};
-       var inputsArray =  this.inputsVar.getData();
-       for (var i = 0; inputsArray && i < inputsArray.length; i++) {
-	   var item = inputsArray[i];
-	   inputs[item.name] = {transmitType: item.transmitType ? 1 : 0,
-				type: item.type || "string"};
-       }
+        var returnType;
+        var jsonText;
+        if (this.serviceResponseType.getDataValue() != "New Type") {
+            returnType = this.serviceResponseType.getDataValue();
+        } else {
+            jsonText = this.returnedJSONEditor.getDataValue();
+            if (!jsonText) {
+                app.toastError("Please enter a sample JSON result or set the returned type to String");
+                return;
+            }
+        }
 
-       c.parameters = inputs;
-      
-      if (returnType) {
-          c.returnType = returnType;
-      } else {
-          var typeDef = new wm.TypeDefinition({name: serviceName + "Response", owner: studio.application});
-	  c.returnType = typeDef.name;
-          wm.TypeDefinition.prototype.getTypeDefinitionDialog();
-          studio.TypeDefinitionGeneratorDialog.page.setTypeDefinition(typeDef);
-          studio.TypeDefinitionGeneratorDialog.page.generateButtonClick(null, null, null, jsonText, typeDef.name);
-      }
-       c.requestType = this.serviceRequestType.getDataValue();
-       if (this.editService) {
-	   this.inherited(arguments);
-	   if (oldName != this.editService.name) {
-	       wm.XhrService.prototype.removeOperation(oldName);
-	   }
-       }
+        var c;
+        if (this.editService) {
+            c = this.editService;
+            if (c.name != serviceName) {
+                c.set_name(serviceName, true);
+            }
+        } else {
+            c = new wm.XhrDefinition({
+                owner: studio.application,
+                name: serviceName
+            });
+        }
+        c.url = url;
+        c.contentType = this.serviceContentType.getDataValue();
+        c.useProxy = this.useProxyCheckbox.getChecked();
+        c.type = this.serviceRequestType.getDataValue();
 
-       c.initType();
-       dojo.publish("wmtypes-changed");
-       studio.refreshServiceTree();
-       app.toastSuccess("You can access the new service using a wm.ServiceVariable, set the service to \"xhrService\" and the operation to \"" + c.name + "\"");
-       this.dismiss();
-   },
+        var headers = {};
+        var headersArray = this.fixedHeadersVar.getData();
+        for (var i = 0; headersArray && i < headersArray.length; i++) {
+            var item = headersArray[i];
+            headers[item.name] = item.dataValue;
+        }
+
+        c.headers = headers;
+
+        var inputs = {};
+        var inputsArray = this.inputsVar.getData();
+        for (var i = 0; inputsArray && i < inputsArray.length; i++) {
+            var item = inputsArray[i];
+            inputs[item.name] = {
+                transmitType: item.transmitType ? 1 : 0,
+                type: item.type || "string"
+            };
+        }
+
+        c.parameters = inputs;
+
+        if (returnType) {
+            c.returnType = returnType;
+        } else {
+            var typeDef = new wm.TypeDefinition({
+                name: serviceName + "Response",
+                owner: studio.application
+            });
+            c.returnType = typeDef.name;
+            wm.TypeDefinition.prototype.getTypeDefinitionDialog();
+            studio.TypeDefinitionGeneratorDialog.page.setTypeDefinition(typeDef);
+            studio.TypeDefinitionGeneratorDialog.page.generateButtonClick(null, null, null, jsonText, typeDef.name);
+        }
+        c.requestType = this.serviceRequestType.getDataValue();
+        if (this.editService) {
+            this.inherited(arguments);
+            if (oldName != this.editService.name) {
+                wm.XhrService.prototype.removeOperation(oldName);
+            }
+        }
+
+        c.initType();
+        dojo.publish("wmtypes-changed");
+        studio.refreshServiceTree();
+        app.toastSuccess("You can access the new service using a wm.ServiceVariable, set the service to \"xhrService\" and the operation to \"" + c.name + "\"");
+        this.dismiss();
+    },
     addInputRow: function() {
-	this.inputsGrid.addRow({type: "String",
-				transmitType: "parameter",
-				name: ""});
+        this.inputsGrid.addRow({
+            type: "String",
+            transmitType: "parameter",
+            name: ""
+        });
 
     },
     cancelClick: function() {
-	this.dismiss();
+        this.dismiss();
     },
     dismiss: function() {
-	if (wm.isInstanceType(this.owner.owner, ImportWebService)) {
-	    this.owner.owner.dismiss();
-	} else {
-	    var tabs = this.owner.parent.parent;
-	    this.owner.parent.destroy();
-	    if (tabs.layers.length == 0) {
-		tabs.parent.hide();
-		studio.workspace.activate();
-	    }
-	}
+        if (wm.isInstanceType(this.owner.owner, ImportWebService)) {
+            this.owner.owner.dismiss();
+        } else {
+            var tabs = this.owner.parent.parent;
+            this.owner.parent.destroy();
+            if (tabs.layers.length == 0) {
+                tabs.parent.hide();
+                studio.workspace.activate();
+            }
+        }
     },
     _end: 0
 });
