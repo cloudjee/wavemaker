a71fab86bf93f9f54bb2324430df90658ec38029
Michael Kantor <mkantor@vmware.com>
2012-09-11 10:59:18 -0700

    isDesignLoaded should always return false if there is no owner: no owner means no way to determine if its designed or not, so presumably not designed.

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/Component.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/Component.js
@@ -256,7 +256,8 @@ dojo.declare("wm.Component", wm.Object, {
         },
     isDesignLoaded: function() {
         if (this._isDesignLoaded) return true;
-        if (!window.studio) return false;
+
+        if (!window.studio || !this.owner) return false;
         if (this.owner == studio.application || this.owner == studio._application) return true; // must come before test for !studio.page
         if (!studio.page) return false;
         if (!this.owner) return false;
