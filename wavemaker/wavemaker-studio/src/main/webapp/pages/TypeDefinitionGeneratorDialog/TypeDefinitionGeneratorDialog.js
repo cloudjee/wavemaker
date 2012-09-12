3e94f83f5fef3b67b3a0d552342f33199dc21fcb
Michael Kantor <mkantor@vmware.com>
2012-09-12 14:06:00 -0700

    Fixes bug where setTypeDefinition didn't clear the moreDefs

--- wavemaker/wavemaker-studio/src/main/webapp/pages/TypeDefinitionGeneratorDialog/TypeDefinitionGeneratorDialog.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/TypeDefinitionGeneratorDialog/TypeDefinitionGeneratorDialog.js
@@ -6,6 +6,7 @@ dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
     },
     setTypeDefinition: function(inTypeDef) {
 	this.typeDef = inTypeDef;
+    this.moreDefs = {};
     },
     /* optionalInText is used by other pages wanting to use this generator as a service */
     generateButtonClick: function(inSender, e, inSender2, optionalInText, optionalInName) {
@@ -18,11 +19,11 @@ dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
 	if (this.typeDef.name != typeName) {
 	    this.typeDef.setName(studio.application.getUniqueName(typeName));
 	}
-      this.parseObj(obj, this.typeDef);      
+      this.parseObj(obj, this.typeDef);
 /*
       this.variable1.setType(this.typeDef.name);
       this.variable1.setData(obj);
-      this.html1.setHtml("<pre>"+this.typeDef.write("") + "\nvariable1.getData() Generates:\n" + 
+      this.html1.setHtml("<pre>"+this.typeDef.write("") + "\nvariable1.getData() Generates:\n" +
                                  dojo.toJson(this.variable1.getData(),true) +"</pre>");
 				 */
 	studio.componentModel.activate();
@@ -36,7 +37,7 @@ dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
 	this.owner.owner.hide();
     },
 /*
-    write: function(c, indent) {        
+    write: function(c, indent) {
         return indent +  c.name + ": [" + c.declaredClass + ",{" + this.writeProps(c) + "}, {}, {" + this.writeChildren(c.$, indent + "\t") + "}]";
     },
     writeProps: function(c) {
@@ -47,9 +48,9 @@ dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
         }
     },
     writeChildren: function(children, indent) {
-        var result = "";        
-        wm.forEachProperty(children, dojo.hitch(this, function(c) { 
-            
+        var result = "";
+        wm.forEachProperty(children, dojo.hitch(this, function(c) {
+
             result += "\n" + indent + this.write(c, "") + ",";}));
             if (!wm.isEmpty(children)) result += "\n";
         return result;
@@ -65,15 +66,15 @@ dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
          } else if (typeof val == "number") {
              type = isList ? "NumberData" : "Number";
          } else if (typeof val == "boolean") {
-             type = isList ? "BooleanData" : "Boolean";             
+             type = isList ? "BooleanData" : "Boolean";
          } else if (val === null || val === undefined) {
-             type = isList ? "StringData" : "String";             
+             type = isList ? "StringData" : "String";
          } else if (typeof val == "object") {
              var typedef = new wm.TypeDefinition({owner: studio.application, name: inTypeDef.name + "." + inKey});
-             this.moreDefs[typedef.name] = typedef;             
+             this.moreDefs[typedef.name] = typedef;
              var field = new wm.TypeDefinitionField({owner: inTypeDef, name: inKey, fieldName: inKey, fieldType: (inTypeDef == this.typeDef ? this.typeName.getDataValue() : inTypeDef.name) + "." + inKey, isList: Boolean(isList)});
              this.parseObj(val, typedef);
-         }          
+         }
          if (type)
              new wm.TypeDefinitionField({owner: inTypeDef, name: inKey, fieldName: inKey, fieldType: type, isList: Boolean(isList)});
       }));
