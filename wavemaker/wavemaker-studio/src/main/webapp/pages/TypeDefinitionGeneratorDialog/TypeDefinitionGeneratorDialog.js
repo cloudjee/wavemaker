16afa403be77d361858ce71dd6ec43273ce0672e
Michael Kantor <mkantor@vmware.com>
2012-09-12 14:08:00 -0700

    Code Reformat

--- wavemaker/wavemaker-studio/src/main/webapp/pages/TypeDefinitionGeneratorDialog/TypeDefinitionGeneratorDialog.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/TypeDefinitionGeneratorDialog/TypeDefinitionGeneratorDialog.js
@@ -1,42 +1,48 @@
 dojo.declare("TypeDefinitionGeneratorDialog", wm.Page, {
     i18n: true,
     start: function() {
-	this.owner.owner.setTitle(this.getDictionaryItem("TITLE"));
-       this.moreDefs = {};
+        this.owner.owner.setTitle(this.getDictionaryItem("TITLE"));
+        this.moreDefs = {};
     },
     setTypeDefinition: function(inTypeDef) {
-	this.typeDef = inTypeDef;
-    this.moreDefs = {};
+        this.typeDef = inTypeDef;
+        this.moreDefs = {};
     },
     /* optionalInText is used by other pages wanting to use this generator as a service */
     generateButtonClick: function(inSender, e, inSender2, optionalInText, optionalInName) {
-      var text = optionalInText || this.jsonText.getDataValue();
-      var obj = dojo.fromJson(text);
-	wm.forEachProperty(this.typeDef.$, function(c) { c.destroy();});
-	wm.forEach(this.moreDefs, function(def) {def.destroy();});
-      this.moreDefs = {};
-	var typeName = optionalInName || this.typeName.getDataValue();
-	if (this.typeDef.name != typeName) {
-	    this.typeDef.setName(studio.application.getUniqueName(typeName));
-	}
-      this.parseObj(obj, this.typeDef);
-/*
+        var text = optionalInText || this.jsonText.getDataValue();
+        var obj = dojo.fromJson(text);
+        wm.forEachProperty(this.typeDef.$, function(c) {
+            c.destroy();
+        });
+        wm.forEach(this.moreDefs, function(def) {
+            def.destroy();
+        });
+        this.moreDefs = {};
+        var typeName = optionalInName || this.typeName.getDataValue();
+        if (this.typeDef.name != typeName) {
+            this.typeDef.setName(studio.application.getUniqueName(typeName));
+        }
+        this.parseObj(obj, this.typeDef);
+        /*
       this.variable1.setType(this.typeDef.name);
       this.variable1.setData(obj);
       this.html1.setHtml("<pre>"+this.typeDef.write("") + "\nvariable1.getData() Generates:\n" +
                                  dojo.toJson(this.variable1.getData(),true) +"</pre>");
-				 */
-	studio.componentModel.activate();
-	studio.refreshServiceTree("");
-	this.typeDef._studioTreeNode.setOpen(true);
-	wm.forEachProperty(this.moreDefs, function(typeDef) {typeDef._studioTreeNode.setOpen(true);});
-	studio.select(this.typeDef);
-	app.toastSuccess("Type generated");
+                 */
+        studio.componentModel.activate();
+        studio.refreshServiceTree("");
+        this.typeDef._studioTreeNode.setOpen(true);
+        wm.forEachProperty(this.moreDefs, function(typeDef) {
+            typeDef._studioTreeNode.setOpen(true);
+        });
+        studio.select(this.typeDef);
+        app.toastSuccess("Type generated");
     },
     closeButtonClick: function() {
-	this.owner.owner.hide();
+        this.owner.owner.hide();
     },
-/*
+    /*
     write: function(c, indent) {
         return indent +  c.name + ": [" + c.declaredClass + ",{" + this.writeProps(c) + "}, {}, {" + this.writeChildren(c.$, indent + "\t") + "}]";
     },
