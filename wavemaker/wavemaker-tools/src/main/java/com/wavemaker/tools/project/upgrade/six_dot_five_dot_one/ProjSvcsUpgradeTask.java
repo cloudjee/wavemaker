bbcb3cc2265d61424d9b58fd9e4b32519c325621
Edward Callahan <ecallahan@vmware.com>
2012-11-08 17:04:13 -0800

    Fixes projSvcs upgrade task for imports - WM-4699

--- wavemaker/wavemaker-tools/src/main/java/com/wavemaker/tools/project/upgrade/six_dot_five_dot_one/ProjSvcsUpgradeTask.java
+++ wavemaker/wavemaker-tools/src/main/java/com/wavemaker/tools/project/upgrade/six_dot_five_dot_one/ProjSvcsUpgradeTask.java
@@ -30,6 +30,8 @@ public class ProjSvcsUpgradeTask implements UpgradeTask {
 
 	private static final String RT_SVC_STR = "\t<import resource=\"classpath:runtimeService.spring.xml\" />\n";
 
+	private static final String SEC_SVC_STR = "\t<import resource=\"classpath:securityService.spring.xml\" />\n";
+	
 	private static final String TO_HDR_STR = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<beans xsi:schemaLocation=\"http://schema.cloudfoundry.org/spring http://schema.cloudfoundry.org/spring/cloudfoundry-spring-0.8.xsd http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-3.1.xsd\" xmlns=\"http://www.springframework.org/schema/beans\" xmlns:cloud=\"http://schema.cloudfoundry.org/spring\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n";
 
 	private static StringBuilder newFileStrB;
@@ -43,18 +45,22 @@ public class ProjSvcsUpgradeTask implements UpgradeTask {
 		try {
 			newFileStrB = new StringBuilder(TO_HDR_STR);
 			File projSvcsFile = project.getRootFolder().getFile(this.relativePath);
-			Folder classesFolder = projSvcsFile.getParent().getFolder("classes/");
-			Resources<File> xmlFiles = classesFolder.find().include(FilterOn.names().ending(".spring.xml")).files();
+			//scan services instead of classes. imported project exports have not been compiled
+			Folder servicesFolder = projSvcsFile.getParent().getFolder("../../services/");
+			Resources<File> xmlFiles = servicesFolder.find().include(FilterOn.names().ending(".spring.xml")).files();
 			Iterator<File> it = xmlFiles.iterator();
 			while(it.hasNext()){
 				File f = (File)it.next();
 				newFileStrB = newFileStrB.append(IMPORT_STR).append(f.getName()).append(CLOSE_STR);
-			} //Older projects may not contain wavemaker and runtime in classes at time of upgrade
+			} //services do not contain runtime, wavemaker or security, just add
 			if(newFileStrB.indexOf(WM_SVC_STR) < 0){
 				newFileStrB = newFileStrB.append(WM_SVC_STR);
 			}
 			if(newFileStrB.indexOf(RT_SVC_STR) < 0){
 				newFileStrB = newFileStrB.append(RT_SVC_STR);
+			}
+			if(newFileStrB.indexOf(SEC_SVC_STR) < 0){
+				newFileStrB = newFileStrB.append(SEC_SVC_STR);
 			}
 			newFileStrB = newFileStrB.append(BEAN_CLOSE_STR);
 			String content = projSvcsFile.getContent().asString();
