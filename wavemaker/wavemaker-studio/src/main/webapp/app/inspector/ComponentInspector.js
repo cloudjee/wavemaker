2a4bd7a919aafda87e9609f920f1e944d9d92dd2
michaelkantor <mkantor@vmware.com>
2012-11-10 18:59:29 -0800

    Loads wm.DateTime if not defined

--- wavemaker/wavemaker-studio/src/main/webapp/app/inspector/ComponentInspector.js
+++ wavemaker/wavemaker-studio/src/main/webapp/app/inspector/ComponentInspector.js
@@ -973,6 +973,7 @@
              case "date":
              case "time":
              case "java.util.date":
+                 wm.getComponentStructure("wm.DateTime");
                  ctor = wm.DateTime;
                  editorProps.dateMode = "Date";
                  break;
