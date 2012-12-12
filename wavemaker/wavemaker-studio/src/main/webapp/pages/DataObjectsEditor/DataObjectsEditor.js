13d85b8d3530b9e133b70c9f2aacf0ab0d1b49da
Michael Kantor <mkantor@vmware.com>
2012-12-11 17:06:35 -0800

    Warns user when a reserved word is used Issues: WM-1552
    Does not prevent user from picking a field name but does warn user
    with an alert when a reserved word is chosen.
    
    Uses the list of reserved words from:
    http://dev.mysql.com/doc/refman/5.5/en/reserved-words.html

--- wavemaker/wavemaker-studio/src/main/webapp/pages/DataObjectsEditor/DataObjectsEditor.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/DataObjectsEditor/DataObjectsEditor.js
@@ -16,6 +16,83 @@
 dojo.provide("wm.studio.pages.DataObjectsEditor.DataObjectsEditor");
 wm.require("wm.DataGrid");
 dojo.declare("DataObjectsEditor", wm.Page, {
+    reservedWords: ["ACCESSIBLE",  "ADD", "ALL",
+                    "ALTER",   "ANALYZE", "AND",
+                    "AS",  "ASC", "ASENSITIVE",
+"BEFORE","BETWEEN","BIGINT",
+"BINARY","BLOB","BOTH",
+"BY","CALL","CASCADE",
+"CASE","CHANGE","CHAR",
+"CHARACTER","CHECK","COLLATE",
+"COLUMN","CONDITION","CONSTRAINT",
+"CONTINUE","CONVERT","CREATE",
+"CROSS","CURRENT_DATE","CURRENT_TIME",
+"CURRENT_TIMESTAMP","CURRENT_USER","CURSOR",
+"DATABASE","DATABASES","DAY_HOUR",
+"DAY_MICROSECOND","DAY_MINUTE","DAY_SECOND",
+"DEC","DECIMAL","DECLARE",
+"DEFAULT","DELAYED","DELETE",
+"DESC","DESCRIBE","DETERMINISTIC",
+"DISTINCT","DISTINCTROW","DIV",
+"DOUBLE","DROP","DUAL",
+"EACH","ELSE","ELSEIF",
+"ENCLOSED","ESCAPED","EXISTS",
+"EXIT","EXPLAIN","FALSE",
+"FETCH","FLOAT","FLOAT4",
+"FLOAT8","FOR","FORCE",
+"FOREIGN","FROM","FULLTEXT",
+"GRANT","GROUP","HAVING",
+"HIGH_PRIORITY","HOUR_MICROSECOND","HOUR_MINUTE",
+"HOUR_SECOND","IF","IGNORE",
+"IN","INDEX","INFILE",
+"INNER","INOUT","INSENSITIVE",
+"INSERT","INT","INT1",
+"INT2","INT3","INT4",
+"INT8","INTEGER","INTERVAL",
+"INTO","IS","ITERATE",
+"JOIN","KEY","KEYS",
+"KILL","LEADING","LEAVE",
+"LEFT","LIKE","LIMIT",
+"LINEAR","LINES","LOAD",
+"LOCALTIME","LOCALTIMESTAMP","LOCK",
+"LONG","LONGBLOB","LONGTEXT",
+"LOOP","LOW_PRIORITY","MASTER_SSL_VERIFY_SERVER_CERT",
+"MATCH","MAXVALUE","MEDIUMBLOB",
+"MEDIUMINT","MEDIUMTEXT","MIDDLEINT",
+"MINUTE_MICROSECOND","MINUTE_SECOND","MOD",
+"MODIFIES","NATURAL","NOT",
+"NO_WRITE_TO_BINLOG","NULL","NUMERIC",
+"ON","OPTIMIZE","OPTION",
+"OPTIONALLY","OR","ORDER",
+"OUT","OUTER","OUTFILE",
+"PRECISION","PRIMARY","PROCEDURE",
+"PURGE","RANGE","READ",
+"READS","READ_WRITE","REAL",
+"REFERENCES","REGEXP","RELEASE",
+"RENAME","REPEAT","REPLACE",
+"REQUIRE","RESIGNAL","RESTRICT",
+"RETURN","REVOKE","RIGHT",
+"RLIKE","SCHEMA","SCHEMAS",
+"SECOND_MICROSECOND","SELECT","SENSITIVE",
+"SEPARATOR","SET","SHOW",
+"SIGNAL","SMALLINT","SPATIAL",
+"SPECIFIC","SQL","SQLEXCEPTION",
+"SQLSTATE","SQLWARNING","SQL_BIG_RESULT",
+"SQL_CALC_FOUND_ROWS","SQL_SMALL_RESULT","SSL",
+"STARTING","STRAIGHT_JOIN","TABLE",
+"TERMINATED","THEN","TINYBLOB",
+"TINYINT","TINYTEXT","TO",
+"TRAILING","TRIGGER","TRUE",
+"UNDO","UNION","UNIQUE",
+"UNLOCK","UNSIGNED","UPDATE",
+"USAGE","USE","USING","UTC_DATE","UTC_TIME","UTC_TIMESTAMP",
+"VALUES","VARBINARY","VARCHAR",
+"VARCHARACTER","VARYING","WHEN",
+"WHERE","WHILE","WITH",
+"WRITE","XOR","YEAR_MONTH",
+"ZEROFILL","GENERAL","IGNORE_SERVER_IDS","MASTER_HEARTBEAT_PERIOD",
+"MAXVALUE","RESIGNAL","SIGNAL",
+"SLOW"],
         i18n: true,
     PK_ATTR: "isPk",
     FK_ATTR: "isFk",
@@ -39,7 +116,7 @@ dojo.declare("DataObjectsEditor", wm.Page, {
         this.subscribe("wm-project-changed", this, "update");
         this.connect(this.relationshipsList.dijit, "onApplyCellEdit", this, "setDirty");
         this.connect(this.columnList.dijit, "onApplyCellEdit", this, "setDirty");
-
+        this.connect(this.columnList.dijit, "onApplyCellEdit", this, "onFieldNameChange");
 
         if (dojo.isIE <= 8) {
         wm.onidle(this, function() {
@@ -62,6 +139,13 @@ dojo.declare("DataObjectsEditor", wm.Page, {
            });
        }
 
+    },
+    onFieldNameChange: function(inValue, inRowIndex, inFieldIndex) {
+        if (inFieldIndex === 0 && dojo.indexOf(this.reservedWords,inValue.toUpperCase()) != -1) {
+            wm.onidle(this, function() {
+                app.alert(inValue + " is a database reserved word, we recommend you chose a different name");
+            });
+        }
     },
     onShow: function() {
             //if (this.dataObject && this.dataObject.name) {
@@ -302,7 +386,7 @@ dojo.declare("DataObjectsEditor", wm.Page, {
         this.dynamicUpdateCheckBox.setChecked(entity.dynamicUpdate);
         this.refreshCheckBox.setChecked(entity.refreshEntity);
     },
-        isEntityDirty: function() {
+    isEntityDirty: function() {
         return this.tableDetailSchemaName.isDirty ||
         this.tableDetailCatalogName && this.tableDetailCatalogName.isDirty ||
         this.tableDetailTableName && this.tableDetailTableName.isDirty ||
