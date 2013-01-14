a535a17b357a8b67c3e64a4e8616b0cde3eaa7e7
Michael Kantor <mkantor@vmware.com>
2013-01-14 14:27:32 -0800

    Copyright update

--- wavemaker-desktop/applications/SampleProjects/projects/WidgetExamples/webapproot/pages/About/About.widgets.js
+++ wavemaker-desktop/applications/SampleProjects/projects/WidgetExamples/webapproot/pages/About/About.widgets.js
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (C) 2013 VMware, Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 About.widgets = {
 	developerList: ["wm.Variable", {"isList":true,"json":"[{\"name\":\"Chris Keene\",\"dataValue\":\"Created the first version of this project for WaveMaker 6.3, and provided the core demos, concepts and layouts\"},{\"name\":\"Michael Kantor\",\"dataValue\":\"Updated the demo for WaveMaker 6.5\"}]","type":"EntryData"}, {}],
 	layoutBox1: ["wm.Layout", {"horizontalAlign":"left","verticalAlign":"top"}, {}, {
--- wavemaker-desktop/applications/SampleProjects/projects/WidgetExamples/webapproot/pages/Button_Popup/Button_Popup.widgets.js
+++ wavemaker-desktop/applications/SampleProjects/projects/WidgetExamples/webapproot/pages/Button_Popup/Button_Popup.widgets.js
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (C) 2013 VMware, Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 Button_Popup.widgets = {
 	popupButtonDialog: ["wm.DesignableDialog", {"buttonBarId":"buttonBar","containerWidgetId":"containerWidget","corner":"cr","desktopHeight":"120px","height":"120px","modal":false,"positionNear":"popupMenuButton1","title":"Popup Button Dialog","width":"300px"}, {"onShow":"popupButtonDialogShow"}, {
 		containerWidget: ["wm.Container", {"_classes":{"domNode":["wmdialogcontainer","MainContent"]},"height":"100%","horizontalAlign":"left","padding":"5","verticalAlign":"top","width":"100%"}, {}, {
