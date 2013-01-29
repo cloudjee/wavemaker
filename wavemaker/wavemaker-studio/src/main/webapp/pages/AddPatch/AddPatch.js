32b4b0a9ec6a69df2775da66d33d02991b3b48fd
Michael Kantor <mkantor@vmware.com>
2013-01-29 11:33:36 -0800

    Reformat code

--- wavemaker/wavemaker-studio/src/main/webapp/pages/AddPatch/AddPatch.js
+++ wavemaker/wavemaker-studio/src/main/webapp/pages/AddPatch/AddPatch.js
@@ -11,7 +11,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 
 
 dojo.provide("wm.studio.pages.AddPatch.AddPatch");
@@ -21,11 +21,11 @@ dojo.declare("AddPatch", wm.Page, {
     i18n: true,
     fileName: "",
     start: function() {
-	this.fileName = wm.version.replace(/[^a-zA-Z0-9]/g,"") + "_patches.js";
-	var text = String(loadDataSync(dojo.moduleUrl("wm.common").path + this.fileName));
-	this.fullText = text;
+        this.fileName = wm.version.replace(/[^a-zA-Z0-9]/g, "") + "_patches.js";
+        var text = String(loadDataSync(dojo.moduleUrl("wm.common").path + this.fileName));
+        this.fullText = text;
 
-/*
+        /*
 	var matches = text.match(/\/\* START WAVEMAKER PATCH \*\/([\s\S]*)\/\* END WAVEMAKER PATCH \*\//);
 	if (matches) {
 	    this.patchText = matches[1];
@@ -33,42 +33,43 @@ dojo.declare("AddPatch", wm.Page, {
 	    this.patchText = "";
 	}
 	*/
-	this.editor.setDataValue(this.fullText);
+        this.editor.setDataValue(this.fullText);
     },
-    patchUrl: studio.getDictionaryItem("URL_PATCHES", {studioVersionNumber: wm.studioConfig.studioVersion.replace(/^(\d+\.\d+)\..*/,"$1"),
-						       studioSubVersionNumber: wm.studioConfig.studioVersion.replace(/^(\d+)\.(\d+)\.(\d+).*/,"$1"+"_" + "$2" + "_" + "$3")}),
+    patchUrl: studio.getDictionaryItem("URL_PATCHES", {
+        studioVersionNumber: wm.studioConfig.studioVersion.replace(/^(\d+\.\d+)\..*/, "$1"),
+        studioSubVersionNumber: wm.studioConfig.studioVersion.replace(/^(\d+)\.(\d+)\.(\d+).*/, "$1" + "_" + "$2" + "_" + "$3")
+    }),
     loadPatchesClick: function() {
-	studio.studioService.requestAsync("getLatestPatches", [this.patchUrl], 
-					  dojo.hitch(this,function(inData) {
-					      studio.endWait();
-					      if (inData == "Could not find patches")
-						  app.alert(this.getDictionaryItem("ALERT_LOAD_PATCH_FAILED"));
-					      else {
-						  inData = inData.replace(/\&\#91;/g,"[").replace(/\&\#36;/g,"$");
-						  inData = inData.replace(/\<p\/\>/g,"");
-						  this.editor.setDataValue(inData);
-					      }
-					  }),
-					  dojo.hitch(this, function() {
-					      studio.endWait();
-					      app.alert(this.getDictionaryItem("ALERT_LOAD_PATCH_FAILED"));
-					  }));
-	studio.beginWait(this.getDictionaryItem("WAIT_LOAD_PATCHES"));
+        studio.studioService.requestAsync("getLatestPatches", [this.patchUrl],
+        dojo.hitch(this, function(inData) {
+            studio.endWait();
+            if (inData == "Could not find patches") app.alert(this.getDictionaryItem("ALERT_LOAD_PATCH_FAILED"));
+            else {
+                inData = inData.replace(/\&\#91;/g, "[").replace(/\&\#36;/g, "$");
+                inData = inData.replace(/\<p\/\>/g, "");
+                this.editor.setDataValue(inData);
+            }
+        }),
+        dojo.hitch(this, function() {
+            studio.endWait();
+            app.alert(this.getDictionaryItem("ALERT_LOAD_PATCH_FAILED"));
+        }));
+        studio.beginWait(this.getDictionaryItem("WAIT_LOAD_PATCHES"));
     },
     findCodeButtonClick: function() {
-	wm.openUrl(this.patchUrl);
+        wm.openUrl(this.patchUrl);
     },
     saveButtonClick: function() {
-	var editorValue = this.editor.getDataValue();
-	try {
-	    eval(editorValue);
-	} catch(e){
-	    alert(e);
-	    return;
-	}
-	//var patch = "/* START WAVEMAKER PATCH */\n" + this.editor.getDataValue() + "\n/* END WAVEMAKER PATCH */\n";
-	var patch = this.editor.getDataValue();
-/*
+        var editorValue = this.editor.getDataValue();
+        try {
+            eval(editorValue);
+        } catch (e) {
+            alert(e);
+            return;
+        }
+        //var patch = "/* START WAVEMAKER PATCH */\n" + this.editor.getDataValue() + "\n/* END WAVEMAKER PATCH */\n";
+        var patch = this.editor.getDataValue();
+        /*
 	var text = this.fullText;
 	if (text.match(/\/\* START WAVEMAKER PATCH[\s\S]*END WAVEMAKER PATCH \*\//)) {
 	    text = text.replace(/\/\* START WAVEMAKER PATCH[\s\S]*END WAVEMAKER PATCH \*\//, patch);
@@ -77,13 +78,13 @@ dojo.declare("AddPatch", wm.Page, {
 	}	
 	this.fullText = text;
 	*/
-	this.fullText = patch;
+        this.fullText = patch;
 
-	studio.resourceManagerService.requestSync("writeFile", ["/common/" + this.fileName, this.fullText]);
-	app.toastSuccess(this.getDictionaryItem("SAVED"));
-	this.owner.owner.hide();
+        studio.resourceManagerService.requestSync("writeFile", ["/common/" + this.fileName, this.fullText]);
+        app.toastSuccess(this.getDictionaryItem("SAVED"));
+        this.owner.owner.hide();
     },
     cancelButtonClick: function() {
-	this.owner.owner.hide();
+        this.owner.owner.hide();
     }
 });
