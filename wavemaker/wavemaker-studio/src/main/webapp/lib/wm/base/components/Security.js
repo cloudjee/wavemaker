c05fd1e8777f69e8f9326933d3ad79df6a86d065
administrator <mkantor@vmware.com>
2013-03-27 08:42:42 -0700

    Reformat code

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/components/Security.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/components/Security.js
@@ -44,36 +44,39 @@ wm.login = function(args, loginSuccessCallback, loginFailedCallback, properties,
     def.addErrback(onError);
 
     var onSuccess = function(inUrl) {
-    	if (app && app._loginDialog && app._loginDialog.showing) app._loginDialog.hide();
+        if (app && app._loginDialog && app._loginDialog.showing) app._loginDialog.hide();
 
-		/* Fire any custom onSuccess handlers BEFORE we do a navigation that will unload the developer's components */
-		deferred.callback(inUrl);
+        /* Fire any custom onSuccess handlers BEFORE we do a navigation that will unload the developer's components */
+        deferred.callback(inUrl);
 
-            var pathname = location.protocol + "//" + location.host + location.pathname + location.search; // sometimes using search helps and sometimes it breaks this test; still working out what is going on
-	    if (dojo.cookie.isSupported() && !wm.disableUserPrincipalCookie) {
-		var p = {username: properties.j_username, roles: wm.getUserRoles(true)};
-		wm.setUserPrincipal(p);
-	    } else {
-		wm.roles = wm.getUserRoles(true);
-	    }
-	    dojo.publish("wmRbacUpdate");
-		if (window["PhoneGap"] && wm.serverTimeOffset === undefined) {
+        var pathname = location.protocol + "//" + location.host + location.pathname + location.search; // sometimes using search helps and sometimes it breaks this test; still working out what is going on
+        if (dojo.cookie.isSupported() && !wm.disableUserPrincipalCookie) {
+            var p = {
+                username: properties.j_username,
+                roles: wm.getUserRoles(true)
+            };
+            wm.setUserPrincipal(p);
+        } else {
+            wm.roles = wm.getUserRoles(true);
+        }
+        dojo.publish("wmRbacUpdate");
+        if (window["PhoneGap"] && wm.serverTimeOffset === undefined) {
             app.getServerTimeOffset();
         }
-	    if (loginSuccessCallback) {
-		loginSuccessCallback(inUrl);
-	    } else if (window["PhoneGap"]) {
-		app.loadPage(app.pageContainer._initialPageName); // this is where tabletMain/phoneMain/main pageName are stored for now
+        if (loginSuccessCallback) {
+            loginSuccessCallback(inUrl);
+        } else if (window["PhoneGap"]) {
+            app.loadPage(app.pageContainer._initialPageName); // this is where tabletMain/phoneMain/main pageName are stored for now
 
-                // Typically this tests to see if we're on login.html and being directed to index.html
-	    } else if (pathname != inUrl) {
-		location.href = inUrl
+            // Typically this tests to see if we're on login.html and being directed to index.html
+        } else if (pathname != inUrl) {
+            location.href = inUrl
 
-                // If the page name is login, but app.main is not login, then we're on the real project,
-                // not the special project used for logging in.  If we're on the real project, a wm page nav is all that is needed
-	    } else if (app._page.name == "login" && app.main != "login") {
-                app.loadPage(app.main);
-            }
+            // If the page name is login, but app.main is not login, then we're on the real project,
+            // not the special project used for logging in.  If we're on the real project, a wm page nav is all that is needed
+        } else if (app._page.name == "login" && app.main != "login") {
+            app.loadPage(app.main);
+        }
     };
 
     def.addCallback(function(response, ioArgs) {
@@ -102,13 +105,13 @@ wm.clearUserPrincipal = function() {
 
 wm.getUserRoles = function(force) {
     if (!force) {
-	if (!wm.disableUserPrincipalCookie) {
-	    if (wm.getUserPrincipal().roles) {
-		return wm.getUserPrincipal().roles;
-	    }
-	} else if (wm.roles) {
-	    return wm.roles;
-	}
+    	if (!wm.disableUserPrincipalCookie) {
+    	    if (wm.getUserPrincipal().roles) {
+    		return wm.getUserPrincipal().roles;
+    	    }
+    	} else if (wm.roles) {
+    	    return wm.roles;
+    	}
     }
 	var s = wm.securityService || (wm.securityService =
 		new wm.JsonRpcService({name: "securityService", sync: true}));
