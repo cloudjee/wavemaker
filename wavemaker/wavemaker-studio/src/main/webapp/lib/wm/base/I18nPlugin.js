b1768c6e299b1ff818ef933dc9a38ed9f0a9a707
administrator <mkantor@vmware.com>
2013-04-21 12:14:39 -0700

    Reformat Code

--- wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/I18nPlugin.js
+++ wavemaker/wavemaker-studio/src/main/webapp/lib/wm/base/I18nPlugin.js
@@ -11,67 +11,71 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 
 dojo.provide("wm.base.I18nPlugin");
 dojo.require("wm.base.Plugin");
 
 wm.getDictionaryItem = function(name, params) {
-    if (params == undefined)
-	return wm.locale.phrases[name];
-    
+    if (params == undefined) return wm.locale.phrases[name];
+
     var newparams = {};
-    for (var i in params)
-	newparams[i] = (params[i] === undefined || params[i] === null) ? "" : params[i];
-	    return dojo.string.substitute(wm.locale.phrases[name],newparams);	
+    for (var i in params) {
+        newparams[i] = (params[i] === undefined || params[i] === null) ? "" : params[i];
+    }
+    return dojo.string.substitute(wm.locale.phrases[name], newparams);
 }
 
 wm.Plugin.plugin("i18n", wm.Component, {
     prepare: function(inProps) {
-	if (inProps && inProps.owner)
-	    var entry = inProps.owner.getDictionaryItem(inProps.name);
-	if (entry) 
-	    inProps = dojo.mixin(inProps, entry);
-	if (wm.branding) {
-	    var app =  inProps.owner ? inProps.owner.getOwnerApp() : null;
-	    if (app && app._brandingDictionary) {
-		var owner = inProps.owner;
-		var ownerName;
-		if (owner == app)
-		    ownerName = "app";
-		else if (owner instanceof wm.Page)
-		    ownerName = owner.declaredClass;
-		if (ownerName && app._brandingDictionary[ownerName] && app._brandingDictionary[ownerName][inProps.name]) {
-		    var componentProps = app._brandingDictionary[ownerName][inProps.name];
-		    var locale = dojo.locale;
-		    for (prop in componentProps) {
-			var propHash = componentProps[prop];
-			if (propHash[locale] !== undefined)
-			    inProps[prop] = propHash[locale];
-			else if (propHash["default"] !== undefined)
-			    inProps[prop] = propHash["default"];
-		    }
-		}
-	    }
-	}
-	this.i18nSocket(arguments);
+        if (inProps && inProps.owner) {
+            var entry = inProps.owner.getDictionaryItem(inProps.name);
+        }
+        if (entry) {
+            inProps = dojo.mixin(inProps, entry);
+        }
+        if (wm.branding) {
+            var app = inProps.owner ? inProps.owner.getOwnerApp() : null;
+            if (app && app._brandingDictionary) {
+                var owner = inProps.owner;
+                var ownerName;
+                if (owner == app) {
+                    ownerName = "app";
+                } else if (owner instanceof wm.Page) {
+                    ownerName = owner.declaredClass;
+                }
+                if (ownerName && app._brandingDictionary[ownerName] && app._brandingDictionary[ownerName][inProps.name]) {
+                    var componentProps = app._brandingDictionary[ownerName][inProps.name];
+                    var locale = dojo.locale;
+                    for (prop in componentProps) {
+                        var propHash = componentProps[prop];
+                        if (propHash[locale] !== undefined) {
+                            inProps[prop] = propHash[locale];
+                        } else if (propHash["default"] !== undefined) {
+                            inProps[prop] = propHash["default"];
+                        }
+                    }
+                }
+            }
+        }
+        this.i18nSocket(arguments);
     },
     getDictionaryItem: function(name, params) {
-	if (!this._i18nDictionary) {
-	    if (this.owner)
-		return this.owner.getDictionaryItem(name,params);
-	    else 
-		return "";
-	}
-	if (params == undefined)
-	    return this._i18nDictionary[name];
-	else {
-	    // undefined as a value can throw errors:
-	    var newparams = {};
-	    for (var i in params)
-		newparams[i] = (params[i] === undefined || params[i] === null) ? "" : params[i];
-	    return dojo.string.substitute(this._i18nDictionary[name],newparams);
-	}
+        if (!this._i18nDictionary) {
+            if (this.owner) {
+                return this.owner.getDictionaryItem(name, params);
+            } else {
+                return "";
+            }
+        }
+        if (params == undefined) {
+            return this._i18nDictionary[name];
+        } else {
+            // undefined as a value can throw errors:
+            var newparams = {};
+            for (var i in params)
+            newparams[i] = (params[i] === undefined || params[i] === null) ? "" : params[i];
+            return dojo.string.substitute(this._i18nDictionary[name], newparams);
+        }
     }
 });
-
