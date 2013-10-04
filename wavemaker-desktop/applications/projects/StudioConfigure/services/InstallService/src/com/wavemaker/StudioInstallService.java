b8ada28f13e25e082d56116f261244d93fb852ca
sowmya <sowmya.d@imaginea.com>
2013-10-04 11:36:23 +0530

    Reference to LGPL changed

--- wavemaker-desktop/applications/projects/StudioConfigure/services/InstallService/src/com/wavemaker/StudioInstallService.java
+++ wavemaker-desktop/applications/projects/StudioConfigure/services/InstallService/src/com/wavemaker/StudioInstallService.java
@@ -53,7 +53,7 @@ public class StudioInstallService extends com.wavemaker.runtime.javaservice.Java
     public void DownloadPackages() throws Exception {
         File webapproot = new File(RuntimeAccess.getInstance().getSession().getServletContext().getRealPath(""));
 
-     URL url = new URL("https://github.com/wavemaker/WaveMaker-LGPL-Resources-6-4/raw/6.6/repo.zip");
+     URL url = new URL("https://github.com/cloudjee/WaveMaker-LGPL-Resources/blob/master/repo.zip");
       URLConnection urlC = url.openConnection();
           // Copy resource to local file, use remote file
           // if no local file name specified
@@ -80,7 +80,7 @@ public class StudioInstallService extends com.wavemaker.runtime.javaservice.Java
          if (!versionFile.exists())
             throw new IOException("This repo.zip file does not contain the correct version information.");
          String s = IOUtils.read(versionFile);
-         if (s.indexOf("6.6") != 0)
+         if (s.indexOf("6.7") != 0)
             throw new IOException("This repo.zip file is from the wrong version of studio");
          File webapproot = new File(RuntimeAccess.getInstance().getSession().getServletContext().getRealPath(""));
 
