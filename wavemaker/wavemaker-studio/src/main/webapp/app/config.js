b4365c80a29550c5a4223019151b5ab2502b71f0
muralidaranm <muralidaran.m@imaginea.com>
2014-02-20 18:14:02 +0530

    updated 6.7.0.RELEASE version in pom files,config.js and boot.js

--- wavemaker/wavemaker-studio/src/main/webapp/app/config.js
+++ wavemaker/wavemaker-studio/src/main/webapp/app/config.js
@@ -31,7 +31,7 @@ if (djConfig.isDebug) {
 wm = {};
 
 wm.studioConfig = {
-        studioVersion: '6.6.0M2',
+        studioVersion: '6.7.0.RELEASE',
 	preventUnloadWarning: true,
 	previewPopup: false,
 	preventLiveData: (location.search.indexOf("nolive") >= 0),
